e69dea9cce46a23078ad618af35a96298a8c24a4
ko tainen <tainenko@gmail.com>
2019-04-14 18:39:25 +0800

    Add the solution of leetcode 922.sort-array-by-parity-ii

--- 922.sort-array-by-parity-ii.py
+++ 922.sort-array-by-parity-ii.py
@@ -28,4 +28,11 @@ class Solution(object):
         :type A: List[int]
         :rtype: List[int]
         """
+        j=1
+        for i in range(0,len(A),2):
+            if A[i]%2:
+                while A[j]%2:
+                    j+=2
+                A[i],A[j]=A[j],A[i]
+        return A
         
--- javascript/922.sort-array-by-parity-ii.js
+++ javascript/922.sort-array-by-parity-ii.js
@@ -27,5 +27,13 @@
  * @return {number[]}
  */
 var sortArrayByParityII = function(A) {
-    
+    for(let i=0,j=1;i<A.length;i+=2){
+        if(A[i]%2){
+            while(A[j]%2){
+                j+=2;
+            }
+            [A[i],A[j]]=[A[j],A[i]];
+        }
+    }
+    return A;
 };
