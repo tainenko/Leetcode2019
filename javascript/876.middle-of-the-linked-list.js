919b5a75ed4cd8b4229c75cad84b72450c28f5f9
ko tainen <tainenko@gmail.com>
2019-04-17 13:23:35 +0800

    Init the leetcode 876 middle of the linked list

---
+++ javascript/876.middle-of-the-linked-list.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * [876] Middle of the Linked List
+
+    https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+    * algorithms
+    * Easy (63.53%)
+    * Source Code:       876.middle-of-the-linked-list.py
+    * Total Accepted:    54.5K
+    * Total Submissions: 85.8K
+    * Testcase Example:  '[1,2,3,4,5]'
+
+    Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+    If there are two middle nodes, return the second middle node.
+
+    Example 1:
+    Input: [1,2,3,4,5]
+    Output: Node 3 from this list (Serialization: [3,4,5])
+    The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+    Note that we returned a ListNode object ans, such that:
+    ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+
+    Example 2:
+    Input: [1,2,3,4,5,6]
+    Output: Node 4 from this list (Serialization: [4,5,6])
+    Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+    Note:
+            The number of nodes in the given list will be between 1 and 100.
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    
+};
