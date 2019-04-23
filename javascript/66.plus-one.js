4b34264685b400abb362e07b6464be901e5e2b57
ko tainen <tainenko@gmail.com>
2019-04-23 22:34:26 +0800

    Add the solution of leetcode 66.plus-one.

--- javascript/66.plus-one.js
+++ javascript/66.plus-one.js
@@ -35,5 +35,21 @@ Explanation: The array represents the integer 4321.
  * @return {number[]}
  */
 var plusOne = function(digits) {
+    let carry=1;
+    for(let i=digits.length-1;i>=0;i--){
+        digits[i]+=carry;
+        if(digits[i]>9){
+            digits[i]-=10;
+            carry=1;
+        }else{
+            carry=0;
+            break;
+        }
+    }
+    if(carry){
+        digits.unshift(carry);
+    }
+
+    return digits;
     
 };
