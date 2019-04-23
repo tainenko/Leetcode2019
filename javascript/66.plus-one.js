7ac657df3207a40a0606be7dc7f5d7c73fb1ac2e
ko tainen <tainenko@gmail.com>
2019-04-23 22:18:32 +0800

    Init problem of leetcode 66 plus one.

---
+++ javascript/66.plus-one.js
@@ -0,0 +1,39 @@
+/**
+ * [66] Plus One
+
+https://leetcode.com/problems/plus-one/description/
+
+* algorithms
+* Easy (40.90%)
+* Source Code:       66.plus-one.js
+* Total Accepted:    377.7K
+* Total Submissions: 920.9K
+* Testcase Example:  '[1,2,3]'
+
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+
+
+Example 2:
+
+
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+};
