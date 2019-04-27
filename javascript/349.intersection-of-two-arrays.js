165585d62aec87f2ecf5614fac59084a4ad7a179
ko tainen <tainenko@gmail.com>
2019-04-27 23:38:28 +0800

    Add solution of leetcode 349 intersection of two arrays.

--- javascript/349.intersection-of-two-arrays.js
+++ javascript/349.intersection-of-two-arrays.js
@@ -38,5 +38,9 @@ Note:
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
+    let set1=new Set(nums1);
+    let set2=new Set(nums2);
+    let intersection = [...set1].filter(x => set2.has(x));
+    return intersection;
     
 };
