01642b3929ce6022b5bd8fb8ff99d856d5c85ca0
ko tainen <tainenko@gmail.com>
2019-04-27 23:25:53 +0800

    Init problem of leetcode 349 intersection of two arrays.

---
+++ javascript/349.intersection-of-two-arrays.js
@@ -0,0 +1,42 @@
+/**
+ * [349] Intersection of Two Arrays
+
+https://leetcode.com/problems/intersection-of-two-arrays/description/
+
+* algorithms
+* Easy (53.69%)
+* Source Code:       349.intersection-of-two-arrays.js
+* Total Accepted:    212K
+* Total Submissions: 391.9K
+* Testcase Example:  '[1,2,2,1]\n[2,2]'
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+
+
+Example 2:
+
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+
+
+Note:
+
+
+        Each element in the result must be unique.
+        The result can be in any order.
+
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    
+};
