502b22991a77df654f0edd9b059c55d80dd8b23d
ko tainen <tainenko@gmail.com>
2019-05-06 21:33:48 +0800

    Add the solution of leetcode 852 Peak Index in a Mountain Array.

--- javascript/852.peak-index-in-a-mountain-array.js
+++ javascript/852.peak-index-in-a-mountain-array.js
@@ -45,5 +45,10 @@ Note:
  * @return {number}
  */
 var peakIndexInMountainArray = function(A) {
+    for(let i=0;i<A.length;i++){
+        if(A[i-1]<A[i] && A[i]>A[i+1]){
+            return i;
+        }
+    }
     
 };
