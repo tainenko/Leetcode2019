0352a9d8abd91268ed9c63f7ccf6797765438de3
ko tainen <tainenko@gmail.com>
2019-06-10 16:37:42 +0800

    Add the solution of leetcode 1051.height-checker.js.

---
+++ javascript/1051.height-checker.js
@@ -0,0 +1,50 @@
+/**
+ * [1051] Height Checker
+
+https://leetcode.com/problems/height-checker/description/
+
+* algorithms
+* Easy (70.44%)
+* Source Code:       1051.height-checker.js
+* Total Accepted:    6.2K
+* Total Submissions: 8.9K
+* Testcase Example:  '[1,1,4,2,1,3]'
+
+Students are asked to stand in non-decreasing order of heights for an annual photo.
+
+Return the minimum number of students not standing in the right positions.  (This is the number of students that must move in order for all students to be standing in non-decreasing order of height.)
+
+ 
+
+Example 1:
+
+
+Input: [1,1,4,2,1,3]
+Output: 3
+Explanation:
+Students with heights 4, 3 and the last 1 are not standing in the right positions.
+
+
+ 
+
+Note:
+
+
+        1 <= heights.length <= 100
+        1 <= heights[i] <= 100
+
+ */
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    let tmp=[...heights].sort((a,b)=>a-b);
+    let count = 0;
+    for(let i=0;i<heights.length;i++){
+        if(tmp[i] !== heights[i]){
+            count+=1;
+        }
+    }
+    return count;
+}
