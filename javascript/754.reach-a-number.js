bf87c42141ce4ff66b08060eb6988a7144541153
ko tainen <tainenko@gmail.com>
2019-06-11 12:42:26 +0800

    Init the problem of 754.reach-a-number.js

---
+++ javascript/754.reach-a-number.js
@@ -0,0 +1,53 @@
+/**
+ * [754] Reach a Number
+
+https://leetcode.com/problems/reach-a-number/description/
+
+* algorithms
+* Easy (32.59%)
+* Source Code:       754.reach-a-number.js
+* Total Accepted:    10.5K
+* Total Submissions: 32.1K
+* Testcase Example:  '1'
+
+
+You are standing at position 0 on an infinite number line.  There is a goal at position target.
+
+On each move, you can either go left or right.  During the n-th move (starting from 1), you take n steps.
+
+Return the minimum number of steps required to reach the destination.
+
+
+Example 1:
+
+Input: target = 3
+Output: 2
+Explanation:
+On the first move we step from 0 to 1.
+On the second step we step from 1 to 3.
+
+
+
+Example 2:
+
+Input: target = 2
+Output: 3
+Explanation:
+On the first move we step from 0 to 1.
+On the second move we step  from 1 to -1.
+On the third move we step from -1 to 2.
+
+
+
+Note:
+target will be a non-zero integer in the range [-10^9, 10^9].
+
+ */
+
+/**
+ * @param {number} target
+ * @return {number}
+ */
+var reachNumber = function(target) {
+    
+};
