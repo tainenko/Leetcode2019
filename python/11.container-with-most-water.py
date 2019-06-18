80d08777927095f6dfe2935e4c17e84503af2680
ko tainen <tainenko@gmail.com>
2019-06-18 23:11:30 +0800

    Add the solution of leetcode 11.container-with-most-water.py

--- python/11.container-with-most-water.py
+++ python/11.container-with-most-water.py
@@ -35,4 +35,16 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        left=0
+        right=len(height)-1
+        res=0
+        while left<right:
+            res=max(res,(right-left)*min(height[right],height[left]))
+            if height[left]<height[right]:
+                left+=1
+            else:
+                right-=1
+
+        return res
+
         
