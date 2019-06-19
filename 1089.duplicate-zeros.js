4e57cbe013f47c5cb36964086c91e7d3b53003fa
ko tainen <tainenko@gmail.com>
2019-06-19 22:15:41 +0800

    Init problem of leetcode 1089.duplicate-zeros

---
+++ 1089.duplicate-zeros.js
@@ -0,0 +1,52 @@
+/**
+ * [1089] Duplicate Zeros
+
+https://leetcode.com/problems/duplicate-zeros/description/
+
+* algorithms
+* Easy (58.90%)
+* Source Code:       1089.duplicate-zeros.js
+* Total Accepted:    5.4K
+* Total Submissions: 9.2K
+* Testcase Example:  '[1,0,2,3,0,4,5,0]'
+
+Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written.
+
+Do the above modifications to the input array in place, do not return anything from your function.
+
+ 
+
+Example 1:
+
+
+Input: [1,0,2,3,0,4,5,0]
+Output: null
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+
+Example 2:
+
+
+Input: [1,2,3]
+Output: null
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+
+
+ 
+
+Note:
+
+
+        1 <= arr.length <= 10000
+        0 <= arr[i] <= 9
+
+ */
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    
+};
