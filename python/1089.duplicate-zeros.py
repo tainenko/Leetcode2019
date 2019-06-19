664edaa29edd4d7fdf134fb8eaef5dcc7db976bf
ko tainen <tainenko@gmail.com>
2019-06-19 22:23:06 +0800

    Add the solution of leetcode 1089.duplicate-zeros

--- python/1089.duplicate-zeros.py
+++ python/1089.duplicate-zeros.py
@@ -50,3 +50,12 @@ class Solution(object):
         :type arr: List[int]
         :rtype: None Do not return anything, modify arr in-place instead.
         """
+        i=0
+        while i<len(arr):
+            if arr[i]==0:
+                arr.insert(i,0)
+                arr.pop()
+                i+=1
+            i+=1
+        return
+
