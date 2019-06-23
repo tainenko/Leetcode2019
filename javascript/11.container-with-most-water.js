fa25f6e5594b59ff8592830f904c5bac203657b3
ko tainen <tainenko@gmail.com>
2019-06-23 21:10:50 +0800

    Add the solution of leetcode 11.container-with-most-water.js

--- javascript/11.container-with-most-water.js
+++ javascript/11.container-with-most-water.js
@@ -34,5 +34,17 @@ Output: 49
  * @return {number}
  */
 var maxArea = function(height) {
+    let max=0;
+    let left=0;
+    let right=height.length-1;
+    while(left<right){
+        max=Math.max(max,Math.min(height[left],height[right])*(right-left));
+        if(height[left]<height[right]){
+            left++;
+        }else{
+            right--;
+        }
+    }
+    return max;
     
 };
