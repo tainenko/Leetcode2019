ce5df3facec55d4a23bfa4bfaf2ba845bdc193a2
ko tainen <tainenko@gmail.com>
2019-06-24 01:52:54 +0800

    Add the solution of leetcode 1037.valid-boomerang.js

--- javascript/1037.valid-boomerang.js
+++ javascript/1037.valid-boomerang.js
@@ -46,5 +46,10 @@ Note:
  * @return {boolean}
  */
 var isBoomerang = function(points) {
-    
+    let dx1=points[1][0]-points[0][0];
+    let dx2=points[2][0]-points[0][0];
+    let dy1=points[1][1]-points[0][1];
+    let dy2=points[2][1]-points[0][1];
+
+    return dx1*dy2!=dx2*dy1;
 };
