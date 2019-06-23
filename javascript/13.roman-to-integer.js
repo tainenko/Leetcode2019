c660a858573daa12123caf6604665d1bad95658e
ko tainen <tainenko@gmail.com>
2019-06-23 21:22:53 +0800

    Add the solution of leetcode 13.roman-to-integet.js

--- javascript/13.roman-to-integer.js
+++ javascript/13.roman-to-integer.js
@@ -74,5 +74,27 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  * @return {number}
  */
 var romanToInt = function(s) {
+    let roman_map=new Map(
+        [
+            ['I',1],
+            ['V',5],
+            ['X',10],
+            ['L',50],
+            ['C',100],
+            ['D',500],
+            ['M',1000]
+        ]
+    );
+    let ret=0;
+    for(var i=0;i<s.length-1;i++){
+        if(roman_map.get(s[i])<roman_map.get(s[i+1])){
+            ret-=roman_map.get(s[i]);
+        }else{
+            ret+=roman_map.get(s[i]);
+        }
+    }
+    ret+=roman_map.get(s[s.length-1]);
+    return ret;
+
     
 };
