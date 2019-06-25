e4d31c378a25002b84c898ebd43b3eb93892ee77
ko tainen <tainenko@gmail.com>
2019-06-25 08:54:42 +0800

    Init the problem of leetcode 24.swqp-nodes-in-paris

---
+++ 24.swap-nodes-in-pairs.js
@@ -0,0 +1,38 @@
+/**
+ * [24] Swap Nodes in Pairs
+
+https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+* algorithms
+* Medium (44.98%)
+* Source Code:       24.swap-nodes-in-pairs.js
+* Total Accepted:    323.7K
+* Total Submissions: 718.4K
+* Testcase Example:  '[1,2,3,4]'
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example:
+
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    
+};
---
+++ 24.swap-nodes-in-pairs.py
@@ -0,0 +1,37 @@
+'''
+[24] Swap Nodes in Pairs
+
+https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+* algorithms
+* Medium (44.98%)
+* Source Code:       24.swap-nodes-in-pairs.py
+* Total Accepted:    323.7K
+* Total Submissions: 718.4K
+* Testcase Example:  '[1,2,3,4]'
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example:
+
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
