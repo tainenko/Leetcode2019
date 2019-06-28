04aa44e3ae534774d493523e56d6fa5bd40705ef
ko tainen <tainenko@gmail.com>
2019-06-28 17:11:34 +0800

    Init problem of leetcode 16.3sum-closest

---
+++ 16.3sum-closest.py
@@ -0,0 +1,30 @@
+'''
+[16] 3Sum Closest
+
+https://leetcode.com/problems/3sum-closest/description/
+
+* algorithms
+* Medium (45.77%)
+* Source Code:       16.3sum-closest.py
+* Total Accepted:    355.9K
+* Total Submissions: 777.5K
+* Testcase Example:  '[-1,2,1,-4]\n1'
+
+Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+Example:
+
+
+Given array nums = [-1, 2, 1, -4], and target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+'''
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
