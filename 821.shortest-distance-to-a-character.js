255ea721c7b6cd33e0d64e1873fafc479933b3df
ko tainen <tainenko@gmail.com>
2019-07-17 09:18:56 +0800

    Init problem of 921.shortest-distance-to-a-chaacter.js

---
+++ 821.shortest-distance-to-a-character.js
@@ -0,0 +1,70 @@
+/**
+ * [821] Shortest Distance to a Character
+
+https://leetcode.com/problems/shortest-distance-to-a-character/description/
+
+* algorithms
+* Easy (63.69%)
+* Source Code:       821.shortest-distance-to-a-character.py
+* Total Accepted:    39.9K
+* Total Submissions: 62.5K
+* Testcase Example:  '"loveleetcode"\n"e"'
+
+Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+
+Example 1:
+
+
+Input: S = "loveleetcode", C = 'e'
+Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+
+
+ 
+
+Note:
+
+
+        S string length is in [1, 10000].
+        C is a single character, and guaranteed to be in string S.
+        All letters in S and C are lowercase.
+
+
+GSC-HEYWARDN1-NB:leetcode ketainian$ leetcode show 821 -g -l javascript
+[821] Shortest Distance to a Character
+
+https://leetcode.com/problems/shortest-distance-to-a-character/description/
+
+* algorithms
+* Easy (63.69%)
+* Source Code:       821.shortest-distance-to-a-character.js
+* Total Accepted:    39.9K
+* Total Submissions: 62.5K
+* Testcase Example:  '"loveleetcode"\n"e"'
+
+Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+
+Example 1:
+
+
+Input: S = "loveleetcode", C = 'e'
+Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+
+
+ 
+
+Note:
+
+
+        S string length is in [1, 10000].
+        C is a single character, and guaranteed to be in string S.
+        All letters in S and C are lowercase.
+
+ */
+/**
+ * @param {string} S
+ * @param {character} C
+ * @return {number[]}
+ */
+var shortestToChar = function(S, C) {
+    
+};
