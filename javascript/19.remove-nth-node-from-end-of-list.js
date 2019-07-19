be75db0269a1d64c5b80b10a1b9733723a4fb80f
ko tainen <tainenko@gmail.com>
2019-07-19 17:12:49 +0800

    Add solution leetcode 19.remove-nth-node-from-end-of-list.js

--- javascript/19.remove-nth-node-from-end-of-list.js
+++ javascript/19.remove-nth-node-from-end-of-list.js
@@ -42,5 +42,19 @@ Could you do this in one pass?
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
+    var fast = head;
+    var slow = head;
+    for(let i=0;i<n;i++){
+        fast=fast.next;
+    }
+    if (fast===null){
+        return head.next;
+    }
+    while(fast.next!=null){
+        slow=slow.next;
+        fast=fast.next;
+    }
+    slow.next=slow.next.next;
+    return head;
     
 };
