434629566a0199ef1bfb0ac0b9a2f9be20bdf0ed
ko tainen <tainenko@gmail.com>
2019-07-20 08:34:56 +0800

    Add solution of 896.monotonic-array.js

--- javascript/896.monotonic-array.js
+++ javascript/896.monotonic-array.js
@@ -75,5 +75,12 @@ Note:
  * @return {boolean}
  */
 var isMonotonic = function(A) {
+    let inc = true;
+    let dec = true;
+    for(let i=0; i<A.length-1;i++){
+        inc = inc && A[i]<=A[i+1];
+        dec = dec && A[i]>=A[i+1];
+    }
+    return inc || dec;
     
 };
