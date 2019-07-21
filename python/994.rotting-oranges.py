ee00ad04c35ee758ea0c0ba6089444c676170525
ko tainen <tainenko@gmail.com>
2019-07-22 00:18:09 +0800

    Add solution of leetcode 994.rotting-oranges.py

--- python/994.rotting-oranges.py
+++ python/994.rotting-oranges.py
@@ -30,7 +30,10 @@ Example 1:
 
 
 
-Input: [[2,1,1],[1,1,0],[0,1,1]]
+Input: [
+[2,1,1],
+[1,1,0],
+[0,1,1]]
 Output: 4
 
 
@@ -38,7 +41,10 @@ Output: 4
 Example 2:
 
 
-Input: [[2,1,1],[0,1,1],[1,0,1]]
+Input: [
+[2,1,1],
+[0,1,1],
+[1,0,1]]
 Output: -1
 Explanation:  The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
 
@@ -68,4 +74,28 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        i = len(grid)
+        j = len(grid[0])
+        fresh=0
+        q=[]
+        for x in range(i):
+            for y in range(j):
+                if grid[x][y]==1:
+                    fresh+=1
+                if grid[x][y]==2:
+                    q.append((x,y,0))
+        direction=[(1,0),(-1,0),(0,1),(0,-1)]
+        res=0
+        while q:
+            x,y,c=q.pop(0)
+            for row,col in direction:
+                if i>x+row>=0 and j>y+col>=0 and grid[x+row][y+col]==1:
+                    q.append((x+row,y+col,c+1))
+                    res=max(res,c+1)
+                    grid[x+row][y+col]=2
+                    fresh-=1
+        if fresh!=0:
+            return -1
+        return res
+
         
