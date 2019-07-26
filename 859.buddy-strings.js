edbebf42625b28a35e8eb26439557ac3171694da
ko tainen <tainenko@gmail.com>
2019-07-27 00:53:23 +0800

    Init problem of 859.buddy-strings

---
+++ 859.buddy-strings.js
@@ -0,0 +1,74 @@
+/**
+ * [859] Buddy Strings
+
+https://leetcode.com/problems/buddy-strings/description/
+
+* algorithms
+* Easy (27.66%)
+* Source Code:       859.buddy-strings.js
+* Total Accepted:    27K
+* Total Submissions: 97.3K
+* Testcase Example:  '"ab"\n"ba"'
+
+Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
+
+ 
+
+Example 1:
+
+
+
+Input: A = "ab", B = "ba"
+Output: true
+
+
+
+Example 2:
+
+
+Input: A = "ab", B = "ab"
+Output: false
+
+
+
+Example 3:
+
+
+Input: A = "aa", B = "aa"
+Output: true
+
+
+
+Example 4:
+
+
+Input: A = "aaaaaaabc", B = "aaaaaaacb"
+Output: true
+
+
+
+Example 5:
+
+
+Input: A = "", B = "aa"
+Output: false
+
+
+ 
+
+Note:
+
+
+        0 <= A.length <= 20000
+        0 <= B.length <= 20000
+        A and B consist only of lowercase letters.
+
+ */
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+var buddyStrings = function(A, B) {
+    
+};
