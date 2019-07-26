0f21b1e0c7f5ceadafa1c2c54be9e2718fe9e1ff
ko tainen <tainenko@gmail.com>
2019-07-26 09:59:04 +0800

    Add solution of leetcode 917.reverse-only-letters

--- python/917.reverse-only-letters.py
+++ python/917.reverse-only-letters.py
@@ -58,10 +58,25 @@ Note:
         S doesn't contain \ or "
 
 '''
+
+
 class Solution(object):
     def reverseOnlyLetters(self, S):
         """
         :type S: str
         :rtype: str
         """
-        
+        S = list(S)
+        left = 0
+        right = len(S) - 1
+        while left < right:
+            while left < len(S) and not S[left].isalpha():
+                left += 1
+            while right >= 0 and not S[right].isalpha():
+                right -= 1
+
+            if left < right:
+                S[left], S[right] = S[right], S[left]
+                left += 1
+                right -= 1
+        return ''.join(S)
