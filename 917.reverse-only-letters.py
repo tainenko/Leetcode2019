f5ac58ed7228f88faf8cf86e7ff8b1bee688c8ad
ko tainen <tainenko@gmail.com>
2019-07-26 09:48:57 +0800

    Init problem of leetcode 917.reverse-only-letters

---
+++ 917.reverse-only-letters.py
@@ -0,0 +1,67 @@
+'''
+[917] Reverse Only Letters
+
+https://leetcode.com/problems/reverse-only-letters/description/
+
+* algorithms
+* Easy (56.00%)
+* Source Code:       917.reverse-only-letters.py
+* Total Accepted:    30.3K
+* Total Submissions: 54K
+* Testcase Example:  '"ab-cd"'
+
+Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+Example 1:
+
+
+Input: "ab-cd"
+Output: "dc-ba"
+
+
+
+Example 2:
+
+
+Input: "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+
+
+
+Example 3:
+
+
+Input: "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+
+
+ 
+
+
+Note:
+
+
+        S.length <= 100
+        33 <= S[i].ASCIIcode <= 122 
+        S doesn't contain \ or "
+
+'''
+class Solution(object):
+    def reverseOnlyLetters(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        
