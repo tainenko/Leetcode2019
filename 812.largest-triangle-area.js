4bb336a3cffdbacb84b9bb87154489c7f791ac5d
ko tainen <tainenko@gmail.com>
2019-07-28 09:46:43 +0800

    Init problem of leetcode 812.largest-triangle-are

---
+++ 812.largest-triangle-area.js
@@ -0,0 +1,75 @@
+/**
+ * [812] Largest Triangle Area
+
+https://leetcode.com/problems/largest-triangle-area/description/
+
+* algorithms
+* Easy (56.33%)
+* Source Code:       812.largest-triangle-area.py
+* Total Accepted:    15.6K
+* Total Submissions: 27.7K
+* Testcase Example:  '[[0,0],[0,1],[1,0],[0,2],[2,0]]'
+
+You have a list of points in the plane. Return the area of the largest triangle that can be formed by any 3 of the points.
+
+
+Example:
+Input: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+Output: 2
+Explanation:
+The five points are show in the figure below. The red triangle is the largest.
+
+
+
+
+Notes:
+
+
+        3 <= points.length <= 50.
+        No points will be duplicated.
+         -50 <= points[i][j] <= 50.
+        Answers within 10^-6 of the true value will be accepted as correct.
+
+
+ 
+
+ketainiande-MacBook-Pro:leetcode ketainian$ leetcode show 812 -g -l javascript
+[812] Largest Triangle Area
+
+https://leetcode.com/problems/largest-triangle-area/description/
+
+* algorithms
+* Easy (56.33%)
+* Source Code:       812.largest-triangle-area.js
+* Total Accepted:    15.6K
+* Total Submissions: 27.7K
+* Testcase Example:  '[[0,0],[0,1],[1,0],[0,2],[2,0]]'
+
+You have a list of points in the plane. Return the area of the largest triangle that can be formed by any 3 of the points.
+
+
+Example:
+Input: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+Output: 2
+Explanation:
+The five points are show in the figure below. The red triangle is the largest.
+
+
+
+
+Notes:
+
+
+        3 <= points.length <= 50.
+        No points will be duplicated.
+         -50 <= points[i][j] <= 50.
+        Answers within 10^-6 of the true value will be accepted as correct.
+
+ */
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var largestTriangleArea = function(points) {
+    
+};
