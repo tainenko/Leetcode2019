b0c47b46817facc5e010f9f492f090760d1348af
ko tainen <tainenko@gmail.com>
2019-07-29 10:24:57 +0800

    Add solution of leetcode 824.goat-latin.py

--- python/824.goat-latin.py
+++ python/824.goat-latin.py
@@ -60,4 +60,10 @@ class Solution(object):
         :type S: str
         :rtype: str
         """
-        
+        vowels={'a','e','i','o','u','A','E','I','O','U'}
+        words=S.split(' ')
+        for i in range(len(words)):
+            if words[i][0] not in vowels:
+                words[i]=words[i][1:]+words[i][0]
+            words[i]=words[i]+'ma'+'a'*(i+1)
+        return ' '.join(words)
