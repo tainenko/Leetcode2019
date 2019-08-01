8a4674555a1be82f6df50aee733dbda758f96c44
ko tainen <tainenko@gmail.com>
2019-08-01 15:05:19 +0800

    Add solution of 558.quad-tree-intersection.py

--- python/558.quad-tree-intersection.py
+++ python/558.quad-tree-intersection.py
@@ -101,6 +101,8 @@ class Node(object):
         self.bottomLeft = bottomLeft
         self.bottomRight = bottomRight
 """
+
+
 class Solution(object):
     def intersect(self, quadTree1, quadTree2):
         """
@@ -108,4 +110,24 @@ class Solution(object):
         :type quadTree2: Node
         :rtype: Node
         """
-        
+        if quadTree1.isLeaf:
+            if quadTree1.val:
+                return quadTree1
+            else:
+                return quadTree2
+        if quadTree2.isLeaf:
+            if quadTree2.val:
+                return quadTree2
+            else:
+                return quadTree1
+
+        quadTree1.topLeft = self.intersect(quadTree1.topLeft, quadTree2.topLeft)
+        quadTree1.topRight = self.intersect(quadTree1.topRight, quadTree2.topRight)
+        quadTree1.bottomLeft = self.intersect(quadTree1.bottomLeft, quadTree2.bottomLeft)
+        quadTree1.bottomRight = self.intersect(quadTree1.bottomRight, quadTree2.bottomRight)
+
+        if quadTree1.topLeft.isLeaf and quadTree1.topRight.isLeaf and quadTree1.bottomLeft.isLeaf and quadTree1.bottomRight.isLeaf:
+            if quadTree1.topLeft.val == quadTree1.topRight.val == quadTree1.bottomLeft.val == quadTree1.bottomRight.val:
+                quadTree1.isLeaf = True
+                quadTree1.val = quadTree1.topLeft.val
+        return quadTree1
