bc6bb018582ccec3915a79675f19939946fca0e9
ko tainen <tainenko@gmail.com>
2019-08-13 20:49:11 +0800

    Add solution of leetcode 34.find-first-and-last-position-of-element-in-sorted-array.py

--- python/34.find-first-and-last-position-of-element-in-sorted-array.py
+++ python/34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -38,4 +38,30 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        right = self.findUpperbound(nums, target)
+        if right < 0 or nums[right] != target:
+            return [-1, -1]
+        left = self.findLowerbound(nums, target)
+        return [left, right]
+
+    def findUpperbound(self, nums, target):
+        right = len(nums) - 1
+        left = 0
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right
+
+    def findLowerbound(self, nums, target):
+        right = len(nums) - 1
+        left = 0
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
