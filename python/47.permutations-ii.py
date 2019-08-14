ed90a84bda52762598322cc1fc92100425bddcde
ko tainen <tainenko@gmail.com>
2019-08-14 21:22:07 +0800

    Init problem of leetcode 47.permutations-ii.py

---
+++ python/47.permutations-ii.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=47 lang=python
+#
+# [47] Permutations II
+#
+# https://leetcode.com/problems/permutations-ii/description/
+#
+# algorithms
+# Medium (41.39%)
+# Total Accepted:    262.4K
+# Total Submissions: 630.4K
+# Testcase Example:  '[1,1,2]'
+#
+# Given a collection of numbers that might contain duplicates, return all
+# possible unique permutations.
+# 
+# Example:
+# 
+# 
+# Input: [1,1,2]
+# Output:
+# [
+# ⁠ [1,1,2],
+# ⁠ [1,2,1],
+# ⁠ [2,1,1]
+# ]
+# 
+# 
+#
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
