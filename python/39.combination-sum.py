5488368425f03c19bfcbde052d9ff06b95bd74ec
ko tainen <tainenko@gmail.com>
2019-08-14 12:01:00 +0800

    Init problem of leetcode 39.combination-sum.py

---
+++ python/39.combination-sum.py
@@ -0,0 +1,59 @@
+#
+# @lc app=leetcode id=39 lang=python
+#
+# [39] Combination Sum
+#
+# https://leetcode.com/problems/combination-sum/description/
+#
+# algorithms
+# Medium (49.72%)
+# Total Accepted:    376.5K
+# Total Submissions: 753K
+# Testcase Example:  '[2,3,6,7]\n7'
+#
+# Given a set of candidate numbers (candidates) (without duplicates) and a
+# target number (target), find all unique combinations in candidates where the
+# candidate numbers sums to target.
+# 
+# The same repeated number may be chosen from candidates unlimited number of
+# times.
+# 
+# Note:
+# 
+# 
+# All numbers (including target) will be positive integers.
+# The solution set must not contain duplicate combinations.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: candidates = [2,3,6,7], target = 7,
+# A solution set is:
+# [
+# ⁠ [7],
+# ⁠ [2,2,3]
+# ]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: candidates = [2,3,5], target = 8,
+# A solution set is:
+# [
+# [2,2,2,2],
+# [2,3,3],
+# [3,5]
+# ]
+# 
+# 
+#
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
