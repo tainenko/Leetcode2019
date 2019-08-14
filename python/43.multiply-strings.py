8e8867228c119285f970d89984b8f1a1651cb88d
ko tainen <tainenko@gmail.com>
2019-08-14 12:53:00 +0800

    Init problem of leetcode 43.multiplyu-strings.py

---
+++ python/43.multiply-strings.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=43 lang=python
+#
+# [43] Multiply Strings
+#
+# https://leetcode.com/problems/multiply-strings/description/
+#
+# algorithms
+# Medium (31.18%)
+# Total Accepted:    217K
+# Total Submissions: 693.2K
+# Testcase Example:  '"2"\n"3"'
+#
+# Given two non-negative integers num1 and num2 represented as strings, return
+# the product of num1 and num2, also represented as a string.
+# 
+# Example 1:
+# 
+# 
+# Input: num1 = "2", num2 = "3"
+# Output: "6"
+# 
+# Example 2:
+# 
+# 
+# Input: num1 = "123", num2 = "456"
+# Output: "56088"
+# 
+# 
+# Note:
+# 
+# 
+# The length of both num1 and num2 is < 110.
+# Both num1 and num2 contain only digits 0-9.
+# Both num1 and num2 do not contain any leading zero, except the number 0
+# itself.
+# You must not use any built-in BigInteger library or convert the inputs to
+# integer directly.
+# 
+# 
+#
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
