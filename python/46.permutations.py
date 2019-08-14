25e8676b3b62a15e5a301259d008d849eee2b212
ko tainen <tainenko@gmail.com>
2019-08-14 21:13:59 +0800

    Init problem of leetcode 46.permutations.py

---
+++ python/46.permutations.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=46 lang=python
+#
+# [46] Permutations
+#
+# https://leetcode.com/problems/permutations/description/
+#
+# algorithms
+# Medium (56.36%)
+# Total Accepted:    415.7K
+# Total Submissions: 733.4K
+# Testcase Example:  '[1,2,3]'
+#
+# Given a collection of distinct integers, return all possible permutations.
+# 
+# Example:
+# 
+# 
+# Input: [1,2,3]
+# Output:
+# [
+# ⁠ [1,2,3],
+# ⁠ [1,3,2],
+# ⁠ [2,1,3],
+# ⁠ [2,3,1],
+# ⁠ [3,1,2],
+# ⁠ [3,2,1]
+# ]
+# 
+# 
+#
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
