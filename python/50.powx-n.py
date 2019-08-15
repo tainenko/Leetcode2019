f0985730e963106bdbb30fbaa640ca4729b2c13a
ko tainen <tainenko@gmail.com>
2019-08-15 11:56:31 +0800

    Add solution of leetcode 50.powx-n.py

--- python/50.powx-n.py
+++ python/50.powx-n.py
@@ -50,4 +50,14 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        
+        if n == 0:
+            return 1
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+        half = self.myPow(x, n // 2)
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
