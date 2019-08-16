e881dcf4fbc01b4c2aca6b83b2047d41d1595eb4
ko tainen <tainenko@gmail.com>
2019-08-16 14:07:36 +0800

    Init problem of leetcode 73.set-matrix-zeroes.py

---
+++ python/73.set-matrix-zeroe.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode id=73 lang=python
+#
+# [73] Set Matrix Zeroes
+#
+# https://leetcode.com/problems/set-matrix-zeroes/description/
+#
+# algorithms
+# Medium (40.53%)
+# Total Accepted:    226.9K
+# Total Submissions: 559.7K
+# Testcase Example:  '[[1,1,1],[1,0,1],[1,1,1]]'
+#
+# Given a m x n matrix, if an element is 0, set its entire row and column to 0.
+# Do it in-place.
+# 
+# Example 1:
+# 
+# 
+# Input: 
+# [
+# [1,1,1],
+# [1,0,1],
+# [1,1,1]
+# ]
+# Output: 
+# [
+# [1,0,1],
+# [0,0,0],
+# [1,0,1]
+# ]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: 
+# [
+# [0,1,2,0],
+# [3,4,5,2],
+# [1,3,1,5]
+# ]
+# Output: 
+# [
+# [0,0,0,0],
+# [0,4,5,0],
+# [0,3,1,0]
+# ]
+# 
+# 
+# Follow up:
+# 
+# 
+# A straight forward solution using O(mn) space is probably a bad idea.
+# A simple improvement uses O(m + n) space, but still not the best
+# solution.
+# Could you devise a constant space solution?
+# 
+# 
+#
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        
--- python/73.set-matrix-zeroes.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def setZeroes(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
-        """
-        
