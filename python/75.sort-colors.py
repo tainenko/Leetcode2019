694ad3dcaed92f6500c943dec221c5b2fa5629f1
ko tainen <tainenko@gmail.com>
2019-08-16 18:42:10 +0800

    Add bubble-sort solution of leetcode 75.sort-colors.py

--- python/75.sort-colors.py
+++ python/75.sort-colors.py
@@ -44,5 +44,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-
-        
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
