1d58d8c787935f47d430284091995c2c058f5bd9
ko tainen <tainenko@gmail.com>
2019-08-19 00:51:58 +0800

    Add solution of leetcode 59.spiral-matrix-ii.py

--- python/59.spiral-matrix-ii.py
+++ python/59.spiral-matrix-ii.py
@@ -27,10 +27,44 @@
 # 
 # 
 #
+import math
+
+
 class Solution(object):
     def generateMatrix(self, n):
         """
         :type n: int
         :rtype: List[List[int]]
         """
-        
+        res = [[None] * n for _ in range(n)]
+
+        left = top = 0
+        right = bottom = n - 1
+
+        num = 1
+        while left < right and bottom > top:
+            for i in range(left, right):
+                res[top][i] = num
+                num += 1
+            for i in range(top, bottom):
+                res[i][right] = num
+                num += 1
+            for i in range(right, left, -1):
+                res[bottom][i] = num
+                num += 1
+            for i in range(bottom, top, -1):
+                res[i][left] = num
+                num += 1
+            bottom -= 1
+            top += 1
+            left += 1
+            right -= 1
+        if bottom == top:
+            for i in range(left, right + 1):
+                res[top][i] = num
+                num += 1
+        elif right == left:
+            for i in range(top, bottom + 1):
+                res[i][right] = num
+                num += 1
+        return res
