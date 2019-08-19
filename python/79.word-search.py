e07a8a3491b2ddf7568f4623ae8fa412cf7d5fa2
ko tainen <tainenko@gmail.com>
2019-08-19 18:48:09 +0800

    init problem of leetcode 79.word-search.py

---
+++ python/79.word-search.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=79 lang=python
+#
+# [79] Word Search
+#
+# https://leetcode.com/problems/word-search/description/
+#
+# algorithms
+# Medium (32.02%)
+# Total Accepted:    313.6K
+# Total Submissions: 978.1K
+# Testcase Example:  '[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]\n"ABCCED"'
+#
+# Given a 2D board and a word, find if the word exists in the grid.
+# 
+# The word can be constructed from letters of sequentially adjacent cell, where
+# "adjacent" cells are those horizontally or vertically neighboring. The same
+# letter cell may not be used more than once.
+# 
+# Example:
+# 
+# 
+# board =
+# [
+# ⁠ ['A','B','C','E'],
+# ⁠ ['S','F','C','S'],
+# ⁠ ['A','D','E','E']
+# ]
+# 
+# Given word = "ABCCED", return true.
+# Given word = "SEE", return true.
+# Given word = "ABCB", return false.
+# 
+# 
+#
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        
