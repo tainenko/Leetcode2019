9aadadc3edf177a63d28a20aa691ebcb3d9a3e58
ko tainen <tainenko@gmail.com>
2019-08-19 21:51:52 +0800

    Add solution of leetcode 80.remove-duplicates-from-sorted-array-ii.py

--- python/80.remove-duplicates-from-sorted-array-ii.py
+++ python/80.remove-duplicates-from-sorted-array-ii.py
@@ -66,4 +66,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if len(nums) < 3:
+            return len(nums)
+        j = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+        return j
