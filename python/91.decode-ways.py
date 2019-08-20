97757aeebee634ddc9e0340cd945bd84e3d975ca
ko tainen <tainenko@gmail.com>
2019-08-20 18:01:23 +0800

    add solution of leetcode 91.decode-ways.py

--- python/91.decode-ways.py
+++ python/91.decode-ways.py
@@ -47,4 +47,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+        dp=[0]*(len(s)+1)
+        dp[0]=1
+        for i in range(1,len(s)+1):
+            if s[i-1]!='0':
+                dp[i]=dp[i-1]
+            if i>1 and (s[i-2]=='1' or (s[i-2]=='2' and s[i-1]<='6')):
+                dp[i]+=dp[i-2]
+
+        return dp[-1]
+
+
+
+
