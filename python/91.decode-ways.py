01678f5b463bed66b88d400c7f008df1862d966e
ko tainen <tainenko@gmail.com>
2019-08-20 17:08:11 +0800

    init problem of leetcode 91.decode-ways.py

---
+++ python/91.decode-ways.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode id=91 lang=python
+#
+# [91] Decode Ways
+#
+# https://leetcode.com/problems/decode-ways/description/
+#
+# algorithms
+# Medium (22.70%)
+# Total Accepted:    282.7K
+# Total Submissions: 1.2M
+# Testcase Example:  '"12"'
+#
+# A message containing letters from A-Z is being encoded to numbers using the
+# following mapping:
+# 
+# 
+# 'A' -> 1
+# 'B' -> 2
+# ...
+# 'Z' -> 26
+# 
+# 
+# Given a non-empty string containing only digits, determine the total number
+# of ways to decode it.
+# 
+# Example 1:
+# 
+# 
+# Input: "12"
+# Output: 2
+# Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: "226"
+# Output: 3
+# Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2
+# 6).
+# 
+#
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
