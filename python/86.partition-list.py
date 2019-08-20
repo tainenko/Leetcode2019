c667576e9928d8d2c5ff869fbe67db75c493a8b9
ko tainen <tainenko@gmail.com>
2019-08-20 12:20:01 +0800

    Init problem of leetcode 86.partition-list.py

---
+++ python/86.partition-list.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=86 lang=python
+#
+# [86] Partition List
+#
+# https://leetcode.com/problems/partition-list/description/
+#
+# algorithms
+# Medium (38.06%)
+# Total Accepted:    174.3K
+# Total Submissions: 457.3K
+# Testcase Example:  '[1,4,3,2,5,2]\n3'
+#
+# Given a linked list and a value x, partition it such that all nodes less than
+# x come before nodes greater than or equal to x.
+# 
+# You should preserve the original relative order of the nodes in each of the
+# two partitions.
+# 
+# Example:
+# 
+# 
+# Input: head = 1->4->3->2->5->2, x = 3
+# Output: 1->2->2->4->3->5
+# 
+# 
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        
