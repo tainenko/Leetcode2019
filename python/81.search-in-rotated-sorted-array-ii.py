743038bddc69d8325ced6db250845bf207ccff85
ko tainen <tainenko@gmail.com>
2019-08-20 12:18:47 +0800

    Add solution of leetcode 81.search-in-rotated=sorted-array-ii.py

--- python/81.search-in-rotated-sorted-array-ii.py
+++ python/81.search-in-rotated-sorted-array-ii.py
@@ -48,4 +48,25 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            while left < right and nums[left] == nums[right]:
+                left += 1
+            mid = (left + right) / 2
+
+            if nums[mid] == target:
+                return True
+
+            if nums[mid] >= nums[left]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            elif nums[mid] <= nums[right]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return False
