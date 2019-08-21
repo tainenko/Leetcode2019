b6f964d5f5703ef2865537aa7ffc76ff7fe39b59
ko tainen <tainenko@gmail.com>
2019-08-21 11:07:08 +0800

    Add solution of leetcode 98.validate-binary-search-tree.py

---
+++ python/98.validate-binary-search-tree.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode id=98 lang=python
+#
+# [98] Validate Binary Search Tree
+#
+# https://leetcode.com/problems/validate-binary-search-tree/description/
+#
+# algorithms
+# Medium (26.10%)
+# Total Accepted:    449.2K
+# Total Submissions: 1.7M
+# Testcase Example:  '[2,1,3]'
+#
+# Given a binary tree, determine if it is a valid binary search tree (BST).
+# 
+# Assume a BST is defined as follows:
+# 
+# 
+# The left subtree of a node contains only nodes with keys less than the node's
+# key.
+# The right subtree of a node contains only nodes with keys greater than the
+# node's key.
+# Both the left and right subtrees must also be binary search trees.
+# 
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# ⁠   2
+# ⁠  / \
+# ⁠ 1   3
+# 
+# Input: [2,1,3]
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# ⁠   5
+# ⁠  / \
+# ⁠ 1   4
+# / \
+# 3   6
+# 
+# Input: [5,1,4,null,null,3,6]
+# Output: false
+# Explanation: The root node's value is 5 but its right child's value is 4.
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
