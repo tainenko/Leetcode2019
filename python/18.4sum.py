71f219d36bdd01f816a34f7e13cd21acca37e947
ko tainen <tainenko@gmail.com>
2019-08-21 19:34:18 +0800

    Add solution of leetcode 18.4sum.py

--- python/18.4sum.py
+++ python/18.4sum.py
@@ -40,4 +40,27 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            arr = self.threeSum(nums[i + 1:], target - nums[i])
+            for ele in arr:
+                if [nums[i]] + ele not in res:
+                    res.append([nums[i]] + ele)
+        return res
+
+    def threeSum(self, nums, target):
+        N = len(nums)
+        res = []
+        for i in range(N):
+            left = i + 1
+            right = N - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == target:
+                    res.append([nums[i], nums[left], nums[right]])
+                if total < target:
+                    left += 1
+                else:
+                    right -= 1
+        return res
