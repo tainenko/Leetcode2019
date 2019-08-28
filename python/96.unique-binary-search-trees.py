fc36c624e843f9b3ab3a9c06feed121d23dcd1ce
ko tainen <tainenko@gmail.com>
2019-08-28 18:07:59 +0800

    Add solution of leetcode 96.unique-binary-search-trees.py

--- python/96.unique-binary-search-trees.py
+++ python/96.unique-binary-search-trees.py
@@ -36,4 +36,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
+        for i in range(2, n + 1):
+            for j in range(i):
+                dp[i] += dp[j] * dp[i - j - 1]
+        return dp[-1]
