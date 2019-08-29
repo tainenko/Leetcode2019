492ef8a6156e8599e1ac0ed622b76586740643f1
ko tainen <tainenko@gmail.com>
2019-08-29 17:59:01 +0800

    init problem of leetcode 103.binary-tree-zigzag-lever-order-traversal.py

---
+++ python/103.binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode id=103 lang=python
+#
+# [103] Binary Tree Zigzag Level Order Traversal
+#
+# https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+#
+# algorithms
+# Medium (42.74%)
+# Total Accepted:    245.2K
+# Total Submissions: 570.8K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, return the zigzag level order traversal of its nodes'
+# values. (ie, from left to right, then right to left for the next level and
+# alternate between).
+# 
+# 
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# 
+# 
+# return its zigzag level order traversal as:
+# 
+# [
+# ⁠ [3],
+# ⁠ [20,9],
+# ⁠ [15,7]
+# ]
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        
