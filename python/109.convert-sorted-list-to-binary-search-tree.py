b71a3602325d789f64782ffd58113ad5f60dda0d
ko tainen <tainenko@gmail.com>
2019-08-29 21:55:56 +0800

    init problem of leetcode 109.convert-sorted-list-to-binary-search-tree.py

---
+++ python/109.convert-sorted-list-to-binary-search-tree.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=109 lang=python
+#
+# [109] Convert Sorted List to Binary Search Tree
+#
+# https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/description/
+#
+# algorithms
+# Medium (41.93%)
+# Total Accepted:    189.2K
+# Total Submissions: 448.6K
+# Testcase Example:  '[-10,-3,0,5,9]'
+#
+# Given a singly linked list where elements are sorted in ascending order,
+# convert it to a height balanced BST.
+# 
+# For this problem, a height-balanced binary tree is defined as a binary tree
+# in which the depth of the two subtrees of every node never differ by more
+# than 1.
+# 
+# Example:
+# 
+# 
+# Given the sorted linked list: [-10,-3,0,5,9],
+# 
+# One possible answer is: [0,-3,9,-10,null,5], which represents the following
+# height balanced BST:
+# 
+# ⁠     0
+# ⁠    / \
+# ⁠  -3   9
+# ⁠  /   /
+# ⁠-10  5
+# 
+# 
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        
