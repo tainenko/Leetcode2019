c0aa26eb6298239e7c42f17c66498b1996afb0bf
ko tainen <tainenko@gmail.com>
2019-09-02 22:50:22 +0800

    add solution of leetcode 129.sum-root-to-leaf-numbers.py

--- python/129.sum-root-to-leaf-numbers.py
+++ python/129.sum-root-to-leaf-numbers.py
@@ -63,6 +63,20 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        def helper(root,total):
+            if not root:
+                return 0
+            total=total*10+root.val
+            if not root.left and not root.right:
+                return total
+            return helper(root.left,total)+helper(root.right,total)
+        return helper(root,0)
+
+    def bfs(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
         if not root:
             return 0
         total = 0
