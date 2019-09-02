7fb2c59eb1602e9c408e60b3c636a2fa998884f4
ko tainen <tainenko@gmail.com>
2019-09-02 22:27:54 +0800

    Add solution of leetcode 129.sum-=root-to-leaf-numbers.py

--- python/129.sum-root-to-leaf-numbers.py
+++ python/129.sum-root-to-leaf-numbers.py
@@ -3,7 +3,7 @@
 #
 # [129] Sum Root to Leaf Numbers
 #
-# https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+# https://leetcode.com/problems/total-root-to-leaf-numbers/description/
 #
 # algorithms
 # Medium (43.35%)
@@ -16,7 +16,7 @@
 # 
 # An example is the root-to-leaf path 1->2->3 which represents the number 123.
 # 
-# Find the total sum of all root-to-leaf numbers.
+# Find the total total of all root-to-leaf numbers.
 # 
 # Note: A leaf is a node with no children.
 # 
@@ -31,7 +31,7 @@
 # Explanation:
 # The root-to-leaf path 1->2 represents the number 12.
 # The root-to-leaf path 1->3 represents the number 13.
-# Therefore, sum = 12 + 13 = 25.
+# Therefore, total = 12 + 13 = 25.
 # 
 # Example 2:
 # 
@@ -47,7 +47,7 @@
 # The root-to-leaf path 4->9->5 represents the number 495.
 # The root-to-leaf path 4->9->1 represents the number 491.
 # The root-to-leaf path 4->0 represents the number 40.
-# Therefore, sum = 495 + 491 + 40 = 1026.
+# Therefore, total = 495 + 491 + 40 = 1026.
 # 
 #
 # Definition for a binary tree node.
@@ -63,4 +63,16 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        
+        if not root:
+            return 0
+        total = 0
+        q = [(root, root.val)]
+        while q:
+            curr, tmp = q.pop(0)
+            if not curr.left and not curr.right:
+                total += tmp
+            if curr.left:
+                q.append((curr.left, tmp * 10 + curr.left.val))
+            if curr.right:
+                q.append((curr.right, tmp * 10 + curr.right.val))
+        return total
