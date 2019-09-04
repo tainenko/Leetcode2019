45aa097460af0435862085105cad470ab6475b27
ko tainen <tainenko@gmail.com>
2019-09-04 21:17:38 +0800

    init problem of leetcode 137.single-number-ii.py

---
+++ python/137.single-number-ii.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=137 lang=python
+#
+# [137] Single Number II
+#
+# https://leetcode.com/problems/single-number-ii/description/
+#
+# algorithms
+# Medium (46.71%)
+# Total Accepted:    177.9K
+# Total Submissions: 378.8K
+# Testcase Example:  '[2,2,3,2]'
+#
+# Given a non-empty array of integers, every element appears three times except
+# for one, which appears exactly once. Find that single one.
+# 
+# Note:
+# 
+# Your algorithm should have a linear runtime complexity. Could you implement
+# it without using extra memory?
+# 
+# Example 1:
+# 
+# 
+# Input: [2,2,3,2]
+# Output: 3
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [0,1,0,1,0,1,99]
+# Output: 99
+# 
+#
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
