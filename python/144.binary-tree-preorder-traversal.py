666ac9baf592c2cdd8feda2ec3f86a06dc6730ed
ko tainen <tainenko@gmail.com>
2019-09-09 17:31:33 +0800

    init problem of leetcode 144.binary-tree-preorder0traversal.py

---
+++ python/144.binary-tree-preorder-traversal.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=144 lang=python
+#
+# [144] Binary Tree Preorder Traversal
+#
+# https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+#
+# algorithms
+# Medium (52.21%)
+# Total Accepted:    373.2K
+# Total Submissions: 711.2K
+# Testcase Example:  '[1,null,2,3]'
+#
+# Given a binary tree, return the preorder traversal of its nodes' values.
+# 
+# Example:
+# 
+# 
+# Input: [1,null,2,3]
+# ⁠  1
+# ⁠   \
+# ⁠    2
+# ⁠   /
+# ⁠  3
+# 
+# Output: [1,2,3]
+# 
+# 
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
