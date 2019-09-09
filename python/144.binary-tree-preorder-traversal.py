8dedb3b565832cb14f6f7f14024299dd4b112d9b
ko tainen <tainenko@gmail.com>
2019-09-09 17:51:30 +0800

    Add solution of leetcode 144.binary-tree-preorder-traversal.py

--- python/144.binary-tree-preorder-traversal.py
+++ python/144.binary-tree-preorder-traversal.py
@@ -42,4 +42,32 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
-        
+        if not root:
+            return []
+        res = []
+        stack = []
+        stack.append(root)
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
+
+    def dfs(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        self.helper(root, res)
+        return res
+
+    def helper(self, root, res):
+        if not root:
+            return
+        res.append(root.val)
+        self.helper(root.left, res)
+        self.helper(root.right, res)
