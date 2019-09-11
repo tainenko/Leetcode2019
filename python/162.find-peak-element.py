d74b01f681c475e57757486e9e8bd7a93ae278fb
ko tainen <tainenko@gmail.com>
2019-09-11 18:04:41 +0800

    Add solution of leetcode 162.find-peak-element.py

--- python/162.find-peak-element.py
+++ python/162.find-peak-element.py
@@ -50,4 +50,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return right
