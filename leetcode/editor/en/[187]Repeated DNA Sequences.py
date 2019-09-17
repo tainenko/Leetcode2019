f6628b8ead15aa3b7db6a38ca22104776acbe98f
ko tainen <tainenko@gmail.com>
2019-09-17 13:44:59 +0800

    Add solutin of leetcode 187.repeated dna sequences.py

--- leetcode/editor/en/[187]Repeated DNA Sequences.py
+++ leetcode/editor/en/[187]Repeated DNA Sequences.py
@@ -1,24 +1,31 @@
-#All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA. 
+# All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
 #
 # Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. 
 #
 # Example: 
 #
 # 
-#Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+# Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
 #
-#Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+# Output: ["AAAAACCCCC", "CCCCCAAAAA"]
 # 
 # Related Topics Hash Table Bit Manipulation
 
 
-
-#leetcode submit region begin(Prohibit modification and deletion)
+# leetcode submit region begin(Prohibit modification and deletion)
 class Solution(object):
     def findRepeatedDnaSequences(self, s):
         """
         :type s: str
         :rtype: List[str]
         """
-        
-#leetcode submit region end(Prohibit modification and deletion)
+        seen = set()
+        repeated = set()
+        for i in range(0, len(s) - 9):
+            curr = s[i:i + 10]
+            if curr in seen:
+                repeated.add(curr)
+            seen.add(curr)
+        return list(repeated)
+
+# leetcode submit region end(Prohibit modification and deletion)
