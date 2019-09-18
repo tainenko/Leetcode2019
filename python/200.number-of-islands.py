5eb96fa187978ab3850365a88a124cebebdb18ac
ko tainen <tainenko@gmail.com>
2019-09-18 11:36:23 +0800

    init problem of leetcode 200.number-of-islands.py

---
+++ python/200.number-of-islands.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=200 lang=python
+#
+# [200] Number of Islands
+#
+# https://leetcode.com/problems/number-of-islands/description/
+#
+# algorithms
+# Medium (42.44%)
+# Total Accepted:    424.4K
+# Total Submissions: 990.1K
+# Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+#
+# Given a 2d grid map of '1's (land) and '0's (water), count the number of
+# islands. An island is surrounded by water and is formed by connecting
+# adjacent lands horizontally or vertically. You may assume all four edges of
+# the grid are all surrounded by water.
+# 
+# Example 1:
+# 
+# 
+# Input:
+# 11110
+# 11010
+# 11000
+# 00000
+# 
+# Output: 1
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:
+# 11000
+# 11000
+# 00100
+# 00011
+# 
+# Output: 3
+# 
+#
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        
