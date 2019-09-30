4113d643df0d25b654c88151dd4d8cd293407a7f
ko tainen <TonyK@hyvesolutions.com>
2019-10-01 00:58:30 +0800

    init problem of leetcode 207.course-schedule.py

---
+++ python/207.course-schedule.py
@@ -0,0 +1,62 @@
+#
+# @lc app=leetcode id=207 lang=python
+#
+# [207] Course Schedule
+#
+# https://leetcode.com/problems/course-schedule/description/
+#
+# algorithms
+# Medium (38.80%)
+# Likes:    2237
+# Dislikes: 106
+# Total Accepted:    262.3K
+# Total Submissions: 665.6K
+# Testcase Example:  '2\n[[1,0]]'
+#
+# There are a total of n courses you have to take, labeled from 0 to n-1.
+# 
+# Some courses may have prerequisites, for example to take course 0 you have to
+# first take course 1, which is expressed as a pair: [0,1]
+# 
+# Given the total number of courses and a list of prerequisite pairs, is it
+# possible for you to finish all courses?
+# 
+# Example 1:
+# 
+# 
+# Input: 2, [[1,0]] 
+# Output: true
+# Explanation: There are a total of 2 courses to take. 
+# To take course 1 you should have finished course 0. So it is possible.
+# 
+# Example 2:
+# 
+# 
+# Input: 2, [[1,0],[0,1]]
+# Output: false
+# Explanation: There are a total of 2 courses to take. 
+# To take course 1 you should have finished course 0, and to take course 0 you
+# should
+# also have finished course 1. So it is impossible.
+# 
+# 
+# Note:
+# 
+# 
+# The input prerequisites is a graph represented by a list of edges, not
+# adjacency matrices. Read more about how a graph is represented.
+# You may assume that there are no duplicate edges in the input prerequisites.
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        
+# @lc code=end
