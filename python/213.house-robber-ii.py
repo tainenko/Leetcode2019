a1451069fcae0c8e865d5e225d09862781bd6b12
ko tainen <TonyK@hyvesolutions.com>
2019-10-02 20:36:52 +0800

    Add solution of leetcode 213.house-robber.ii.py

--- python/213.house-robber-ii.py
+++ python/213.house-robber-ii.py
@@ -52,5 +52,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        return max(self.dp(nums[:-1]), self.dp(nums[1:]))
+
+    def dp(self, nums):
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+        return dp[-1]
+
 # @lc code=end
