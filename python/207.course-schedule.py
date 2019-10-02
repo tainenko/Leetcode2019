c0d52f18f57f2be105111e99358da448794308a8
ko tainen <TonyK@hyvesolutions.com>
2019-10-02 11:14:15 +0800

    Add dfs solution of leetcode 207.course-schedule.py

--- python/207.course-schedule.py
+++ python/207.course-schedule.py
@@ -58,6 +58,33 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
+        visited = [0] * numCourses
+        graph = collections.defaultdict(list)
+        for u, v in prerequisites:
+            graph[v].append(u)
+        for i in range(numCourses):
+            if not self.dfs(graph, visited, i):
+                return False
+        return True
+
+    def dfs(self, graph, visited, i):
+        if visited[i] == 1:
+            return False
+        if visited[i] == 2:
+            return True
+        visited[i] = 1
+        for j in graph[i]:
+            if not self.dfs(graph, visited, j):
+                return False
+        visited[i] = 2
+        return True
+
+    def canFinish_bfs(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
         graph = collections.defaultdict(list)
         indegree = collections.defaultdict(int)
         for u, v in prerequisites:
