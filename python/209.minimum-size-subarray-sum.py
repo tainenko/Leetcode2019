4e285e4d45d3a912913b25f93898a2fd84b10226
ko tainen <TonyK@hyvesolutions.com>
2019-10-02 17:23:00 +0800

    init problem of leetcode 209.minimun-size-subarray-sum

---
+++ python/209.minimum-size-subarray-sum.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=209 lang=python
+#
+# [209] Minimum Size Subarray Sum
+#
+# https://leetcode.com/problems/minimum-size-subarray-sum/description/
+#
+# algorithms
+# Medium (35.40%)
+# Likes:    1377
+# Dislikes: 83
+# Total Accepted:    198.8K
+# Total Submissions: 557K
+# Testcase Example:  '7\n[2,3,1,2,4,3]'
+#
+# Given an array of n positive integers and a positive integer s, find the
+# minimal length of a contiguous subarray of which the sum ≥ s. If there isn't
+# one, return 0 instead.
+# 
+# Example: 
+# 
+# 
+# Input: s = 7, nums = [2,3,1,2,4,3]
+# Output: 2
+# Explanation: the subarray [4,3] has the minimal length under the problem
+# constraint.
+# 
+# Follow up:
+# 
+# If you have figured out the O(n) solution, try coding another solution of
+# which the time complexity is O(n log n). 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def minSubArrayLen(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+# @lc code=end
