441b303f8cdc3a4153143c15a261751c21714317
ko tainen <TonyK@hyvesolutions.com>
2019-10-04 12:17:34 +0800

    init problem of leetcode 221.maximal-square.py

---
+++ python/221.maximal-square.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=221 lang=python
+#
+# [221] Maximal Square
+#
+# https://leetcode.com/problems/maximal-square/description/
+#
+# algorithms
+# Medium (33.75%)
+# Likes:    1647
+# Dislikes: 41
+# Total Accepted:    153.8K
+# Total Submissions: 449.3K
+# Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
+#
+# Given a 2D binary matrix filled with 0's and 1's, find the largest square
+# containing only 1's and return its area.
+# 
+# Example:
+# 
+# 
+# Input: 
+# 
+# 1 0 1 0 0
+# 1 0 1 1 1
+# 1 1 1 1 1
+# 1 0 0 1 0
+# 
+# Output: 4
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        
+# @lc code=end
