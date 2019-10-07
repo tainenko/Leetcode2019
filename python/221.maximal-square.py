660275c6d9a22ef59c3e527681388bf8e9dffb7f
ko tainen <TonyK@hyvesolutions.com>
2019-10-07 10:54:36 +0800

    add solution of leetcode 221.maximal-square.py

--- python/221.maximal-square.py
+++ python/221.maximal-square.py
@@ -24,6 +24,7 @@
 # 1 0 1 0 0
 # 1 0 1 1 1
 # 1 1 1 1 1
+
 # 1 0 0 1 0
 # 
 # Output: 4
@@ -37,5 +38,23 @@ class Solution(object):
         :type matrix: List[List[str]]
         :rtype: int
         """
-        
+        if not matrix:
+            return 0
+
+        n = len(matrix)
+        m = len(matrix[0])
+        dp = [[0] * m for _ in range(n)]
+        for j in range(m):
+            dp[0][j] = int(matrix[0][j])
+        for i in range(n):
+            dp[i][0] = int(matrix[i][0])
+
+        for i in range(1, n):
+            for j in range(1, m):
+                if matrix[i][j] == '0':
+                    continue
+                elif matrix[i][j] == '1':
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+        return max(map(max, dp)) ** 2
+
 # @lc code=end
