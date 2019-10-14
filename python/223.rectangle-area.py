f6345fc8616b6a6e6f55689571b2f76a7318c60d
ko tainen <TonyK@hyvesolutions.com>
2019-10-14 14:46:31 +0800

    init problem of leetcode 223.rectangle-are.pyp

---
+++ python/223.rectangle-area.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode id=223 lang=python
+#
+# [223] Rectangle Area
+#
+# https://leetcode.com/problems/rectangle-area/description/
+#
+# algorithms
+# Medium (36.20%)
+# Likes:    289
+# Dislikes: 562
+# Total Accepted:    93.8K
+# Total Submissions: 257.1K
+# Testcase Example:  '-3\n0\n3\n4\n0\n-1\n9\n2'
+#
+# Find the total area covered by two rectilinear rectangles in a 2D plane.
+# 
+# Each rectangle is defined by its bottom left corner and top right corner as
+# shown in the figure.
+# 
+# 
+# 
+# Example:
+# 
+# 
+# Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
+# Output: 45
+# 
+# Note:
+# 
+# Assume that the total area is never beyond the maximum possible value of int.
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """
+        
+# @lc code=end
