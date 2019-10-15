e0a7d1b39fc03e3934ed01b6989891833fba1b80
ko tainen <TonyK@hyvesolutions.com>
2019-10-15 17:45:38 +0800

    init problem of leetcode 228.summary-ranges.py

---
+++ python/228.summary-ranges.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=228 lang=python
+#
+# [228] Summary Ranges
+#
+# https://leetcode.com/problems/summary-ranges/description/
+#
+# algorithms
+# Medium (36.57%)
+# Likes:    438
+# Dislikes: 428
+# Total Accepted:    142.9K
+# Total Submissions: 385.3K
+# Testcase Example:  '[0,1,2,4,5,7]'
+#
+# Given a sorted integer array without duplicates, return the summary of its
+# ranges.
+# 
+# Example 1:
+# 
+# 
+# Input:  [0,1,2,4,5,7]
+# Output: ["0->2","4->5","7"]
+# Explanation: 0,1,2 form a continuous range; 4,5 form a continuous range.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:  [0,2,3,4,6,8,9]
+# Output: ["0","2->4","6","8->9"]
+# Explanation: 2,3,4 form a continuous range; 8,9 form a continuous range.
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        
+# @lc code=end
