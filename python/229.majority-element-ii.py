fa72bda2638be734209f7c9a1fc4ddff8b574394
ko tainen <TonyK@hyvesolutions.com>
2019-10-16 11:31:40 +0800

    init problem of leetcode 229.majority-element-ii

---
+++ python/229.majority-element-ii.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=229 lang=python
+#
+# [229] Majority Element II
+#
+# https://leetcode.com/problems/majority-element-ii/description/
+#
+# algorithms
+# Medium (32.78%)
+# Likes:    1061
+# Dislikes: 124
+# Total Accepted:    115K
+# Total Submissions: 345.1K
+# Testcase Example:  '[3,2,3]'
+#
+# Given an integer array of size n, find all elements that appear more than ⌊
+# n/3 ⌋ times.
+# 
+# Note: The algorithm should run in linear time and in O(1) space.
+# 
+# Example 1:
+# 
+# 
+# Input: [3,2,3]
+# Output: [3]
+# 
+# Example 2:
+# 
+# 
+# Input: [1,1,1,3,3,2,2,2]
+# Output: [1,2]
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+# @lc code=end
