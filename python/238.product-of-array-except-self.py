fbbf5d8daed75aa5ef7312b5450084df75fcebe1
ko tainen <TonyK@hyvesolutions.com>
2019-11-04 13:41:16 +0800

    init problem of leetcode 238.product-of-array-except-self

---
+++ python/238.product-of-array-except-self.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=238 lang=python
+#
+# [238] Product of Array Except Self
+#
+# https://leetcode.com/problems/product-of-array-except-self/description/
+#
+# algorithms
+# Medium (56.04%)
+# Likes:    2995
+# Dislikes: 259
+# Total Accepted:    331.5K
+# Total Submissions: 578.7K
+# Testcase Example:  '[1,2,3,4]'
+#
+# Given an array nums of n integers where n > 1,  return an array output such
+# that output[i] is equal to the product of all the elements of nums except
+# nums[i].
+# 
+# Example:
+# 
+# 
+# Input:  [1,2,3,4]
+# Output: [24,12,8,6]
+# 
+# 
+# Note: Please solve it without division and in O(n).
+# 
+# Follow up:
+# Could you solve it with constant space complexity? (The output array does not
+# count as extra space for the purpose of space complexity analysis.)
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+# @lc code=end
