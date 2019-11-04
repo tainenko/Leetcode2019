2a433847974b2f5e81a61a76d7cf943ba08e9c4d
ko tainen <TonyK@hyvesolutions.com>
2019-11-04 11:26:32 +0800

    Init problem of 236.lowest-common-ancestor-of-a-binary-tree.py

---
+++ python/236.lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode id=236 lang=python
+#
+# [236] Lowest Common Ancestor of a Binary Tree
+#
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+#
+# algorithms
+# Medium (38.82%)
+# Likes:    2511
+# Dislikes: 150
+# Total Accepted:    344.7K
+# Total Submissions: 849.1K
+# Testcase Example:  '[3,5,1,6,2,0,8,null,null,7,4]\n5\n1'
+#
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes
+# in the tree.
+# 
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor
+# is defined between two nodes p and q as the lowest node in T that has both p
+# and q as descendants (where we allow a node to be a descendant of itself).”
+# 
+# Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+# Output: 3
+# Explanation: The LCA of nodes 5 and 1 is 3.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+# Output: 5
+# Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant
+# of itself according to the LCA definition.
+# 
+# 
+# 
+# 
+# Note:
+# 
+# 
+# All of the nodes' values will be unique.
+# p and q are different and both values will exist in the binary tree.
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        
+# @lc code=end
