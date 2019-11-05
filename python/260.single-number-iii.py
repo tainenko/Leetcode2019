76922aaea46d6cfb08aac6a547e3f9e5063438c1
ko tainen <TonyK@hyvesolutions.com>
2019-11-05 22:17:29 +0800

    init problem of leetcode 260.single-number-iii.py

---
+++ python/260.single-number-iii.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=260 lang=python
+#
+# [260] Single Number III
+#
+# https://leetcode.com/problems/single-number-iii/description/
+#
+# algorithms
+# Medium (57.80%)
+# Likes:    1043
+# Dislikes: 84
+# Total Accepted:    118.7K
+# Total Submissions: 202.5K
+# Testcase Example:  '[1,2,1,3,2,5]'
+#
+# Given an array of numbers nums, in which exactly two elements appear only
+# once and all the other elements appear exactly twice. Find the two elements
+# that appear only once.
+# 
+# Example:
+# 
+# 
+# Input:  [1,2,1,3,2,5]
+# Output: [3,5]
+# 
+# Note:
+# 
+# 
+# The order of the result is not important. So in the above example, [5, 3] is
+# also correct.
+# Your algorithm should run in linear runtime complexity. Could you implement
+# it using only constant space complexity?
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+# @lc code=end
