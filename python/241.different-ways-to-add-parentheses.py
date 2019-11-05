e57adcb9d35fbec4650fe347496ee12692cd9ec4
ko tainen <TonyK@hyvesolutions.com>
2019-11-05 22:11:21 +0800

    Add solution of leetcode 241.different-ways-to-add-parentheses.py

--- python/241.different-ways-to-add-parentheses.py
+++ python/241.different-ways-to-add-parentheses.py
@@ -47,5 +47,22 @@ class Solution(object):
         :type input: str
         :rtype: List[int]
         """
-        
+        res = []
+        N = len(input)
+        for i in range(N):
+            if input[i] == "+" or input[i] == "-" or input[i] == "*":
+                lefts = self.diffWaysToCompute(input[:i])
+                rights = self.diffWaysToCompute((input[i + 1:]))
+                for left in lefts:
+                    for right in rights:
+                        if input[i] == '+':
+                            res.append(left + right)
+                        elif input[i] == '-':
+                            res.append(left - right)
+                        elif input[i] == '*':
+                            res.append(left * right)
+        if not res:
+            res.append(int(input))
+        return res
+
 # @lc code=end
