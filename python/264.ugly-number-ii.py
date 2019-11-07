3788e0b7d58de1d3aa919bde0091a0ea557daeec
ko tainen <TonyK@hyvesolutions.com>
2019-11-07 13:13:15 +0800

    init problem of leetcode 264.ugly-number-ii.py

---
+++ python/264.ugly-number-ii.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=264 lang=python
+#
+# [264] Ugly Number II
+#
+# https://leetcode.com/problems/ugly-number-ii/description/
+#
+# algorithms
+# Medium (37.08%)
+# Likes:    1182
+# Dislikes: 74
+# Total Accepted:    120.6K
+# Total Submissions: 318.4K
+# Testcase Example:  '10'
+#
+# Write a program to find the n-th ugly number.
+# 
+# Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
+# 
+# Example:
+# 
+# 
+# Input: n = 10
+# Output: 12
+# Explanation: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10
+# ugly numbers.
+# 
+# Note:  
+# 
+# 
+# 1 is typically treated as an ugly number.
+# n does not exceed 1690.
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+# @lc code=end
