123aaa2ad2ce946069358394c0826e558cecc3c5
ko tainen <TonyK@hyvesolutions.com>
2019-11-07 14:12:16 +0800

    Add solution of leetcode 264.ugly-number-ii.py

--- python/264.ugly-number-ii.py
+++ python/264.ugly-number-ii.py
@@ -40,5 +40,21 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        if n <= 1:
+            return n
+        t2 = 0
+        t3 = 0
+        t5 = 0
+        res = [1]
+        for i in range(n - 1):
+            tmp = min(res[t2] * 2, res[t3] * 3, res[t5] * 5)
+            res.append(tmp)
+            if res[-1] == res[t2] * 2:
+                t2 += 1
+            if res[-1] == res[t3] * 3:
+                t3 += 1
+            if res[-1] == res[t5] * 5:
+                t5 += 1
+        return res[-1]
+
 # @lc code=end
