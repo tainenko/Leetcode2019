3cee40e3a7d4c09901b772f1725be263451df0dd
ko tainen <TonyK@hyvesolutions.com>
2019-11-11 14:10:33 +0800

    init problem of leetcode 289.game-of-life.py

---
+++ python/289.game-of-life.py
@@ -0,0 +1,81 @@
+#
+# @lc app=leetcode id=289 lang=python
+#
+# [289] Game of Life
+#
+# https://leetcode.com/problems/game-of-life/description/
+#
+# algorithms
+# Medium (47.00%)
+# Likes:    1216
+# Dislikes: 223
+# Total Accepted:    139.9K
+# Total Submissions: 284.6K
+# Testcase Example:  '[[0,1,0],[0,0,1],[1,1,1],[0,0,0]]'
+#
+# According to the Wikipedia's article: "The Game of Life, also known simply as
+# Life, is a cellular automaton devised by the British mathematician John
+# Horton Conway in 1970."
+# 
+# Given a board with m by n cells, each cell has an initial state live (1) or
+# dead (0). Each cell interacts with its eight neighbors (horizontal, vertical,
+# diagonal) using the following four rules (taken from the above Wikipedia
+# article):
+# 
+# 
+# Any live cell with fewer than two live neighbors dies, as if caused by
+# under-population.
+# Any live cell with two or three live neighbors lives on to the next
+# generation.
+# Any live cell with more than three live neighbors dies, as if by
+# over-population..
+# Any dead cell with exactly three live neighbors becomes a live cell, as if by
+# reproduction.
+# 
+# 
+# Write a function to compute the next state (after one update) of the board
+# given its current state. The next state is created by applying the above
+# rules simultaneously to every cell in the current state, where births and
+# deaths occur simultaneously.
+# 
+# Example:
+# 
+# 
+# Input: 
+# [
+# [0,1,0],
+# [0,0,1],
+# [1,1,1],
+# [0,0,0]
+# ]
+# Output: 
+# [
+# [0,0,0],
+# [1,0,1],
+# [0,1,1],
+# [0,1,0]
+# ]
+# 
+# 
+# Follow up:
+# 
+# 
+# Could you solve it in-place? Remember that the board needs to be updated at
+# the same time: You cannot update some cells first and then use their updated
+# values to update other cells.
+# In this question, we represent the board using a 2D array. In principle, the
+# board is infinite, which would cause problems when the active area encroaches
+# the border of the array. How would you address these problems?
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def gameOfLife(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        
+# @lc code=end
