9d68f00fc8b8fd9dc3ad7e7d07de574b03415626
ko tainen <TonyK@hyvesolutions.com>
2019-11-11 11:41:21 +0800

    Add solution of leetcode 275.h-index-ii.py

--- python/275.h-index-ii.py
+++ python/275.h-index-ii.py
@@ -55,5 +55,19 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        
+
+        left = 0
+        right = len(citations) - 1
+        n = len(citations)
+        while left <= right:
+            mid = left + (right - left) // 2
+            if citations[mid] == n - mid:
+                return n - mid
+            elif citations[mid] > n - mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return n - left
+
 # @lc code=end
