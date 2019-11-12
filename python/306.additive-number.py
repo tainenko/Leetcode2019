6b594c4ce18a8afca7a580cb3bda06d2b942582d
ko tainen <TonyK@hyvesolutions.com>
2019-11-12 11:05:08 +0800

    add solution of leetcode 306.additive-number.py

--- python/306.additive-number.py
+++ python/306.additive-number.py
@@ -65,5 +65,20 @@ class Solution(object):
         :type num: str
         :rtype: bool
         """
-        
+        return self.dfs(num, [])
+
+    def dfs(self, num, path):
+        if len(path) >= 3 and path[-1] != path[-2] + path[-3]:
+            return False
+        if not num and len(path) >= 3:
+            return True
+
+        for i in range(len(num)):
+            curr = num[:i + 1]
+            if curr[0] == '0' and len(curr) > 1:
+                continue
+            if self.dfs(num[i + 1:], path + [int(curr)]):
+                return True
+        return False
+
 # @lc code=end
