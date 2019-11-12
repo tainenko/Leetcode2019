14f7df1e0566d4506570e47568358ec7ce3b4958
ko tainen <TonyK@hyvesolutions.com>
2019-11-12 11:17:04 +0800

    init problem of leetcode 307.range-sum-query-mutable.py

---
+++ python/307.range-sum-query-mutable.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode id=307 lang=python
+#
+# [307] Range Sum Query - Mutable
+#
+# https://leetcode.com/problems/range-sum-query-mutable/description/
+#
+# algorithms
+# Medium (29.71%)
+# Likes:    896
+# Dislikes: 68
+# Total Accepted:    85.4K
+# Total Submissions: 275.3K
+# Testcase Example:  '["NumArray","sumRange","update","sumRange"]\n[[[1,3,5]],[0,2],[1,2],[0,2]]'
+#
+# Given an integer array nums, find the sum of the elements between indices i
+# and j (i ≤ j), inclusive.
+# 
+# The update(i, val) function modifies nums by updating the element at index i
+# to val.
+# 
+# Example:
+# 
+# 
+# Given nums = [1, 3, 5]
+# 
+# sumRange(0, 2) -> 9
+# update(1, 2)
+# sumRange(0, 2) -> 8
+# 
+# 
+# Note:
+# 
+# 
+# The array is only modifiable by the update function.
+# You may assume the number of calls to update and sumRange function is
+# distributed evenly.
+# 
+# 
+#
+
+# @lc code=start
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        
+
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: None
+        """
+        
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(i,val)
+# param_2 = obj.sumRange(i,j)
+# @lc code=end
