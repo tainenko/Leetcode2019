7e7ee1e08a7d81befe4a2bba35c926c17ff9686b
ko tainen <TonyK@hyvesolutions.com>
2019-11-12 11:09:39 +0800

    init problem of leetcode 304.range-sum-query-2d-immutable.py

---
+++ python/304.range-sum-query-2d-immutable.py
@@ -0,0 +1,76 @@
+#
+# @lc app=leetcode id=304 lang=python
+#
+# [304] Range Sum Query 2D - Immutable
+#
+# https://leetcode.com/problems/range-sum-query-2d-immutable/description/
+#
+# algorithms
+# Medium (33.40%)
+# Likes:    611
+# Dislikes: 149
+# Total Accepted:    87.1K
+# Total Submissions: 249.2K
+# Testcase Example:  '["NumMatrix","sumRegion","sumRegion","sumRegion"]\n' +
+  '[[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]'
+#
+# Given a 2D matrix matrix, find the sum of the elements inside the rectangle
+# defined by its upper left corner (row1, col1) and lower right corner (row2,
+# col2).
+# 
+# 
+# 
+# The above rectangle (with the red border) is defined by (row1, col1) = (2, 1)
+# and (row2, col2) = (4, 3), which contains sum = 8.
+# 
+# 
+# Example:
+# 
+# Given matrix = [
+# ⁠ [3, 0, 1, 4, 2],
+# ⁠ [5, 6, 3, 2, 1],
+# ⁠ [1, 2, 0, 1, 5],
+# ⁠ [4, 1, 0, 1, 7],
+# ⁠ [1, 0, 3, 0, 5]
+# ]
+# 
+# sumRegion(2, 1, 4, 3) -> 8
+# sumRegion(1, 1, 2, 2) -> 11
+# sumRegion(1, 2, 2, 4) -> 12
+# 
+# 
+# 
+# Note:
+# 
+# You may assume that the matrix does not change.
+# There are many calls to sumRegion function.
+# You may assume that row1 ≤ row2 and col1 ≤ col2.
+# 
+# 
+#
+
+# @lc code=start
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+
+        
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+# @lc code=end
