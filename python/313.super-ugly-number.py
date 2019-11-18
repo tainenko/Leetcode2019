7e7d86c40d6f87f913156d0b9719caa949da16d4
ko tainen <TonyK@hyvesolutions.com>
2019-11-18 18:11:21 +0800

    init problem of leetcode 313.super-ugly-number.py

---
+++ python/313.super-ugly-number.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode id=313 lang=python
+#
+# [313] Super Ugly Number
+#
+# https://leetcode.com/problems/super-ugly-number/description/
+#
+# algorithms
+# Medium (42.24%)
+# Likes:    448
+# Dislikes: 113
+# Total Accepted:    67.4K
+# Total Submissions: 156.5K
+# Testcase Example:  '12\n[2,7,13,19]'
+#
+# Write a program to find the n^th super ugly number.
+# 
+# Super ugly numbers are positive numbers whose all prime factors are in the
+# given prime list primes of size k.
+# 
+# Example:
+# 
+# 
+# Input: n = 12, primes = [2,7,13,19]
+# Output: 32 
+# Explanation: [1,2,4,7,8,13,14,16,19,26,28,32] is the sequence of the first
+# 12 
+# ⁠            super ugly numbers given primes = [2,7,13,19] of size 4.
+# 
+# Note:
+# 
+# 
+# 1 is a super ugly number for any given primes.
+# The given numbers in primes are in ascending order.
+# 0 < k ≤ 100, 0 < n ≤ 10^6, 0 < primes[i] < 1000.
+# The n^th super ugly number is guaranteed to fit in a 32-bit signed integer.
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def nthSuperUglyNumber(self, n, primes):
+        """
+        :type n: int
+        :type primes: List[int]
+        :rtype: int
+        """
+        
+# @lc code=end
