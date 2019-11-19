66f2decbb09c257dd1160b426c0e2ad47d1b8f5f
ko tainen <TonyK@hyvesolutions.com>
2019-11-19 17:34:34 +0800

    init problem of leetcode 318.maximum-product-of-word-lengths.py

---
+++ python/318.maximum-product-of-word-lengths.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode id=318 lang=python
+#
+# [318] Maximum Product of Word Lengths
+#
+# https://leetcode.com/problems/maximum-product-of-word-lengths/description/
+#
+# algorithms
+# Medium (48.99%)
+# Likes:    640
+# Dislikes: 52
+# Total Accepted:    87.2K
+# Total Submissions: 175.8K
+# Testcase Example:  '["abcw","baz","foo","bar","xtfn","abcdef"]'
+#
+# Given a string array words, find the maximum value of length(word[i]) *
+# length(word[j]) where the two words do not share common letters. You may
+# assume that each word will contain only lower case letters. If no such two
+# words exist, return 0.
+# 
+# Example 1:
+# 
+# 
+# Input: ["abcw","baz","foo","bar","xtfn","abcdef"]
+# Output: 16 
+# Explanation: The two words can be "abcw", "xtfn".
+# 
+# Example 2:
+# 
+# 
+# Input: ["a","ab","abc","d","cd","bcd","abcd"]
+# Output: 4 
+# Explanation: The two words can be "ab", "cd".
+# 
+# Example 3:
+# 
+# 
+# Input: ["a","aa","aaa","aaaa"]
+# Output: 0 
+# Explanation: No such pair of words.
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def maxProduct(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        
+# @lc code=end
