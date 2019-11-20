08046dd597d5f7f58aa647747c884b8000eecbee
ko tainen <TonyK@hyvesolutions.com>
2019-11-20 12:32:37 +0800

    init problem of leetcode 319.bulb-switcher.py

---
+++ python/319.bulb-switcher.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode id=319 lang=python
+#
+# [319] Bulb Switcher
+#
+# https://leetcode.com/problems/bulb-switcher/description/
+#
+# algorithms
+# Medium (44.21%)
+# Likes:    348
+# Dislikes: 783
+# Total Accepted:    67.4K
+# Total Submissions: 151K
+# Testcase Example:  '3'
+#
+# There are n bulbs that are initially off. You first turn on all the bulbs.
+# Then, you turn off every second bulb. On the third round, you toggle every
+# third bulb (turning on if it's off or turning off if it's on). For the i-th
+# round, you toggle every i bulb. For the n-th round, you only toggle the last
+# bulb. Find how many bulbs are on after n rounds.
+# 
+# Example:
+# 
+# 
+# Input: 3
+# Output: 1 
+# Explanation: 
+# At first, the three bulbs are [off, off, off].
+# After first round, the three bulbs are [on, on, on].
+# After second round, the three bulbs are [on, off, on].
+# After third round, the three bulbs are [on, off, off]. 
+# 
+# So you should return 1, because there is only one bulb is on.
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+# @lc code=end
