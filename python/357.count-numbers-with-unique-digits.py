7aed85abefa8d5eae98a8269551f2c53a3480f00
ko tainen <TonyK@hyvesolutions.com>
2019-12-06 17:09:06 +0800

    init problem of leetcode 357.count-numbers-with-unique-digits.py

---
+++ python/357.count-numbers-with-unique-digits.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=357 lang=python
+#
+# [357] Count Numbers with Unique Digits
+#
+# https://leetcode.com/problems/count-numbers-with-unique-digits/description/
+#
+# algorithms
+# Medium (47.28%)
+# Likes:    304
+# Dislikes: 713
+# Total Accepted:    68.9K
+# Total Submissions: 144.5K
+# Testcase Example:  '2'
+#
+# Given a non-negative integer n, count all numbers with unique digits, x,
+# where 0 ≤ x < 10^n.
+# 
+# 
+# Example:
+# 
+# 
+# Input: 2
+# Output: 91 
+# Explanation: The answer should be the total numbers in the range of 0 ≤ x <
+# 100, 
+# excluding 11,22,33,44,55,66,77,88,99
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+# @lc code=end
