7a8ef4943d021171b98c197e6658a9a13c124bf4
ko tainen <TonyK@hyvesolutions.com>
2019-12-12 12:16:10 +0800

    add solution of leetcode 357.count-numbers-with-unique-digits.py

--- python/357.count-numbers-with-unique-digits.py
+++ python/357.count-numbers-with-unique-digits.py
@@ -36,5 +36,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        nums = [9, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        product = 1
+        res = 1
+        for i in range(min(n, 10)):
+            product *= nums[i]
+            res += product
+        return res
+
 # @lc code=end
