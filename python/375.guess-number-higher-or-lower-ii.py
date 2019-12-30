fa17fc3180f3efc50b54711c3e575cad28b4c09a
ko tainen <TonyK@hyvesolutions.com>
2019-12-30 11:47:02 +0800

    feat(leedcode 375.guess-number-higher-or-lower-ii.py): Add solution of leetcode 375.guess-number-highter-or-lower-ii.py

--- python/375.guess-number-higher-or-lower-ii.py
+++ python/375.guess-number-higher-or-lower-ii.py
@@ -48,5 +48,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        return self.solve(dp, 1, n)
 
+    def solve(self, dp, L, R):
+        if L >= R:
+            return 0
+        if dp[L][R]:
+            return dp[L][R]
+        dp[L][R] = min(i + max(self.solve(dp, L, i - 1), self.solve(dp, i + 1, R)) for i in range(L, R + 1))
+        return dp[L][R]
 # @lc code=end
