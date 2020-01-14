b5a5583fc240b9250df4cd0d02fdfaa59ac90d88
ko tainen <TonyK@hyvesolutions.com>
2020-01-14 15:01:10 +0800

    feat(leetcode 177.nth-highest-salary.sql): Add solution of leetcode 177.nth-highest-salary.sql

--- mysql/177.nth-highest-salary.sql
+++ mysql/177.nth-highest-salary.sql
@@ -42,8 +42,15 @@
 -- @lc code=start
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+  DECLARE M INT;
+  SET M = N - 1;
   RETURN (
       # Write your MySQL query statement below.
+      SELECT SALARY
+      FROM Employee
+      GROUP BY SALARY
+      ORDER BY SALARY DESC
+      LIMIT N-1,1
 
   );
 END
