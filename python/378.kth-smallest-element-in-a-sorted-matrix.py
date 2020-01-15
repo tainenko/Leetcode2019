a3cf68954d77a41935403c474adeccc6af207fb4
ko tainen <TonyK@hyvesolutions.com>
2020-01-15 11:13:14 +0800

    feat(leetcode 378.kth-smallest-element-in-a-matrix.py): Init problem of leetcode 378.kth-smallest-element-in-a-matrix.py

---
+++ python/378.kth-smallest-element-in-a-sorted-matrix.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode id=378 lang=python
+#
+# [378] Kth Smallest Element in a Sorted Matrix
+#
+# https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/description/
+#
+# algorithms
+# Medium (50.33%)
+# Likes:    1704
+# Dislikes: 101
+# Total Accepted:    147K
+# Total Submissions: 283.6K
+# Testcase Example:  '[[1,5,9],[10,11,13],[12,13,15]]\n8'
+#
+# Given a n x n matrix where each of the rows and columns are sorted in
+# ascending order, find the kth smallest element in the matrix.
+# 
+# 
+# Note that it is the kth smallest element in the sorted order, not the kth
+# distinct element.
+# 
+# 
+# Example:
+# 
+# matrix = [
+# ⁠  [ 1,  5,  9],
+# ⁠  [10, 11, 13],
+# ⁠  [12, 13, 15]
+# ],
+# k = 8,
+# 
+# return 13.
+# 
+# 
+# 
+# Note: 
+# You may assume k is always valid, 1 ≤ k ≤ n^2.
+#
+
+# @lc code=start
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+
+# @lc code=end
