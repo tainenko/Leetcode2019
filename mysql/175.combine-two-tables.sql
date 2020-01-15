b415256409732ed2ace4ac1deafe15e1cc7369d0
ko tainen <TonyK@hyvesolutions.com>
2020-01-15 10:19:34 +0800

    feat(leetcode 175.combine-two-tables.sql): Init problem of leetcode 175.combine-two-talbes.sql

---
+++ mysql/175.combine-two-tables.sql
@@ -0,0 +1,54 @@
+--
+-- @lc app=leetcode id=175 lang=mysql
+--
+-- [175] Combine Two Tables
+--
+-- https://leetcode.com/problems/combine-two-tables/description/
+--
+-- database
+-- Easy (53.47%)
+-- Total Accepted:    186K
+-- Total Submissions: 345.3K
+-- Testcase Example:  '{"headers": {"Person": ["PersonId", "LastName", "FirstName"], "Address": ["AddressId", "PersonId", "City", "State"]}, "rows": {"Person": [[1, "Wang", "Allen"]], "Address": [[1, 2, "New York City", "New York"]]}}'
+--
+-- Table: Person
+-- 
+-- 
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- PersonId is the primary key column for this table.
+-- 
+-- 
+-- Table: Address
+-- 
+-- 
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- AddressId is the primary key column for this table.
+-- 
+-- 
+-- 
+-- 
+-- Write a SQL query for a report that provides the following information for
+-- each person in the Person table, regardless if there is an address for each
+-- of those people:
+-- 
+-- 
+-- FirstName, LastName, City, State
+-- 
+-- 
+--
+# Write your MySQL query statement below
+
+
