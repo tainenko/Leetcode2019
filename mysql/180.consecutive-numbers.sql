20379529469d36c0428bfb7420d7b6c4a7a7fa95
ko tainen <TonyK@hyvesolutions.com>
2020-01-16 10:32:22 +0800

    Init problem of leetcode 180.consecutive-numbers.sql

---
+++ mysql/180.consecutive-numbers.sql
@@ -0,0 +1,49 @@
+--
+-- @lc app=leetcode id=180 lang=mysql
+--
+-- [180] Consecutive Numbers
+--
+-- https://leetcode.com/problems/consecutive-numbers/description/
+--
+-- database
+-- Medium (34.66%)
+-- Likes:    324
+-- Dislikes: 84
+-- Total Accepted:    71.9K
+-- Total Submissions: 195.7K
+-- Testcase Example:  '{"headers": {"Logs": ["Id", "Num"]}, "rows": {"Logs": [[1, 1], [2, 1], [3, 1], [4, 2], [5, 1], [6, 2], [7, 2]]}}'
+--
+-- Write a SQL query to find all numbers that appear at least three times
+-- consecutively.
+-- 
+-- 
+-- +----+-----+
+-- | Id | Num |
+-- +----+-----+
+-- | 1  |  1  |
+-- | 2  |  1  |
+-- | 3  |  1  |
+-- | 4  |  2  |
+-- | 5  |  1  |
+-- | 6  |  2  |
+-- | 7  |  2  |
+-- +----+-----+
+-- 
+-- 
+-- For example, given the above Logs table, 1 is the only number that appears
+-- consecutively for at least three times.
+-- 
+-- 
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+-- 
+-- 
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+-- @lc code=end
