8f0d8af317e2fdf9536ddac02833d9f331621773
ko tainen <TonyK@hyvesolutions.com>
2020-01-20 12:11:28 +0800

    Add iterative solution of leetcode 397.integer-replacement.py

--- python/397.integer-replacement.py
+++ python/397.integer-replacement.py
@@ -66,6 +66,27 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # 解題思路
+        # 如果是奇數，把奇數變成四的倍數後變1的步驟會減少，唯有3是例外(3-1後的步驟比3+1更少)
+        # 因此我們首先判斷n是奇數或偶數，如果n是奇數，且末兩位是'11'則加一，其餘的情況直接減一
+        # 為了加速運算，我們可以用binary operation
+        cnt=0
+        while n>1:
+            cnt+=1
+            if n&1:
+                if n&2 and n!=3:
+                    n+=1
+                else:
+                    n-=1
+            else:
+                n>>=1
+        return cnt
+
+    def integerReplacement_recursive(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         return self.helper(n, 0)
 
     def helper(self, n, cnt):
