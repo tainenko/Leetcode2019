8c88547716bcb8776f12a18ae1671b2ac1c1f815
ko tainen <TonyK@hyvesolutions.com>
2020-01-30 13:50:27 +0800

    Init problem of leetcode 445.add-two-numbers-ii.py

---
+++ python/445.add-two-numbers-ii.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode id=445 lang=python
+#
+# [445] Add Two Numbers II
+#
+# https://leetcode.com/problems/add-two-numbers-ii/description/
+#
+# algorithms
+# Medium (50.88%)
+# Likes:    1042
+# Dislikes: 131
+# Total Accepted:    126.4K
+# Total Submissions: 240.5K
+# Testcase Example:  '[7,2,4,3]\n[5,6,4]'
+#
+# You are given two non-empty linked lists representing two non-negative
+# integers. The most significant digit comes first and each of their nodes
+# contain a single digit. Add the two numbers and return it as a linked list.
+# 
+# You may assume the two numbers do not contain any leading zero, except the
+# number 0 itself.
+# 
+# Follow up:
+# What if you cannot modify the input lists? In other words, reversing the
+# lists is not allowed.
+# 
+# 
+# 
+# Example:
+# 
+# Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 8 -> 0 -> 7
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
+# @lc code=end
