519df071c69f8985fe209a710f51e81b4ecc9ef9
ko tainen <TonyK@hyvesolutions.com>
2020-02-19 19:20:13 +0800

    add solution of leetcode 146.lru-cache.py

--- python/146.lru-cache.py
+++ python/146.lru-cache.py
@@ -44,20 +44,36 @@
 # 
 # 
 #
+class ListNode(object):
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = self
+        self.next = self
+
+
 class LRUCache(object):
 
     def __init__(self, capacity):
         """
         :type capacity: int
         """
-        
+        self.__capacity = capacity
+        self.__size = 0
+        self.__dict = {}
+        self.__root = ListNode(0, 0)
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
-        
+        if key not in self.__dict:
+            return -1
+        node = self.__dict[key]
+        self.__del_node_from_list(node)
+        self.__add_node_at_head(node)
+        return node.value
 
     def put(self, key, value):
         """
@@ -65,8 +81,42 @@ class LRUCache(object):
         :type value: int
         :rtype: None
         """
-        
+        if key in self.__dict:
+            node = self.__dict[key]
+            self.__del_node_from_list(node)
+            self.__add_node_at_head(node)
+            node.value = value
+            return
+        if self.__size >= self.__capacity:
+            self.__del_node_from_tail()
+            self.__size -= 1
+        node = ListNode(key, value)
+        self.__dict[key] = node
+        self.__add_node_at_head(node)
+        self.__size += 1
 
+    def __add_node_at_head(self, node):
+        next = self.__root.next
+        node.next = next
+        node.prev = self.__root
+        self.__root.next = node
+        next.prev = node
+
+    def __del_node_from_list(self, node):
+        if node == self.__root:
+            return
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+        node.prev = node.next = None
+
+    def __del_node_from_tail(self):
+        if self.__size == 0:
+            return
+        node = self.__root.prev
+        del self.__dict[node.key]
+        self.__del_node_from_list(node)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
