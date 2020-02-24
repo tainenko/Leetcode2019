6dc4a310a93f4c05d76ab5400259e30d650c77f4
ko tainen <TonyK@hyvesolutions.com>
2020-02-24 11:37:21 +0800

    add problem of leetcode 194.transpose-file.sh

---
+++ bash/194.transpose-file.sh
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=194 lang=bash
+#
+# [194] Transpose File
+#
+# https://leetcode.com/problems/transpose-file/description/
+#
+# shell
+# Medium (23.36%)
+# Likes:    56
+# Dislikes: 162
+# Total Accepted:    12.2K
+# Total Submissions: 51.3K
+# Testcase Example:  'a'
+#
+# Given a text file file.txt, transpose its content.
+# 
+# You may assume that each row has the same number of columns and each field is
+# separated by the ' ' character.
+# 
+# Example:
+# 
+# If file.txt has the following content:
+# 
+# 
+# name age
+# alice 21
+# ryan 30
+# 
+# 
+# Output the following:
+# 
+# 
+# name alice ryan
+# age 21 30
+# 
+# 
+# 
+# 
+#
+
+# @lc code=start
+# Read from the file file.txt and print its transposed content to stdout.
+
+# @lc code=end
