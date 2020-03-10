c23cf3bd235e7a4fe4c034e9fa2e4a0e0bde03a9
ko tainen <TonyK@hyvesolutions.com>
2020-03-10 14:19:32 +0800

    Init problem of leetcode 384.shuffle-an-array.py

---
+++ python/384.shuffle-an-array.py
@@ -0,0 +1,66 @@
+#
+# @lc app=leetcode id=384 lang=python
+#
+# [384] Shuffle an Array
+#
+# https://leetcode.com/problems/shuffle-an-array/description/
+#
+# algorithms
+# Medium (50.62%)
+# Likes:    417
+# Dislikes: 884
+# Total Accepted:    107.5K
+# Total Submissions: 207.2K
+# Testcase Example:  '["Solution","shuffle","reset","shuffle"]\n[[[1,2,3]],[],[],[]]'
+#
+# Shuffle a set of numbers without duplicates.
+# 
+# 
+# Example:
+# 
+# // Init an array with set 1, 2, and 3.
+# int[] nums = {1,2,3};
+# Solution solution = new Solution(nums);
+# 
+# // Shuffle the array [1,2,3] and return its result. Any permutation of
+# [1,2,3] must equally likely to be returned.
+# solution.shuffle();
+# 
+# // Resets the array back to its original configuration [1,2,3].
+# solution.reset();
+# 
+# // Returns the random shuffling of array [1,2,3].
+# solution.shuffle();
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
+# @lc code=end
