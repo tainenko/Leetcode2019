cea81f9d79599f8627eca24c09f89dc6e68cb5c7
ko tainen <TonyK@hyvesolutions.com>
2020-03-10 14:48:58 +0800

    Add solution of leetcode 384.shuffle-an-ayyay.py

--- python/384.shuffle-an-array.py
+++ python/384.shuffle-an-array.py
@@ -36,28 +36,37 @@
 #
 
 # @lc code=start
+import random
+
+
 class Solution(object):
 
     def __init__(self, nums):
         """
         :type nums: List[int]
         """
-        
+        self.nums = nums
+        self.size = len(self.nums)
 
     def reset(self):
         """
         Resets the array to its original configuration and return it.
         :rtype: List[int]
         """
-        
+        return self.nums
 
     def shuffle(self):
         """
         Returns a random shuffling of the array.
         :rtype: List[int]
         """
-        
-
+        # Fisher–Yates shuffle
+        # Traverse from head to tail, swap each digit with random position after.
+        output = self.nums[:]
+        for i in range(self.size):
+            idx = random.randrange(i, self.size)
+            output[i], output[idx] = output[idx], output[i]
+        return output
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
