09c80f1ba4f048d458dcaf07cfd7294fc09e9f9f
ko tainen <TonyK@hyvesolutions.com>
2020-03-12 14:27:32 +0800

    Add solution of leetcode 416.partition-equal-subset-sum.py

--- python/416.partition-equal-subset-sum.py
+++ python/416.partition-equal-subset-sum.py
@@ -59,5 +59,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        target = total >> 1
+        dp = [False] * (target + 1)
+        dp[0] = True
+        for num in nums:
+            for j in range(target, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
+        return dp[target]
+
 # @lc code=end
