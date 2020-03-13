8dd785404bf4f17b35e17a0ba0efcccc3a454154
ko tainen <TonyK@hyvesolutions.com>
2020-03-13 14:59:29 +0800

    Add solution of leetcode 419.battleships-in-a-board.py

--- python/419.battleships-in-a-board.py
+++ python/419.battleships-in-a-board.py
@@ -52,5 +52,14 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: int
         """
-        
+        if len(board) == 0 or len(board[0]) == 0:
+            return 0
+        row, col = len(board), len(board[0])
+        cnt = 0
+        for i in range(row):
+            for j in range(col):
+                if board[i][j] == 'X' and (i == 0 or board[i - 1][j] == '.') and (j == 0 or board[i][j - 1] == '.'):
+                    cnt += 1
+        return cnt
+
 # @lc code=end
