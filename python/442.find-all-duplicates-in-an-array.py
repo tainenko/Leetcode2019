6a46163b4511f19be04990b8d9f12be24a54b72a
ko tainen <TonyK@hyvesolutions.com>
2020-05-05 17:36:15 +0800

    init problem of leetcode 442.find-all-duplicates-in-an-array.py

---
+++ python/442.find-all-duplicates-in-an-array.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=442 lang=python
+#
+# [442] Find All Duplicates in an Array
+#
+# https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+#
+# algorithms
+# Medium (64.78%)
+# Likes:    1810
+# Dislikes: 142
+# Total Accepted:    148.5K
+# Total Submissions: 229.2K
+# Testcase Example:  '[4,3,2,7,8,2,3,1]'
+#
+# Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements
+# appear twice and others appear once.
+# 
+# Find all the elements that appear twice in this array.
+# 
+# Could you do it without extra space and in O(n) runtime?
+# 
+# Example:
+# 
+# Input:
+# [4,3,2,7,8,2,3,1]
+# 
+# Output:
+# [2,3]
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+# @lc code=end
