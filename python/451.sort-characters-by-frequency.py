b3840da73202984387428083409e42b79215b32d
ko tainen <TonyK@hyvesolutions.com>
2020-05-06 18:13:40 +0800

    Init problem of leetcode 451.sort-characters-by-frequency.py

---
+++ python/451.sort-characters-by-frequency.py
@@ -0,0 +1,71 @@
+#
+# @lc app=leetcode id=451 lang=python
+#
+# [451] Sort Characters By Frequency
+#
+# https://leetcode.com/problems/sort-characters-by-frequency/description/
+#
+# algorithms
+# Medium (59.72%)
+# Likes:    1209
+# Dislikes: 98
+# Total Accepted:    140.6K
+# Total Submissions: 235.1K
+# Testcase Example:  '"tree"'
+#
+# Given a string, sort it in decreasing order based on the frequency of
+# characters.
+# 
+# Example 1:
+# 
+# Input:
+# "tree"
+# 
+# Output:
+# "eert"
+# 
+# Explanation:
+# 'e' appears twice while 'r' and 't' both appear once.
+# So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid
+# answer.
+# 
+# 
+# 
+# Example 2:
+# 
+# Input:
+# "cccaaa"
+# 
+# Output:
+# "cccaaa"
+# 
+# Explanation:
+# Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+# Note that "cacaca" is incorrect, as the same characters must be together.
+# 
+# 
+# 
+# Example 3:
+# 
+# Input:
+# "Aabb"
+# 
+# Output:
+# "bbAa"
+# 
+# Explanation:
+# "bbaA" is also a valid answer, but "Aabb" is incorrect.
+# Note that 'A' and 'a' are treated as two different characters.
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+# @lc code=end
