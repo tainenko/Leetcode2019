f44ac6968c19caa24c1e13c0b8132b274616144f
ko tainen <TonyK@hyvesolutions.com>
2020-06-19 11:54:46 +0800

    Init problem of leetcode 498.diagonal-traverse.py

---
+++ python/498.diagonal-traverse.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode id=498 lang=python
+#
+# [498] Diagonal Traverse
+#
+# https://leetcode.com/problems/diagonal-traverse/description/
+#
+# algorithms
+# Medium (47.17%)
+# Likes:    710
+# Dislikes: 327
+# Total Accepted:    80.7K
+# Total Submissions: 169.1K
+# Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+#
+# Given a matrix of M x N elements (M rows, N columns), return all elements of
+# the matrix in diagonal order as shown in the below image.
+# 
+# 
+# 
+# Example:
+# 
+# 
+# Input:
+# [
+# ⁠[ 1, 2, 3 ],
+# ⁠[ 4, 5, 6 ],
+# ⁠[ 7, 8, 9 ]
+# ]
+# 
+# Output:  [1,2,4,7,5,3,6,8,9]
+# 
+# Explanation:
+# 
+# 
+# 
+# 
+# 
+# Note:
+# 
+# The total number of elements of the given matrix will not exceed 10,000.
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def findDiagonalOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        res=[]
+        while
+        
+# @lc code=end
