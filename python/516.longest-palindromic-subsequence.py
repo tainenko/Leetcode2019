f4dd368e3ebdcb5def690b07c39bb24d608480e7
ko tainen <TonyK@hyvesolutions.com>
2020-07-13 11:52:47 +0800

    Init problem of leetcode 516.longest-palindromic-subsequence.py

---
+++ python/516.longest-palindromic-subsequence.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode id=516 lang=python
+#
+# [516] Longest Palindromic Subsequence
+#
+# https://leetcode.com/problems/longest-palindromic-subsequence/description/
+#
+# algorithms
+# Medium (51.49%)
+# Likes:    1948
+# Dislikes: 178
+# Total Accepted:    117.5K
+# Total Submissions: 222.9K
+# Testcase Example:  '"bbbab"'
+#
+# Given a string s, find the longest palindromic subsequence's length in s. You
+# may assume that the maximum length of s is 1000.
+# 
+# Example 1:
+# Input:
+# 
+# 
+# "bbbab"
+# 
+# Output:
+# 
+# 
+# 4
+# 
+# One possible longest palindromic subsequence is "bbbb".
+# 
+# 
+# 
+# Example 2:
+# Input:
+# 
+# 
+# "cbbd"
+# 
+# Output:
+# 
+# 
+# 2
+# 
+# One possible longest palindromic subsequence is "bb".
+# 
+# Constraints:
+# 
+# 
+# 1 <= s.length <= 1000
+# s consists only of lowercase English letters.
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+# @lc code=end
