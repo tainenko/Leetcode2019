2061b83607f985d1b54ef6ca4c423e032a7d35c4
ko tainen <TonyK@hyvesolutions.com>
2020-07-13 12:46:02 +0800

    Add solution of leetcode 516.longest-palindromic-subsequency.py

--- python/516.longest-palindromic-subsequence.py
+++ python/516.longest-palindromic-subsequence.py
@@ -60,5 +60,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+        n = len(s)
+        L = [[0 for x in range(n)] for x in range(n)]
+        for i in range(n):
+            L[i][i] = 1
+        for cl in range(2, n + 1):
+            for i in range(n - cl + 1):
+                j = i + cl - 1
+                if s[i] == s[j] and cl == 2:
+                    L[i][j] = 2
+                elif s[i] == s[j]:
+                    L[i][j] = L[i + 1][j - 1] + 2
+                else:
+                    L[i][j] = max(L[i + 1][j], L[i][j - 1])
+        return L[0][n - 1]
 # @lc code=end
