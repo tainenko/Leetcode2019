74c3ffbb450e7296dac90f8576d088251ffe13c4
ko tainen <TonyK@hyvesolutions.com>
2020-08-04 13:17:08 +0800

    Init problem of leetcode 524.longest-word-in-dictionary-through-deleting.py

---
+++ python/524.longest-word-in-dictionary-through-deleting.py
@@ -0,0 +1,62 @@
+#
+# @lc app=leetcode id=524 lang=python
+#
+# [524] Longest Word in Dictionary through Deleting
+#
+# https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/description/
+#
+# algorithms
+# Medium (47.90%)
+# Likes:    560
+# Dislikes: 236
+# Total Accepted:    68.5K
+# Total Submissions: 141.4K
+# Testcase Example:  '"abpcplea"\n["ale","apple","monkey","plea"]'
+#
+# 
+# Given a string and a string dictionary, find the longest string in the
+# dictionary that can be formed by deleting some characters of the given
+# string. If there are more than one possible results, return the longest word
+# with the smallest lexicographical order. If there is no possible result,
+# return the empty string.
+# 
+# Example 1:
+# 
+# Input:
+# s = "abpcplea", d = ["ale","apple","monkey","plea"]
+# 
+# Output: 
+# "apple"
+# 
+# 
+# 
+# 
+# Example 2:
+# 
+# Input:
+# s = "abpcplea", d = ["a","b","c"]
+# 
+# Output: 
+# "a"
+# 
+# 
+# 
+# Note:
+# 
+# All the strings in the input will only contain lower-case letters.
+# The size of the dictionary won't exceed 1,000.
+# The length of all the strings in the input won't exceed 1,000.
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def findLongestWord(self, s, d):
+        """
+        :type s: str
+        :type d: List[str]
+        :rtype: str
+        """
+        
+# @lc code=end
