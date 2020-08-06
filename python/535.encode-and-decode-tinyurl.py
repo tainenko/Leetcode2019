eae3856bc33ade1250f8c88f588360872f8f5eca
ko tainen <TonyK@hyvesolutions.com>
2020-08-06 08:32:54 +0800

    init problem of leetcode 535.encode-and-decode-tinyurl.py

---
+++ python/535.encode-and-decode-tinyurl.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode id=535 lang=python
+#
+# [535] Encode and Decode TinyURL
+#
+# https://leetcode.com/problems/encode-and-decode-tinyurl/description/
+#
+# algorithms
+# Medium (79.12%)
+# Likes:    661
+# Dislikes: 1456
+# Total Accepted:    108.6K
+# Total Submissions: 136.1K
+# Testcase Example:  '"https://leetcode.com/problems/design-tinyurl"'
+#
+# Note: This is a companion problem to the System Design problem: Design
+# TinyURL.
+# 
+# TinyURL is a URL shortening service where you enter a URL such as
+# https://leetcode.com/problems/design-tinyurl and it returns a short URL such
+# as http://tinyurl.com/4e9iAk.
+# 
+# Design the encode and decode methods for the TinyURL service. There is no
+# restriction on how your encode/decode algorithm should work. You just need to
+# ensure that a URL can be encoded to a tiny URL and the tiny URL can be
+# decoded to the original URL.
+# 
+#
+
+# @lc code=start
+class Codec:
+
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+        
+        :type longUrl: str
+        :rtype: str
+        """
+        
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+        
+        :type shortUrl: str
+        :rtype: str
+        """
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
+# @lc code=end
