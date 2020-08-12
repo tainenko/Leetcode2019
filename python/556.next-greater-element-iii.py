00f13554af8da1eaac9abdd640efc02bbf5a65cf
ko tainen <TonyK@hyvesolutions.com>
2020-08-12 18:45:17 +0800

    init problem of leetcode 556.next-greter-element-iii.py

---
+++ python/556.next-greater-element-iii.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=556 lang=python
+#
+# [556] Next Greater Element III
+#
+# https://leetcode.com/problems/next-greater-element-iii/description/
+#
+# algorithms
+# Medium (31.20%)
+# Likes:    690
+# Dislikes: 195
+# Total Accepted:    42.9K
+# Total Submissions: 135.3K
+# Testcase Example:  '12'
+#
+# Given a positive 32-bit integer n, you need to find the smallest 32-bit
+# integer which has exactly the same digits existing in the integer n and is
+# greater in value than n. If no such positive 32-bit integer exists, you need
+# to return -1.
+# 
+# Example 1:
+# 
+# 
+# Input: 12
+# Output: 21
+# 
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: 21
+# Output: -1
+# 
+# 
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def nextGreaterElement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+# @lc code=end
