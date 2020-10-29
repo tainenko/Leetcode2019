91c2818d4f561c4c05e3238e179e37fa0d5d58ae
ko tainen <TonyK@hyvesolutions.com>
2020-10-30 00:44:27 +0800

    init problem of leetcode 42.trapping-rain-water.py

---
+++ python/42.trapping-rain-water.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=42 lang=python
+#
+# [42] Trapping Rain Water
+#
+# https://leetcode.com/problems/trapping-rain-water/description/
+#
+# algorithms
+# Hard (47.56%)
+# Likes:    8650
+# Dislikes: 132
+# Total Accepted:    596.3K
+# Total Submissions: 1.2M
+# Testcase Example:  '[0,1,0,2,1,0,1,3,2,1,2,1]'
+#
+# Given n non-negative integers representing an elevation map where the width
+# of each bar is 1, compute how much water it can trap after raining.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# Output: 6
+# Explanation: The above elevation map (black section) is represented by array
+# [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section)
+# are being trapped.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: height = [4,2,0,3,2,5]
+# Output: 9
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# n == height.length
+# 0 <= n <= 3 * 10^4
+# 0 <= height[i] <= 10^5
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
+# @lc code=end
