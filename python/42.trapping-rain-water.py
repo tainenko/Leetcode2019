63292e2833fb230309917b7cc323bfc7cf1d4572
ko tainen <TonyK@hyvesolutions.com>
2020-10-30 09:24:16 +0800

    add solution of leetcode 42.trapping-rain-water.py

--- python/42.trapping-rain-water.py
+++ python/42.trapping-rain-water.py
@@ -52,5 +52,19 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        
+        res = 0
+        i = 0
+        n = len(height)
+        stack = []
+        while i < n:
+            if not stack or height[i] < height[stack[-1]]:
+                stack.append(i)
+                i += 1
+            else:
+                t = stack.pop()
+                if not stack:
+                    continue
+                res += (min(height[i], height[stack[-1]]) - height[t]) * (i - stack[-1] - 1)
+        return res
+
 # @lc code=end
