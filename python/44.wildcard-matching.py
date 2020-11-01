adebeeb6467b785099ee2ef919853e82a283fa4f
ko tainen <TonyK@hyvesolutions.com>
2020-11-01 22:54:16 +0800

    add solution of leetcode 44.wildcard-batching.py

--- python/44.wildcard-matching.py
+++ python/44.wildcard-matching.py
@@ -84,5 +84,19 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        
+        m, n = len(s), len(p)
+        dp = [[False for _ in range(n + 1)] for _ in range(m + 1)]
+        dp[0][0] = True
+        for j in range(1, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 1]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '*':
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+                elif s[i - 1] == p[j - 1] or p[j - 1] == "?":
+                    dp[i][j] = dp[i - 1][j - 1]
+        return dp[m][n]
+
 # @lc code=end
