d706aacb00ec069dedd21f91fb97ebdeefd173af
ko tainen <TonyK@hyvesolutions.com>
2020-11-02 22:42:23 +0800

    init problem of leetcode 45.jump-game-ii.py

---
+++ python/45.jump-game-ii.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode id=45 lang=python
+#
+# [45] Jump Game II
+#
+# https://leetcode.com/problems/jump-game-ii/description/
+#
+# algorithms
+# Hard (30.06%)
+# Likes:    3187
+# Dislikes: 158
+# Total Accepted:    290.2K
+# Total Submissions: 936.5K
+# Testcase Example:  '[2,3,1,1,4]'
+#
+# Given an array of non-negative integers nums, you are initially positioned at
+# the first index of the array.
+# 
+# Each element in the array represents your maximum jump length at that
+# position.
+# 
+# Your goal is to reach the last index in the minimum number of jumps.
+# 
+# You can assume that you can always reach the last index.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [2,3,1,1,4]
+# Output: 2
+# Explanation: The minimum number of jumps to reach the last index is 2. Jump 1
+# step from index 0 to 1, then 3 steps to the last index.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [2,3,0,1,4]
+# Output: 2
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 3 * 10^4
+# 0 <= nums[i] <= 10^5
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+# @lc code=end
