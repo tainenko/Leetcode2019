4c95d26beed38e9396db9fce71e6859878501eeb
ko tainen <TonyK@hyvesolutions.com>
2020-11-02 23:03:28 +0800

    add solution of leetcode 45.jump-game-ii.py

--- python/45.jump-game-ii.py
+++ python/45.jump-game-ii.py
@@ -57,5 +57,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        ret = 0
+        last = 0
+        curr = 0
+        for i, num in enumerate(nums):
+            if i > last:
+                last = curr
+                ret += 1
+            curr = max(curr, i + num)
+        return ret
+
 # @lc code=end
