a3566e19bd5957921ea69a0a0a0bdbd6fe2b3cc2
ko tainen <TonyK@hyvesolutions.com>
2021-04-25 00:16:57 +0800

    init problem of leetcode 611.valid triangle number.py

---
+++ leetcode/editor/en/[611]Valid Triangle Number.py
@@ -0,0 +1,35 @@
+# Given an array consists of non-negative integers, your task is to count the nu
+# mber of triplets chosen from the array that can make triangles if we take them a
+# s side lengths of a triangle.
+# 
+#  Example 1: 
+#  
+# Input: [2,2,3,4]
+# Output: 3
+# Explanation:
+# Valid combinations are: 
+# 2,3,4 (using the first 2)
+# 2,3,4 (using the second 2)
+# 2,2,3
+#  
+#  
+# 
+#  Note: 
+#  
+#  The length of the given array won't exceed 1000. 
+#  The integers in the given array are in the range of [0, 1000]. 
+#  
+#  
+#  Related Topics Array 
+#  👍 1385 👎 106
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def triangleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+# leetcode submit region end(Prohibit modification and deletion)
