809757737642f0638fa59139660624057505f588
ko tainen <TonyK@hyvesolutions.com>
2021-07-25 23:09:50 +0800

    add solution of leetcode 1039 Minimum score triangulation of polygon.py

--- leetcode/editor/en/[1039]Minimum Score Triangulation of Polygon.py
+++ leetcode/editor/en/[1039]Minimum Score Triangulation of Polygon.py
@@ -58,5 +58,12 @@ class Solution(object):
         :type values: List[int]
         :rtype: int
         """
-        
+        n = len(values)
+        dp = [[0] * n for _ in range(n)]
+        for j in range(2, n):
+            for i in range(j - 2, -1, -1):
+                dp[i][j] = float('inf')
+                for k in range(i + 1, j):
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + values[i] * values[j] * values[k])
+        return dp[0][n - 1]
 # leetcode submit region end(Prohibit modification and deletion)
