0d9b7f69ab13a27303014a46e58a2246123856f9
ko tainen <TonyK@hyvesolutions.com>
2021-07-25 23:16:17 +0800

    init problem of leetcode 128 Longest Consecutive Sequence.py

---
+++ leetcode/editor/en/[128]Longest Consecutive Sequence.py
@@ -0,0 +1,42 @@
+# Given an unsorted array of integers nums, return the length of the longest con
+# secutive elements sequence. 
+# 
+#  You must write an algorithm that runs in O(n) time. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [100,4,200,1,3,2]
+# Output: 4
+# Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Theref
+# ore its length is 4.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [0,3,7,2,5,8,4,6,0,1]
+# Output: 9
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  0 <= nums.length <= 105 
+#  -109 <= nums[i] <= 109 
+#  
+#  Related Topics Array Hash Table Union Find 
+#  👍 6224 👎 289
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+# leetcode submit region end(Prohibit modification and deletion)
