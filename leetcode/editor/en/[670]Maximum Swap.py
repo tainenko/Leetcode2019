9dcfbbce8083fe3d6d9c6c3ac157f4752703c205
ko tainen <TonyK@hyvesolutions.com>
2021-08-10 00:04:18 +0800

    init problem of leetcode 670 maximum swap.py

---
+++ leetcode/editor/en/[670]Maximum Swap.py
@@ -0,0 +1,41 @@
+# You are given an integer num. You can swap two digits at most once to get the 
+# maximum valued number. 
+# 
+#  Return the maximum valued number you can get. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: num = 2736
+# Output: 7236
+# Explanation: Swap the number 2 and the number 7.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: num = 9973
+# Output: 9973
+# Explanation: No swap.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  0 <= num <= 108 
+#  
+#  Related Topics Math Greedy 
+#  👍 1701 👎 98
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def maximumSwap(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        
+# leetcode submit region end(Prohibit modification and deletion)
