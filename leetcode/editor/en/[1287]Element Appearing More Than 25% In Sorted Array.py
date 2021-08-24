7c84e473e27d6de17976401e367ceb0527caa474
ko tainen <TonyK@hyvesolutions.com>
2021-08-25 07:57:30 +0800

    init problem of leetcode 1287 element appearing more than 25% in sorted array

---
+++ leetcode/editor/en/[1287]Element Appearing More Than 25% In Sorted Array.py
@@ -0,0 +1,39 @@
+# Given an integer array sorted in non-decreasing order, there is exactly one in
+# teger in the array that occurs more than 25% of the time, return that integer. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: arr = [1,2,2,6,6,6,6,7,10]
+# Output: 6
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: arr = [1,1]
+# Output: 1
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= arr.length <= 104 
+#  0 <= arr[i] <= 105 
+#  
+#  Related Topics Array 
+#  👍 526 👎 34
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def findSpecialInteger(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+       
+
+    # leetcode submit region end(Prohibit modification and deletion)
