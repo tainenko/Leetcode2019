467a65c7dcb3b16063fee120ee66e2006a733831
ko tainen <TonyK@hyvesolutions.com>
2021-08-26 00:00:21 +0800

    add solution of leetcode 672 bulb switcher ii.py

--- leetcode/editor/en/[672]Bulb Switcher II.py
+++ leetcode/editor/en/[672]Bulb Switcher II.py
@@ -63,5 +63,12 @@ class Solution(object):
         :type presses: int
         :rtype: int
         """
-        
+        if presses == 0:
+            return 1
+        if n >= 3:
+            return 4 if presses == 1 else 7 if presses == 2 else 8
+        if n == 2:
+            return 3 if presses == 1 else 4
+        if n == 1:
+            return 2
 # leetcode submit region end(Prohibit modification and deletion)
