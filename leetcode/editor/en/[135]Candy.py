1ba540ad7d6949a07205bfc7c998da340f4cb3d6
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-11-30 23:19:00 +0800

    add solution of leetcode 135 candy.py

--- leetcode/editor/en/[135]Candy.py
+++ leetcode/editor/en/[135]Candy.py
@@ -46,5 +46,14 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        
+        res = [1] * len(ratings)
+        for i in range(len(ratings) - 1):
+            if ratings[i + 1] > ratings[i]:
+                res[i + 1] = res[i] + 1
+
+        for i in range(len(ratings) - 1, 0, -1):
+            if ratings[i - 1] > ratings[i]:
+                res[i - 1] = max(res[i-1], res[i] + 1)
+        return sum(res)
+
 # leetcode submit region end(Prohibit modification and deletion)
