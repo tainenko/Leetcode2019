b377ef0f5bf9b09f31f93ed6b74514caa5c0e860
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-11-30 23:27:45 +0800

    add solution of leetcode 252 meeting rooms.py

--- leetcode/editor/en/[252]Meeting Rooms.py
+++ leetcode/editor/en/[252]Meeting Rooms.py
@@ -23,5 +23,10 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        
+        intervals.sort(key=lambda x: x[0])
+        for i in range(len(intervals) - 1):
+            if intervals[i][1] > intervals[i + 1][0]:
+                return False
+        return True
+
 # leetcode submit region end(Prohibit modification and deletion)
