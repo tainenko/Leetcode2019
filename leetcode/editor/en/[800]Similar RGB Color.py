23db687a6d3e1c29c7b023cc98ac877b1cfdef75
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-12-02 00:45:27 +0800

    add solution of leetcode 80 similar rgb color.py

--- leetcode/editor/en/[800]Similar RGB Color.py
+++ leetcode/editor/en/[800]Similar RGB Color.py
@@ -1,51 +1,58 @@
-# The red-green-blue color "#AABBCC" can be written as "#ABC" in shorthand. 
-# 
-#  
-#  For example, "#15c" is shorthand for the color "#1155cc". 
-#  
-# 
-#  The similarity between the two colors "#ABCDEF" and "#UVWXYZ" is -(AB - UV)² 
-# - (CD - WX)² - (EF - YZ)². 
-# 
-#  Given a string color that follows the format "#ABCDEF", return a string 
+# The red-green-blue color "#AABBCC" can be written as "#ABC" in shorthand.
+#
+#
+#  For example, "#15c" is shorthand for the color "#1155cc".
+#
+#
+#  The similarity between the two colors "#ABCDEF" and "#UVWXYZ" is -(AB - UV)²
+# - (CD - WX)² - (EF - YZ)².
+#
+#  Given a string color that follows the format "#ABCDEF", return a string
 # represents the color that is most similar to the given color and has a shorthand (i.
-# e., it can be represented as some "#XYZ"). 
-# 
-#  Any answer which has the same highest similarity as the best answer will be 
-# accepted. 
-# 
-#  
-#  Example 1: 
-# 
-#  
+# e., it can be represented as some "#XYZ").
+#
+#  Any answer which has the same highest similarity as the best answer will be
+# accepted.
+#
+#
+#  Example 1:
+#
+#
 # Input: color = "#09f166"
 # Output: "#11ee66"
-# Explanation: 
+# Explanation:
 # The similarity is -(0x09 - 0x11)² -(0xf1 - 0xee)² - (0x66 - 0x66)² = -64 -9 -0
 #  = -73.
 # This is the highest among any shorthand color.
-#  
-# 
-#  Example 2: 
-# 
-#  
+#
+#
+#  Example 2:
+#
+#
 # Input: color = "#4e3fe1"
 # Output: "#5544dd"
-#  
-# 
-#  
-#  Constraints: 
-# 
-#  
-#  color.length == 7 
-#  color[0] == '#' 
-#  color[i] is either digit or character in the range ['a', 'f'] for i > 0. 
-#  
+#
+#
+#
+#  Constraints:
+#
+#
+#  color.length == 7
+#  color[0] == '#'
+#  color[i] is either digit or character in the range ['a', 'f'] for i > 0.
+#
 #  Related Topics Math String Enumeration 👍 76 👎 469
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def similarRGB(self, color: str) -> str:
-        
+        res = "#"
+        hex_map = "0123456789abcdef"
+        for idx in range(1, len(color), 2):
+            num = int(color[idx:idx + 2], 16)
+            i = num // 17 + (1 if num % 17 > 8 else 0)
+            res += hex_map[i] * 2
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
