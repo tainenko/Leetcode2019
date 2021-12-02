9c954cc03b8e775aeb1eb6a42bf2c25d4b38e852
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-12-02 23:00:57 +0800

    init problem of leetcode 266 palindome permutation.py

---
+++ leetcode/editor/en/[266]Palindrome Permutation.py
@@ -0,0 +1,40 @@
+# Given a string s, return true if a permutation of the string could form a 
+# palindrome. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: s = "code"
+# Output: false
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: s = "aab"
+# Output: true
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: s = "carerac"
+# Output: true
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 5000 
+#  s consists of only lowercase English letters. 
+#  
+#  Related Topics Hash Table String Bit Manipulation 👍 754 👎 64
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        
+# leetcode submit region end(Prohibit modification and deletion)
