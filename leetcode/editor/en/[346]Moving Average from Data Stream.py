1fbc61fb962c12e50ce3fde1a6038c91685b77c3
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-12-13 23:11:22 +0800

    init problem of leetcode 346 moving average from data stream

---
+++ leetcode/editor/en/[346]Moving Average from Data Stream.py
@@ -0,0 +1,57 @@
+# Given a stream of integers and a window size, calculate the moving average of 
+# all integers in the sliding window. 
+# 
+#  Implement the MovingAverage class: 
+# 
+#  
+#  MovingAverage(int size) Initializes the object with the size of the window 
+# size. 
+#  double next(int val) Returns the moving average of the last size values of 
+# the stream. 
+#  
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input
+# ["MovingAverage", "next", "next", "next", "next"]
+# [[3], [1], [10], [3], [5]]
+# Output
+# [null, 1.0, 5.5, 4.66667, 6.0]
+# 
+# Explanation
+# MovingAverage movingAverage = new MovingAverage(3);
+# movingAverage.next(1); // return 1.0 = 1 / 1
+# movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+# movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+# movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= size <= 1000 
+#  -10⁵ <= val <= 10⁵ 
+#  At most 10⁴ calls will be made to next. 
+#  
+#  Related Topics Array Design Queue Data Stream 👍 1095 👎 106
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class MovingAverage:
+
+    def __init__(self, size: int):
+
+        
+
+    def next(self, val: int) -> float:
+
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
+# leetcode submit region end(Prohibit modification and deletion)
