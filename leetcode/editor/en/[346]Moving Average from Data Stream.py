3ba4d15030d91c43742d3a693c7d732a2b8053a1
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-12-13 23:23:01 +0800

    replace list with deque

--- leetcode/editor/en/[346]Moving Average from Data Stream.py
+++ leetcode/editor/en/[346]Moving Average from Data Stream.py
@@ -40,20 +40,21 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+from collections import deque
+
+
 class MovingAverage:
 
     def __init__(self, size: int):
         self.size = size
-        self.window = []
+        self.window = deque()
         self.total = 0
 
     def next(self, val: int) -> float:
         self.total += val
         self.window.append(val)
         if len(self.window) > self.size:
-            first = self.window[0]
-            self.total -= first
-            self.window = self.window[1:]
+            self.total -= self.window.popleft()
         return self.total / len(self.window)
 
 # Your MovingAverage object will be instantiated and called as such:
