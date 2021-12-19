3ae8bd50fac61c691f07f751b0812bc2cd67fe90
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-12-19 22:06:43 +0800

    init problem of leetcode 159 longest substring with at most two distinct characters.py

---
+++ leetcode/editor/en/[159]Longest Substring with At Most Two Distinct Characters.py
@@ -0,0 +1,35 @@
+# Given a string s, return the length of the longest substring that contains at 
+# most two distinct characters. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: s = "eceba"
+# Output: 3
+# Explanation: The substring is "ece" which its length is 3.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: s = "ccaabbb"
+# Output: 5
+# Explanation: The substring is "aabbb" which its length is 5.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 10⁵ 
+#  s consists of English letters. 
+#  
+#  Related Topics Hash Table String Sliding Window 👍 1541 👎 25
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
