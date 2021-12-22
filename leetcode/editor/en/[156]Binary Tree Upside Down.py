d74b7fb5926194d33d7ea1fc1506d218a6d2764c
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-12-22 22:50:07 +0800

    add solution of leetcode 156 binary tree upside down.py

--- leetcode/editor/en/[156]Binary Tree Upside Down.py
+++ leetcode/editor/en/[156]Binary Tree Upside Down.py
@@ -54,7 +54,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
+
 class Solution:
     def upsideDownBinaryTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
+        if not root or (not root.left and not root.right):
+            return root
+        new_root = self.upsideDownBinaryTree(root.left)
+        root.left.left = root.right
+        root.left.right = root
+        root.left = None
+        root.right = None
+        return new_root
+
 # leetcode submit region end(Prohibit modification and deletion)
