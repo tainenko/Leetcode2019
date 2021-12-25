799b23675ad3950926538d51ae703aab971b2b4b
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-12-25 21:11:24 +0800

    init problem of leetcode 186 reverse words in a string ii.py

---
+++ leetcode/editor/en/[186]Reverse Words in a String II.py
@@ -0,0 +1,37 @@
+# Given a character array s, reverse the order of the words. 
+# 
+#  A word is defined as a sequence of non-space characters. The words in s will 
+# be separated by a single space. 
+# 
+#  Your code must solve the problem in-place, i.e. without allocating extra 
+# space. 
+# 
+#  
+#  Example 1: 
+#  Input: s = ["t","h","e"," ","s","k","y"," ","i","s"," ","b","l","u","e"]
+# Output: ["b","l","u","e"," ","i","s"," ","s","k","y"," ","t","h","e"]
+#  Example 2: 
+#  Input: s = ["a"]
+# Output: ["a"]
+#  
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 10⁵ 
+#  s[i] is an English letter (uppercase or lowercase), digit, or space ' '. 
+#  There is at least one word in s. 
+#  s does not contain leading or trailing spaces. 
+#  All the words in s are guaranteed to be separated by a single space. 
+#  
+#  Related Topics Two Pointers String 👍 778 👎 125
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def reverseWords(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+# leetcode submit region end(Prohibit modification and deletion)
