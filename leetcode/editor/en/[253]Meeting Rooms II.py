1c6583a47758aa7df0f8085abb671fe115383304
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-16 13:17:53 +0800

    init problem of leetcode 253 meeting rooms ii.py

---
+++ leetcode/editor/en/[253]Meeting Rooms II.py
@@ -0,0 +1,27 @@
+# Given an array of meeting time intervals intervals where intervals[i] = [
+# starti, endi], return the minimum number of conference rooms required. 
+# 
+#  
+#  Example 1: 
+#  Input: intervals = [[0,30],[5,10],[15,20]]
+# Output: 2
+#  Example 2: 
+#  Input: intervals = [[7,10],[2,4]]
+# Output: 1
+#  
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= intervals.length <= 10⁴ 
+#  0 <= starti < endi <= 10⁶ 
+#  
+#  Related Topics Array Two Pointers Greedy Sorting Heap (Priority Queue) 👍 490
+# 2 👎 97
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+
+# leetcode submit region end(Prohibit modification and deletion)
