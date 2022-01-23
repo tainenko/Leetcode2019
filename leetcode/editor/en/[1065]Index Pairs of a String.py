50372aabaa3b4a9db91023176a79121a2951096d
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-23 19:42:44 +0800

    init problem of leetcode 1065 index pairs of a string.py

---
+++ leetcode/editor/en/[1065]Index Pairs of a String.py
@@ -0,0 +1,41 @@
+# Given a string text and an array of strings words, return an array of all 
+# index pairs [i, j] so that the substring text[i...j] is in words. 
+# 
+#  Return the pairs [i, j] in sorted order (i.e., sort them by their first 
+# coordinate, and in case of ties sort them by their second coordinate). 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: text = "thestoryofleetcodeandme", words = ["story","fleet","leetcode"]
+# Output: [[3,7],[9,13],[10,17]]
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: text = "ababa", words = ["aba","ab"]
+# Output: [[0,1],[0,2],[2,3],[2,4]]
+# Explanation: Notice that matches can overlap, see "aba" is found in [0,2] and 
+# [2,4].
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= text.length <= 100 
+#  1 <= words.length <= 20 
+#  1 <= words[i].length <= 50 
+#  text and words[i] consist of lowercase English letters. 
+#  All the strings of words are unique. 
+#  
+#  Related Topics Array String Trie Sorting 👍 203 👎 73
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
