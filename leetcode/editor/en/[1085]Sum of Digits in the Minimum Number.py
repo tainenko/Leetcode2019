4b8acf8893925977566dbcfb423d7da12354f029
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-23 21:26:56 +0800

    init problem of leetcode 1085 sum of digits in the minimum number.py

---
+++ leetcode/editor/en/[1085]Sum of Digits in the Minimum Number.py
@@ -0,0 +1,37 @@
+# Given an integer array nums, return 0 if the sum of the digits of the minimum 
+# integer in nums is odd, or 1 otherwise. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [34,23,1,24,75,33,54,8]
+# Output: 0
+# Explanation: The minimal element is 1, and the sum of those digits is 1 which 
+# is odd, so the answer is 0.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [99,77,33,66,55]
+# Output: 1
+# Explanation: The minimal element is 33, and the sum of those digits is 3 + 3 =
+#  6 which is even, so the answer is 1.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= nums.length <= 100 
+#  1 <= nums[i] <= 100 
+#  
+#  Related Topics Array Math 👍 73 👎 132
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def sumOfDigits(self, nums: List[int]) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
