727a8e280feb89704fb808712925efb0c5baee38
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-25 00:25:48 +0800

    add solution of leetcode 2089 find target indices after soring array

--- leetcode/editor/en/[2089]Find Target Indices After Sorting Array.py
+++ leetcode/editor/en/[2089]Find Target Indices After Sorting Array.py
@@ -47,5 +47,11 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if nums[i] == target:
+                res.append(i)
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
