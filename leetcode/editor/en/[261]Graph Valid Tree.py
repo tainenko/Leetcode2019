738cfd10a2c846719bfdc0665755d35c32b702d6
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-25 00:39:49 +0800

    init problem of leetcode 261 graph valid tree

---
+++ leetcode/editor/en/[261]Graph Valid Tree.py
@@ -0,0 +1,42 @@
+# You have a graph of n nodes labeled from 0 to n - 1. You are given an integer 
+# n and a list of edges where edges[i] = [ai, bi] indicates that there is an 
+# undirected edge between nodes ai and bi in the graph. 
+# 
+#  Return true if the edges of the given graph make up a valid tree, and false 
+# otherwise. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: n = 5, edges = [[0,1],[0,2],[0,3],[1,4]]
+# Output: true
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: n = 5, edges = [[0,1],[1,2],[2,3],[1,3],[1,4]]
+# Output: false
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= n <= 2000 
+#  0 <= edges.length <= 5000 
+#  edges[i].length == 2 
+#  0 <= ai, bi < n 
+#  ai != bi 
+#  There are no self-loops or repeated edges. 
+#  
+#  Related Topics Depth-First Search Breadth-First Search Union Find Graph 👍 21
+# 01 👎 55
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+# leetcode submit region end(Prohibit modification and deletion)
