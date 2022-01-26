08cedb8cb7ed83163ff93c9fdfed39bc71340687
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-26 23:39:04 +0800

    add solution of leetcode 1304 find n unique integers sum up to zero.py

--- leetcode/editor/en/[1304]Find N Unique Integers Sum up to Zero.py
+++ leetcode/editor/en/[1304]Find N Unique Integers Sum up to Zero.py
@@ -36,5 +36,8 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        
-# leetcode submit region end(Prohibit modification and deletion)
+        if n % 2 == 1:
+            return [i for i in range(-(n // 2), n // 2 + 1)]
+        return [i for i in range(-(n // 2), 0)] + [i for i in range(1, n // 2 + 1)]
+
+    # leetcode submit region end(Prohibit modification and deletion)
