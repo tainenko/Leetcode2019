8cef3244010e7e5df88ef1f8eca521e8fc2efd07
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-26 23:41:24 +0800

    add solution of leetcode 1295 find numbers with even number of digits.py

--- leetcode/editor/en/[1295]Find Numbers with Even Number of Digits.py
+++ leetcode/editor/en/[1295]Find Numbers with Even Number of Digits.py
@@ -38,5 +38,6 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        
+        return sum([1 for num in nums if len(str(num)) % 2 == 0])
+
 # leetcode submit region end(Prohibit modification and deletion)
