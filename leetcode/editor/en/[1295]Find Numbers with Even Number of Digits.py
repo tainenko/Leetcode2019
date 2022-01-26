78dbc5bcc9e29bd8232acb218efd8be89af4b818
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-26 23:39:35 +0800

    init problem of leetcode 1295 find numbers with even number of digits.py

---
+++ leetcode/editor/en/[1295]Find Numbers with Even Number of Digits.py
@@ -0,0 +1,42 @@
+# Given an array nums of integers, return how many of them contain an even 
+# number of digits. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [12,345,2,6,7896]
+# Output: 2
+# Explanation: 
+# 12 contains 2 digits (even number of digits). 
+# 345 contains 3 digits (odd number of digits). 
+# 2 contains 1 digit (odd number of digits). 
+# 6 contains 1 digit (odd number of digits). 
+# 7896 contains 4 digits (even number of digits). 
+# Therefore only 12 and 7896 contain an even number of digits.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [555,901,482,1771]
+# Output: 1 
+# Explanation: 
+# Only 1771 contains an even number of digits.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= nums.length <= 500 
+#  1 <= nums[i] <= 10⁵ 
+#  
+#  Related Topics Array 👍 1065 👎 94
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
