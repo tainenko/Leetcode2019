98474df39025df2cce23308d8fb09f0e478c150e
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-30 00:18:47 +0800

    init problem of leetcode 1118 number of days in a month.py

---
+++ leetcode/editor/en/[1118]Number of Days in a Month.py
@@ -0,0 +1,29 @@
+# Given a year year and a month month, return the number of days of that month. 
+# 
+# 
+#  
+#  Example 1: 
+#  Input: year = 1992, month = 7
+# Output: 31
+#  Example 2: 
+#  Input: year = 2000, month = 2
+# Output: 29
+#  Example 3: 
+#  Input: year = 1900, month = 2
+# Output: 28
+#  
+#  
+#  Constraints: 
+# 
+#  
+#  1583 <= year <= 2100 
+#  1 <= month <= 12 
+#  
+#  Related Topics Math 👍 28 👎 130
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numberOfDays(self, year: int, month: int) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
