edd248cdd1b4a1a5843bcbb02da4d0a8dc80e6a9
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-30 22:46:35 +0800

    add solution of leetcode 1165 single-row keyboard.py

---
+++ leetcode/editor/en/[1165]Single-Row Keyboard.py
@@ -0,0 +1,45 @@
+# There is a special keyboard with all keys in a single row. 
+# 
+#  Given a string keyboard of length 26 indicating the layout of the keyboard (
+# indexed from 0 to 25). Initially, your finger is at index 0. To type a character,
+#  you have to move your finger to the index of the desired character. The time 
+# taken to move your finger from index i to index j is |i - j|. 
+# 
+#  You want to type a string word. Write a function to calculate how much time 
+# it takes to type it with one finger. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: keyboard = "abcdefghijklmnopqrstuvwxyz", word = "cba"
+# Output: 4
+# Explanation: The index moves from 0 to 2 to write 'c' then to 1 to write 'b' 
+# then to 0 again to write 'a'.
+# Total time = 2 + 1 + 1 = 4. 
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: keyboard = "pqrstuvwxyzabcdefghijklmno", word = "leetcode"
+# Output: 73
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  keyboard.length == 26 
+#  keyboard contains each English lowercase letter exactly once in some order. 
+#  1 <= word.length <= 10⁴ 
+#  word[i] is an English lowercase letter. 
+#  
+#  Related Topics Hash Table String 👍 328 👎 17
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
