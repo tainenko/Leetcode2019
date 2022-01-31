37c97f64d5b1e8f5f8f2fd2fa91faf92f52e1a20
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-31 17:44:31 +0800

    init problem of leetcode 1189 maximum number of ballons.py

---
+++ leetcode/editor/en/[1189]Maximum Number of Balloons.py
@@ -0,0 +1,47 @@
+# Given a string text, you want to use the characters of text to form as many 
+# instances of the word "balloon" as possible. 
+# 
+#  You can use each character in text at most once. Return the maximum number 
+# of instances that can be formed. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# 
+#  
+# Input: text = "nlaebolko"
+# Output: 1
+#  
+# 
+#  Example 2: 
+# 
+#  
+# 
+#  
+# Input: text = "loonbalxballpoon"
+# Output: 2
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: text = "leetcode"
+# Output: 0
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= text.length <= 10⁴ 
+#  text consists of lower case English letters only. 
+#  
+#  Related Topics Hash Table String Counting 👍 941 👎 66
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
