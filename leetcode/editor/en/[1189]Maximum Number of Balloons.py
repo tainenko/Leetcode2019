505ae01428104e19fc3815bc0c239f1fd8222318
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-01-31 17:47:09 +0800

    add solution of leetcode 1189 maximum number of balloons.py

--- leetcode/editor/en/[1189]Maximum Number of Balloons.py
+++ leetcode/editor/en/[1189]Maximum Number of Balloons.py
@@ -41,7 +41,12 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+from collections import Counter
+
+
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        
+        count = Counter(text)
+        return min(count.get('b', 0), count.get('a', 0), count.get('l', 0) // 2, count.get('o', 0) // 2,
+                   count.get('n', 0))
 # leetcode submit region end(Prohibit modification and deletion)
