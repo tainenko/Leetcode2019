5a1c0f5375fd45fef95abc77ab2ccb83df3011aa
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-04 02:15:10 +0800

    add solution of leetcode 1275 find winner on a tic tac tow game.py

--- leetcode/editor/en/[1275]Find Winner on a Tic Tac Toe Game.py
+++ leetcode/editor/en/[1275]Find Winner on a Tic Tac Toe Game.py
@@ -1,4 +1,4 @@
-    qq# Tic-tac-toe is played by two players A and B on a 3 x 3 grid. The rules of
+# Tic-tac-toe is played by two players A and B on a 3 x 3 grid. The rules of
 # Tic-Tac-Toe are: 
 # 
 #  
@@ -62,5 +62,28 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        
+        m = [[" " for _ in range(3)] for _ in range(3)]
+        for move in moves[::2]:
+            row = move[0]
+            col = move[1]
+            m[row][col] = 'A'
+        for move in moves[1::2]:
+            row = move[0]
+            col = move[1]
+            m[row][col] = 'B'
+        res = self.helper(m)
+        if res is not None:
+            return res
+        return "Draw" if len(moves) == 9 else "Pending"
+
+    def helper(self, matrix: List[List[str]]):
+        for i in range(3):
+            if " " != matrix[i][0] == matrix[i][1] == matrix[i][2]:
+                return matrix[i][0]
+            if " " != matrix[0][i] == matrix[1][i] == matrix[2][i]:
+                return matrix[0][i]
+        if " " != matrix[0][0] == matrix[1][1] == matrix[2][2]:
+            return matrix[0][0]
+        if " " != matrix[2][0] == matrix[1][1] == matrix[0][2]:
+            return matrix[2][0]
 # leetcode submit region end(Prohibit modification and deletion)
