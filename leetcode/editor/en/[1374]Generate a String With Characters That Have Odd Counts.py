d52b345d5b84e7135a9fed6f49d8607af7255b39
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-04 22:47:19 +0800

    add solution of leetcode 1374 generate a string with characters that have odd counts.py

--- leetcode/editor/en/[1374]Generate a String With Characters That Have Odd Counts.py
+++ leetcode/editor/en/[1374]Generate a String With Characters That Have Odd Counts.py
@@ -43,5 +43,9 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def generateTheString(self, n: int) -> str:
-        
+        if n % 2 == 1:
+            return "a" * n
+        else:
+            return "a" * (n - 1) + "b"
+
 # leetcode submit region end(Prohibit modification and deletion)
