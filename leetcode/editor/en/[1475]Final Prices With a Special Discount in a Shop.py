fcd6fc49d0e187f9b187dd3a2827e67adb7d829f
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-07 22:22:56 +0800

    add solution of leetcode 1475 find prices with a special discount in a shop.py

--- leetcode/editor/en/[1475]Final Prices With a Special Discount in a Shop.py
+++ leetcode/editor/en/[1475]Final Prices With a Special Discount in a Shop.py
@@ -1,16 +1,16 @@
 # Given the array prices where prices[i] is the price of the ith item in a shop.
-#  There is a special discount for items in the shop, if you buy the ith item, 
-# then you will receive a discount equivalent to prices[j] where j is the minimum 
-# index such that j > i and prices[j] <= prices[i], otherwise, you will not receive 
-# any discount at all. 
-# 
-#  Return an array where the ith element is the final price you will pay for 
-# the ith item of the shop considering the special discount. 
-# 
-#  
-#  Example 1: 
-# 
-#  
+#  There is a special discount for items in the shop, if you buy the ith item,
+# then you will receive a discount equivalent to prices[j] where j is the minimum
+# index such that j > i and prices[j] <= prices[i], otherwise, you will not receive
+# any discount at all.
+#
+#  Return an array where the ith element is the final price you will pay for
+# the ith item of the shop considering the special discount.
+#
+#
+#  Example 1:
+#
+#
 # Input: prices = [8,4,6,2,3]
 # Output: [4,2,4,2,3]
 # Explanation: 
@@ -21,36 +21,45 @@
 # For item 2 with price[2]=6 you will receive a discount equivalent to prices[3]
 # =2, therefore, the final price you will pay is 6 - 2 = 4. 
 # For items 3 and 4 you will not receive any discount at all.
-#  
-# 
-#  Example 2: 
-# 
-#  
+#
+#
+#  Example 2:
+#
+#
 # Input: prices = [1,2,3,4,5]
 # Output: [1,2,3,4,5]
-# Explanation: In this case, for all items, you will not receive any discount 
+# Explanation: In this case, for all items, you will not receive any discount
 # at all.
-#  
-# 
-#  Example 3: 
-# 
-#  
+#
+#
+#  Example 3:
+#
+#
 # Input: prices = [10,1,1,6]
 # Output: [9,0,1,6]
-#  
-# 
-#  
-#  Constraints: 
-# 
-#  
-#  1 <= prices.length <= 500 
-#  1 <= prices[i] <= 10^3 
-#  
+#
+#
+#
+#  Constraints:
+#
+#
+#  1 <= prices.length <= 500
+#  1 <= prices[i] <= 10^3
+#
 #  Related Topics Array Stack Monotonic Stack 👍 910 👎 62
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        
+        stack = []
+        for i in range(len(prices) - 1, -1, -1):
+            tmp = prices[i]
+            while stack and stack[-1] > tmp:
+                stack.pop()
+            if stack:
+                prices[i] -= stack[-1]
+            stack.append(tmp)
+        return prices
+
 # leetcode submit region end(Prohibit modification and deletion)
