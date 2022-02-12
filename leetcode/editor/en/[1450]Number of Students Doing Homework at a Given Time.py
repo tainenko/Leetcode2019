454221329a2dae28f2f2a36094179fbbd9beb748
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-13 02:26:26 +0800

    add solution of leetcode 1480 number of students doing homework at a given time.py

--- leetcode/editor/en/[1450]Number of Students Doing Homework at a Given Time.py
+++ leetcode/editor/en/[1450]Number of Students Doing Homework at a Given Time.py
@@ -46,5 +46,10 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        
+        cnt = 0
+        for start, end in zip(startTime, endTime):
+            if start <= queryTime <= end:
+                cnt += 1
+        return cnt
+
 # leetcode submit region end(Prohibit modification and deletion)
