00c95668783b820384d42cdedce19ef0287e9596
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-13 19:56:16 +0800

    add solution of leetcode 1437 check if all 1's are at least length k places away.py

--- leetcode/editor/en/[1437]Check If All 1's Are at Least Length K Places Away.py
+++ leetcode/editor/en/[1437]Check If All 1's Are at Least Length K Places Away.py
@@ -1,36 +1,45 @@
-# Given an binary array nums and an integer k, return true if all 1's are at 
-# least k places away from each other, otherwise return false. 
-# 
-#  
-#  Example 1: 
-# 
-#  
+# Given an binary array nums and an integer k, return true if all 1's are at
+# least k places away from each other, otherwise return false.
+#
+#
+#  Example 1:
+#
+#
 # Input: nums = [1,0,0,0,1,0,0,1], k = 2
 # Output: true
 # Explanation: Each of the 1s are at least 2 places away from each other.
-#  
-# 
-#  Example 2: 
-# 
-#  
+#
+#
+#  Example 2:
+#
+#
 # Input: nums = [1,0,0,1,0,1], k = 2
 # Output: false
 # Explanation: The second 1 and third 1 are only one apart from each other.
-#  
-# 
-#  
-#  Constraints: 
-# 
-#  
-#  1 <= nums.length <= 10⁵ 
-#  0 <= k <= nums.length 
-#  nums[i] is 0 or 1 
-#  
+#
+#
+#
+#  Constraints:
+#
+#
+#  1 <= nums.length <= 10⁵
+#  0 <= k <= nums.length
+#  nums[i] is 0 or 1
+#
 #  Related Topics Array 👍 367 👎 190
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
-        
+        dis = float('inf')
+        for num in nums:
+            if num == 1:
+                if dis < k:
+                    return False
+                dis = 0
+            else:
+                dis += 1
+        return True
+
 # leetcode submit region end(Prohibit modification and deletion)
