6fa9e2ac00b8081a6ac8aec76f6a150264c82401
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-17 23:49:33 +0800

    add solution of leetcode 1408 string matching in an array.py

---
+++ leetcode/editor/en/[1408]String Matching in an Array.py
@@ -0,0 +1,49 @@
+# Given an array of string words. Return all strings in words which is 
+# substring of another word in any order. 
+# 
+#  String words[i] is substring of words[j], if can be obtained removing some 
+# characters to left and/or right side of words[j]. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: words = ["mass","as","hero","superhero"]
+# Output: ["as","hero"]
+# Explanation: "as" is substring of "mass" and "hero" is substring of 
+# "superhero".
+# ["hero","as"] is also a valid answer.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: words = ["leetcode","et","code"]
+# Output: ["et","code"]
+# Explanation: "et", "code" are substring of "leetcode".
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: words = ["blue","green","bu"]
+# Output: []
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= words.length <= 100 
+#  1 <= words[i].length <= 30 
+#  words[i] contains only lowercase English letters. 
+#  It's guaranteed that words[i] will be unique. 
+#  
+#  Related Topics String String Matching 👍 478 👎 70
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
