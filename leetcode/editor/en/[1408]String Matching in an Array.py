802cfa1416b32942f99cddff28947b160b63ee2e
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-18 00:01:20 +0800

    add solution of leetcode 1408 string matching in an array.py

--- leetcode/editor/en/[1408]String Matching in an Array.py
+++ leetcode/editor/en/[1408]String Matching in an Array.py
@@ -45,5 +45,14 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        
+        res = []
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i == j:
+                    continue
+                if words[i] in words[j]:
+                    res.append(words[i])
+                    break
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
