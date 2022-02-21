ece64d3873fed803d81ea3cb58c33cf375fa5431
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-21 23:20:47 +0800

    init problem of leetcode 169 majorit element.py

---
+++ leetcode/editor/en/[169]Majority Element.py
@@ -0,0 +1,32 @@
+# Given an array nums of size n, return the majority element. 
+# 
+#  The majority element is the element that appears more than ⌊n / 2⌋ times. 
+# You may assume that the majority element always exists in the array. 
+# 
+#  
+#  Example 1: 
+#  Input: nums = [3,2,3]
+# Output: 3
+#  Example 2: 
+#  Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+#  
+#  
+#  Constraints: 
+# 
+#  
+#  n == nums.length 
+#  1 <= n <= 5 * 10⁴ 
+#  -2³¹ <= nums[i] <= 2³¹ - 1 
+#  
+# 
+#  
+# Follow-up: Could you solve the problem in linear time and in O(1) space? 
+# Related Topics Array Hash Table Divide and Conquer Sorting Counting 👍 8632 👎 316
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
