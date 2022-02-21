455eddbff98b40283d27fe5e55f5285aa23fa661
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-21 23:22:37 +0800

    add solution of leetcode 169 majority element.py

--- leetcode/editor/en/[169]Majority Element.py
+++ leetcode/editor/en/[169]Majority Element.py
@@ -28,5 +28,16 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
+        curr = -1
+        cnt = 0
+        for num in nums:
+            if num == curr:
+                cnt += 1
+            else:
+                cnt -= 1
+            if cnt < 0:
+                curr = num
+                cnt = 1
+        return curr
+
 # leetcode submit region end(Prohibit modification and deletion)
