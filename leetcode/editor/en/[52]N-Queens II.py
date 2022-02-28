8dbde112ef058f2fe0b5004193f79e32521496a4
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-02-28 21:13:54 +0800

    add solution of leetcode 51 n-queens ii.py

--- leetcode/editor/en/[52]N-Queens II.py
+++ leetcode/editor/en/[52]N-Queens II.py
@@ -33,5 +33,27 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def totalNQueens(self, n: int) -> int:
+        board = [-1] * n
+        res = 0
+
+        def helper(board, row):
+            if row == len(board):
+                nonlocal res
+                res += 1
+                return
+            for col in range(n):
+                board[row] = col
+                if self.is_valid(board, row, col):
+                    helper(board, row + 1)
+                board[row] = -1
+
+        helper(board, 0)
+        return res
+
+    def is_valid(self, board, row, col):
+        for pre_row in range(row):
+            if board[pre_row] == -1 or board[pre_row] == col or abs(pre_row - row) == abs(board[pre_row] - col):
+                return False
+        return True
 
 # leetcode submit region end(Prohibit modification and deletion)
