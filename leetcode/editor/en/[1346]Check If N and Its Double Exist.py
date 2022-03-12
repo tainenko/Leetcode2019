6bb963906c736dca6eaa430c51bcd320e4f34716
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-12 23:29:40 +0800

    Add solution of leetcode 1346 check if n and its double exist.py

--- leetcode/editor/en/[1346]Check If N and Its Double Exist.py
+++ leetcode/editor/en/[1346]Check If N and Its Double Exist.py
@@ -48,5 +48,11 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        
+        nums = set()
+        for num in arr:
+            if num * 2 in nums or num / 2 in nums:
+                return True
+            nums.add(num)
+        return False
+
 # leetcode submit region end(Prohibit modification and deletion)
