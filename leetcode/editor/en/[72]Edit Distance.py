ebf990af79fab6f3ee72cbd0bd79ce71bcff0883
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-13 00:07:25 +0800

    add solution of leetocde 72 edit distance.py

--- leetcode/editor/en/[72]Edit Distance.py
+++ leetcode/editor/en/[72]Edit Distance.py
@@ -52,5 +52,18 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
-        
+        l1 = len(word1)
+        l2 = len(word2)
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+        for i in range(l1 + 1):
+            dp[i][0] = i
+        for j in range(l2 + 1):
+            dp[0][j] = j
+        for i in range(1, l1 + 1):
+            for j in range(1, l2 + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j])
+        return dp[l1][l2]
 # leetcode submit region end(Prohibit modification and deletion)
