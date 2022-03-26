8714fa588eaea4ab37b3ed571986c04d2c05e9a2
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-27 01:11:35 +0800

    init problem of leetcode 1486 xor operation in an array.py

---
+++ leetcode/editor/en/[1486]XOR Operation in an Array.py
@@ -0,0 +1,42 @@
+# You are given an integer n and an integer start. 
+# 
+#  Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.
+# length. 
+# 
+#  Return the bitwise XOR of all elements of nums. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: n = 5, start = 0
+# Output: 8
+# Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) 
+# = 8.
+# Where "^" corresponds to bitwise XOR operator.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: n = 4, start = 3
+# Output: 8
+# Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= n <= 1000 
+#  0 <= start <= 1000 
+#  n == nums.length 
+#  
+#  Related Topics Math Bit Manipulation 👍 776 👎 281
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
