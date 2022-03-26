d6c4f957bf95e0dd1d742a965282ddefc8b71848
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-27 01:48:12 +0800

    init problem of leetcode 1491 average salary excluding the minimum and miximum salary.py

---
+++ leetcode/editor/en/[1491]Average Salary Excluding the Minimum and Maximum Salary.py
@@ -0,0 +1,43 @@
+# You are given an array of unique integers salary where salary[i] is the 
+# salary of the iᵗʰ employee. 
+# 
+#  Return the average salary of employees excluding the minimum and maximum 
+# salary. Answers within 10⁻⁵ of the actual answer will be accepted. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: salary = [4000,3000,1000,2000]
+# Output: 2500.00000
+# Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+# 
+# Average salary excluding minimum and maximum salary is (2000+3000) / 2 = 2500
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: salary = [1000,2000,3000]
+# Output: 2000.00000
+# Explanation: Minimum salary and maximum salary are 1000 and 3000 respectively.
+# 
+# Average salary excluding minimum and maximum salary is (2000) / 1 = 2000
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  3 <= salary.length <= 100 
+#  1000 <= salary[i] <= 10⁶ 
+#  All the integers of salary are unique. 
+#  
+#  Related Topics Array Sorting 👍 570 👎 85
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        
+# leetcode submit region end(Prohibit modification and deletion)
