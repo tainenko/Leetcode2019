8c8a46089e18e08da2825e574bd51bf4ab35c34c
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-29 22:01:20 +0800

    init problem of 1502 can make arithmetic pregression from sequence.py

---
+++ leetcode/editor/en/[1502]Can Make Arithmetic Progression From Sequence.py
@@ -0,0 +1,41 @@
+# A sequence of numbers is called an arithmetic progression if the difference 
+# between any two consecutive elements is the same. 
+# 
+#  Given an array of numbers arr, return true if the array can be rearranged to 
+# form an arithmetic progression. Otherwise, return false. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: arr = [3,5,1]
+# Output: true
+# Explanation: We can reorder the elements as [1,3,5] or [5,3,1] with 
+# differences 2 and -2 respectively, between each consecutive elements.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: arr = [1,2,4]
+# Output: false
+# Explanation: There is no way to reorder the elements to obtain an arithmetic 
+# progression.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  2 <= arr.length <= 1000 
+#  -10⁶ <= arr[i] <= 10⁶ 
+#  
+#  Related Topics Array Sorting 👍 621 👎 41
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        
+        
+# leetcode submit region end(Prohibit modification and deletion)
