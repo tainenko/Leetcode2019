4610d95dcb1a3504d35327a707700a9a2d1c5319
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-29 22:05:26 +0800

    Add solution of leetcode  1550 three consecutive odds.py

--- leetcode/editor/en/[1550]Three Consecutive Odds.py
+++ leetcode/editor/en/[1550]Three Consecutive Odds.py
@@ -30,5 +30,14 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        
+        cnt = 0
+        for num in arr:
+            if num % 2 == 1:
+                cnt += 1
+            else:
+                cnt = 0
+            if cnt >= 3:
+                return True
+        return False
+
 # leetcode submit region end(Prohibit modification and deletion)
