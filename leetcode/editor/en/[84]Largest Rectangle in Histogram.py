5f15612e21ae2fc962f04a7f32f82d7198308667
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-29 23:34:15 +0800

    init problem of leetcode 84 largest rectangle in histogram.py

---
+++ leetcode/editor/en/[84]Largest Rectangle in Histogram.py
@@ -0,0 +1,36 @@
+# Given an array of integers heights representing the histogram's bar height 
+# where the width of each bar is 1, return the area of the largest rectangle in the 
+# histogram. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: heights = [2,1,5,6,2,3]
+# Output: 10
+# Explanation: The above is a histogram where width of each bar is 1.
+# The largest rectangle is shown in the red area, which has an area = 10 units.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: heights = [2,4]
+# Output: 4
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= heights.length <= 10⁵ 
+#  0 <= heights[i] <= 10⁴ 
+#  
+#  Related Topics Array Stack Monotonic Stack 👍 9458 👎 139
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
