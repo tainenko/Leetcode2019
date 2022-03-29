c7abd4362ab7926810ba26aa4a9f0cdf92f8adb8
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-29 22:03:17 +0800

    Add solution of leetcode 1502 can make arithmetic progression from sequence.py

--- leetcode/editor/en/[1502]Can Make Arithmetic Progression From Sequence.py
+++ leetcode/editor/en/[1502]Can Make Arithmetic Progression From Sequence.py
@@ -36,6 +36,13 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
-        
-        
+        if len(arr) <= 2:
+            return True
+        arr.sort()
+        diff = arr[1] - arr[0]
+        for i in range(2, len(arr)):
+            if arr[i - 1] + diff != arr[i]:
+                return False
+        return True
+
 # leetcode submit region end(Prohibit modification and deletion)
