26af95eec1bf2828b3a18c78e51e51b701880ff7
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-30 22:27:20 +0800

    add solution of leetcode 84 largest rectangle in histogram.py

--- leetcode/editor/en/[84]Largest Rectangle in Histogram.py
+++ leetcode/editor/en/[84]Largest Rectangle in Histogram.py
@@ -32,5 +32,19 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        
+        stack = []
+        res = 0
+        heights.append(0)
+        for i in range(len(heights)):
+            if not stack or heights[i] > heights[stack[-1]]:
+                stack.append(i)
+            else:
+                while stack and heights[i] <= heights[stack[-1]]:
+                    h = heights[stack[-1]]
+                    stack.pop()
+                    w = i if not stack else i - stack[-1] - 1
+                    res = max(res, h * w)
+                stack.append(i)
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
