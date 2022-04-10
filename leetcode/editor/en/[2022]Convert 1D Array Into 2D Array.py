6c62338d9c7e0dcdedd5dc57f80a48a624c5b2ce
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-04-11 01:23:37 +0800

    add solution of leetcode 2022 convert 1d array into 2d array.py

--- leetcode/editor/en/[2022]Convert 1D Array Into 2D Array.py
+++ leetcode/editor/en/[2022]Convert 1D Array Into 2D Array.py
@@ -56,5 +56,8 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        
+        if len(original) != m * n:
+            return []
+        return [original[i * n:(i + 1) * n] for i in range(m)]
+
 # leetcode submit region end(Prohibit modification and deletion)
