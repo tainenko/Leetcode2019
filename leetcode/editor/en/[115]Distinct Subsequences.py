28dfb3807a847c529a65755c779a822ae3d75d2c
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-04-12 23:40:13 +0800

    init problem of leetcode 115 distinct subsequences.py

---
+++ leetcode/editor/en/[115]Distinct Subsequences.py
@@ -0,0 +1,52 @@
+# Given two strings s and t, return the number of distinct subsequences of s 
+# which equals t. 
+# 
+#  A string's subsequence is a new string formed from the original string by 
+# deleting some (can be none) of the characters without disturbing the remaining 
+# characters' relative positions. (i.e., "ACE" is a subsequence of "ABCDE" while 
+# "AEC" is not). 
+# 
+#  The test cases are generated so that the answer fits on a 32-bit signed 
+# integer. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: s = "rabbbit", t = "rabbit"
+# Output: 3
+# Explanation:
+# As shown below, there are 3 ways you can generate "rabbit" from S.
+# rabbbit
+# rabbbit
+# rabbbit
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: s = "babgbag", t = "bag"
+# Output: 5
+# Explanation:
+# As shown below, there are 5 ways you can generate "bag" from S.
+# babgbag
+# babgbag
+# babgbag
+# babgbag
+# babgbag 
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length, t.length <= 1000 
+#  s and t consist of English letters. 
+#  
+#  Related Topics String Dynamic Programming 👍 3299 👎 146
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
