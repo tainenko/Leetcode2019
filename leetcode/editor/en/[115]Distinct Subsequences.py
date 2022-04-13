99d0616f80ddfeec528a49e85860a88cc363c73b
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-04-14 01:05:53 +0800

    add solution of leetcode 115 idstinct subsequences.py

--- leetcode/editor/en/[115]Distinct Subsequences.py
+++ leetcode/editor/en/[115]Distinct Subsequences.py
@@ -48,5 +48,15 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        
+        m, n = len(s), len(t)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        for j in range(m + 1):
+            dp[0][j] = 1
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if t[i - 1] == s[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]
+                else:
+                    dp[i][j] = dp[i][j - 1]
+        return dp[-1][-1]
 # leetcode submit region end(Prohibit modification and deletion)
