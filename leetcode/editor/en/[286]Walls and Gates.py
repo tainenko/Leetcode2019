515422ce0c5ab46526b444c610e748a2fa7aa100
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-05-12 00:03:41 +0800

    add solution of leetcode 256 walls and gates.py

--- leetcode/editor/en/[286]Walls and Gates.py
+++ leetcode/editor/en/[286]Walls and Gates.py
@@ -46,5 +46,21 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        
+        def bfs(x, y, val):
+            if x < 0 or x >= len(rooms) or y < 0 or y >= len(rooms[0]) or rooms[x][y] <= val:
+                return
+            rooms[x][y] = val
+            bfs(x - 1, y, val + 1)
+            bfs(x, y - 1, val + 1)
+            bfs(x + 1, y, val + 1)
+            bfs(x, y + 1, val + 1)
+
+        for i in range(len(rooms)):
+            for j in range(len(rooms[0])):
+                if rooms[i][j] == 0:
+                    bfs(i - 1, j, 1)
+                    bfs(i, j - 1, 1)
+                    bfs(i + 1, j, 1)
+                    bfs(i, j + 1, 1)
+
 # leetcode submit region end(Prohibit modification and deletion)
