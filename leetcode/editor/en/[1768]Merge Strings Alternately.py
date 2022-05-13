0c9aec808fa2921061f9623e8564b828603f6b61
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-05-14 00:37:14 +0800

    add solution of leetcode 1768 merge strings alternately.py

--- leetcode/editor/en/[1768]Merge Strings Alternately.py
+++ leetcode/editor/en/[1768]Merge Strings Alternately.py
@@ -50,5 +50,18 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
+        i = 0
+        j = 0
+        res = ""
+        while i < len(word1) and j < len(word2):
+            res += word1[i]
+            res += word2[j]
+            i += 1
+            j += 1
+        if i < len(word1):
+            res += word1[i:]
+        if j < len(word2):
+            res += word2[j:]
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
