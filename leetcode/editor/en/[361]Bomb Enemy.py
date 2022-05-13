34087cc5693107f8f30332b13439df9279be1090
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-05-13 23:36:39 +0800

    add solution of leetcode 361 bomb enemy.py

--- leetcode/editor/en/[361]Bomb Enemy.py
+++ leetcode/editor/en/[361]Bomb Enemy.py
@@ -35,5 +35,36 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def maxKilledEnemies(self, grid: List[List[str]]) -> int:
-        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 'E':
+                    self.bfs(i, j, grid)
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j].isdigit():
+                    res = max(int(grid[i][j]), res)
+        return res
+
+    def bfs(self, i, j, grid):
+        for x in range(i, -1, -1):
+            if grid[x][j] == 'W':
+                break
+            if grid[x][j].isdigit():
+                grid[x][j] = str(int(grid[x][j]) + 1)
+        for x in range(i + 1, len(grid)):
+            if grid[x][j] == 'W':
+                break
+            if grid[x][j].isdigit():
+                grid[x][j] = str(int(grid[x][j]) + 1)
+        for y in range(j, -1, -1):
+            if grid[i][y] == 'W':
+                break
+            if grid[i][y].isdigit():
+                grid[i][y] = str(int(grid[i][y]) + 1)
+        for y in range(j + 1, len(grid[0])):
+            if grid[i][y] == 'W':
+                break
+            if grid[i][y].isdigit():
+                grid[i][y] = str(int(grid[i][y]) + 1)
 # leetcode submit region end(Prohibit modification and deletion)
