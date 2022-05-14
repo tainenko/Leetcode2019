87f862b58fe84249dc71df8c9e0930de3353b158
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-05-14 21:53:43 +0800

    add solution of leetcode 1763 longest nice substring.py

--- leetcode/editor/en/[1763]Longest Nice Substring.py
+++ leetcode/editor/en/[1763]Longest Nice Substring.py
@@ -49,5 +49,18 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        
+        def is_nice(s):
+            m = set(s)
+            for c in s:
+                if c.swapcase() not in m:
+                    return False
+            return True
+
+        res = ""
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                if is_nice(s[i:j]) and len(s[i:j]) > len(res):
+                    res = s[i:j]
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
