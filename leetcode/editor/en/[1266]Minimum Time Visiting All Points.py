3d1623468039a31ff883ff02c865c38a3217674c
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-05-24 00:11:43 +0800

    add solution of leetcode 1266 minimum time visiting all points.py

--- leetcode/editor/en/[1266]Minimum Time Visiting All Points.py
+++ leetcode/editor/en/[1266]Minimum Time Visiting All Points.py
@@ -53,5 +53,9 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        
+        t = 0
+        for i in range(1, len(points)):
+            t += max([abs(points[i][0] - points[i - 1][0]), abs(points[i][1] - points[i - 1][1])])
+        return t
+
 # leetcode submit region end(Prohibit modification and deletion)
