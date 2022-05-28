27c12ffa4705c7ce30826761595e453c31824529
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-05-29 00:24:41 +0800

    add solution of leetcode 1640 check array formation through concatenation.py

---
+++ leetcode/editor/en/[1640]Check Array Formation Through Concatenation.py
@@ -0,0 +1,58 @@
+# You are given an array of distinct integers arr and an array of integer 
+# arrays pieces, where the integers in pieces are distinct. Your goal is to form arr by 
+# concatenating the arrays in pieces in any order. However, you are not allowed 
+# to reorder the integers in each array pieces[i]. 
+# 
+#  Return true if it is possible to form the array arr from pieces. Otherwise, 
+# return false. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: arr = [15,88], pieces = [[88],[15]]
+# Output: true
+# Explanation: Concatenate [15] then [88]
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: arr = [49,18,16], pieces = [[16,18,49]]
+# Output: false
+# Explanation: Even though the numbers match, we cannot reorder pieces[0].
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: arr = [91,4,64,78], pieces = [[78],[4,64],[91]]
+# Output: true
+# Explanation: Concatenate [91] then [4,64] then [78]
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= pieces.length <= arr.length <= 100 
+#  sum(pieces[i].length) == arr.length 
+#  1 <= pieces[i].length <= arr.length 
+#  1 <= arr[i], pieces[i][j] <= 100 
+#  The integers in arr are distinct. 
+#  The integers in pieces are distinct (i.e., If we flatten pieces in a 1D 
+# array, all the integers in this array are distinct). 
+#  
+#  Related Topics Array Hash Table 👍 687 👎 114
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        d = {piece[0]: piece for piece in pieces}
+        res = []
+        for item in arr:
+            res += d.get(item, [])
+        return res == arr
+
+# leetcode submit region end(Prohibit modification and deletion)
