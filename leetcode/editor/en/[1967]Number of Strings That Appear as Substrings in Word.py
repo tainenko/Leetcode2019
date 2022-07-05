bebad309ad3202287466b27448be6b2613b19bc9
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-07-06 00:51:29 +0800

    add solution of leetcode 1967 number of styring s that appear as substrings in word

--- leetcode/editor/en/[1967]Number of Strings That Appear as Substrings in Word.py
+++ leetcode/editor/en/[1967]Number of Strings That Appear as Substrings in Word.py
@@ -52,5 +52,10 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def numOfStrings(self, patterns: List[str], word: str) -> int:
-        
+        cnt = 0
+        for pattern in patterns:
+            if pattern in word:
+                cnt += 1
+        return cnt
+
 # leetcode submit region end(Prohibit modification and deletion)
