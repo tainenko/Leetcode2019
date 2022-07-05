5dd617a3a98d08005b5862c798f357fdf502f9f5
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-07-06 01:05:46 +0800

    add solution of leetcode 1952 three divisors.py

--- leetcode/editor/en/[1952]Three Divisors.py
+++ leetcode/editor/en/[1952]Three Divisors.py
@@ -31,7 +31,16 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+import math
+
+
 class Solution:
     def isThree(self, n: int) -> bool:
-        
+        cnt = 0
+        for i in range(1, n + 1):
+            if n % i == 0:
+                cnt += 1
+            if cnt > 3:
+                return False
+        return cnt == 3
 # leetcode submit region end(Prohibit modification and deletion)
