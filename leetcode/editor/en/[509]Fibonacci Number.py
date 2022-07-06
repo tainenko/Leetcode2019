aecbba0c6aaa7b2c02a9562a2ccd63a0bcf14b19
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-07-07 01:04:44 +0800

    add solution of leetcode fibonacci number.py

--- leetcode/editor/en/[509]Fibonacci Number.py
+++ leetcode/editor/en/[509]Fibonacci Number.py
@@ -46,5 +46,13 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def fib(self, n: int) -> int:
-        
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        prev = 0
+        curr = 1
+        for _ in range(n - 1):
+            prev, curr = curr, prev + curr
+        return curr
 # leetcode submit region end(Prohibit modification and deletion)
