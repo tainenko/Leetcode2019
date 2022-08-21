4e1068611d59aeb8150c7c0f49a8431f54d37b53
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-08-21 23:57:38 +0800

    add solution of leetcode 2032 twot out of three

--- leetcode/editor/en/[2032]Two Out of Three.py
+++ leetcode/editor/en/[2032]Two Out of Three.py
@@ -46,5 +46,6 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
-        
+        s1, s2, s3 = set(nums1), set(nums2), set(nums3)
+        return list((s1 & s2) | (s2 & s3) | (s1 & s3))
 # leetcode submit region end(Prohibit modification and deletion)
