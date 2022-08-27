e1accbb81d2a5bdebe126bca47201a71806bf924
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-08-27 23:41:28 +0800

    add solution of leetcode 562 longest line of consecutive one in matrix

--- leetcode/editor/en/[562]Longest Line of Consecutive One in Matrix.py
+++ leetcode/editor/en/[562]Longest Line of Consecutive One in Matrix.py
@@ -35,5 +35,24 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def longestLine(self, mat: List[List[int]]) -> int:
-        
+        m = len(mat)
+        n = len(mat[0])
+        dp = [[[0, 0, 0, 0] for _ in range(n)] for _ in range(m)]
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    continue
+                dp[i][j] = [1, 1, 1, 1]
+                if i > 0:
+                    dp[i][j][0] += dp[i - 1][j][0]
+                if j > 0:
+                    dp[i][j][1] += dp[i][j - 1][1]
+                if i > 0 and j > 0:
+                    dp[i][j][2] += dp[i - 1][j - 1][2]
+                if i > 0 and j < n - 1:
+                    dp[i][j][3] += dp[i - 1][j + 1][3]
+                res = max(res, *dp[i][j])
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
