c58e229465c9956ab27a3427b9fd6c029301e523
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-08-28 11:21:56 +0800

    init problem of leetcode 1791 find center of start graph

---
+++ leetcode/editor/en/[1791]Find Center of Star Graph.py
@@ -0,0 +1,45 @@
+# There is an undirected star graph consisting of n nodes labeled from 1 to n. 
+# A star graph is a graph where there is one center node and exactly n - 1 edges 
+# that connect the center node with every other node. 
+# 
+#  You are given a 2D integer array edges where each edges[i] = [ui, vi] 
+# indicates that there is an edge between the nodes ui and vi. Return the center of the 
+# given star graph. 
+# 
+#  
+#  Example 1: 
+#  
+#  
+# Input: edges = [[1,2],[2,3],[4,2]]
+# Output: 2
+# Explanation: As shown in the figure above, node 2 is connected to every other 
+# node, so 2 is the center.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: edges = [[1,2],[5,1],[1,3],[1,4]]
+# Output: 1
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  3 <= n <= 10⁵ 
+#  edges.length == n - 1 
+#  edges[i].length == 2 
+#  1 <= ui, vi <= n 
+#  ui != vi 
+#  The given edges represent a valid star graph. 
+#  
+# 
+#  Related Topics Graph 👍 772 👎 98
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
