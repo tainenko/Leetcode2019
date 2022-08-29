272c2b6d25eca582a1ef2f1cd6bf0626c3c8fa51
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-08-29 23:00:13 +0800

    add solution of leetcode 1688 count of matches in tournament

--- leetcode/editor/en/[1688]Count of Matches in Tournament.py
+++ leetcode/editor/en/[1688]Count of Matches in Tournament.py
@@ -52,5 +52,14 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        
+        match = 0
+        while n > 1:
+            if n % 2 == 0:
+                match += n // 2
+                n //= 2
+            else:
+                match += (n - 1) // 2
+                n = (n - 1) // 2 + 1
+        return match
+
 # leetcode submit region end(Prohibit modification and deletion)
