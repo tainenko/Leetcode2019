98dd4acfbf74cd4f62149dee951b1d5d86dacf06
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-08-29 21:16:05 +0800

    add solution of leetcode 1566 defect pattern of length m repeated k or more times

--- leetcode/editor/en/[1566]Detect Pattern of Length M Repeated K or More Times.py
+++ leetcode/editor/en/[1566]Detect Pattern of Length M Repeated K or More Times.py
@@ -52,5 +52,11 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
-        
+        for i in range(0, len(arr) - m * k + 1):
+            target = arr[i:i + m] * k
+            for candidate in range(i, len(arr) - m * k + 1):
+                if target == arr[i:i + m * k]:
+                    return True
+        return False
+
 # leetcode submit region end(Prohibit modification and deletion)
