70b1949baace691819e9aa8a320a85fc276af81f
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-09-03 00:17:14 +0800

    add solution of leetcode 1893 check if all the integers in a range are covered

--- leetcode/editor/en/[1893]Check if All the Integers in a Range Are Covered.py
+++ leetcode/editor/en/[1893]Check if All the Integers in a Range Are Covered.py
@@ -1,47 +1,52 @@
-# You are given a 2D integer array ranges and two integers left and right. Each 
-# ranges[i] = [starti, endi] represents an inclusive interval between starti and 
-# endi. 
-# 
-#  Return true if each integer in the inclusive range [left, right] is covered 
-# by at least one interval in ranges. Return false otherwise. 
-# 
+# You are given a 2D integer array ranges and two integers left and right. Each
+# ranges[i] = [starti, endi] represents an inclusive interval between starti and
+# endi.
+#
+#  Return true if each integer in the inclusive range [left, right] is covered
+# by at least one interval in ranges. Return false otherwise.
+#
 #  An integer x is covered by an interval ranges[i] = [starti, endi] if starti <
-# = x <= endi. 
-# 
-#  
-#  Example 1: 
-# 
-#  
+# = x <= endi.
+#
+#
+#  Example 1:
+#
+#
 # Input: ranges = [[1,2],[3,4],[5,6]], left = 2, right = 5
 # Output: true
 # Explanation: Every integer between 2 and 5 is covered:
 # - 2 is covered by the first range.
 # - 3 and 4 are covered by the second range.
 # - 5 is covered by the third range.
-#  
-# 
-#  Example 2: 
-# 
-#  
+#
+#
+#  Example 2:
+#
+#
 # Input: ranges = [[1,10],[10,20]], left = 21, right = 21
 # Output: false
 # Explanation: 21 is not covered by any range.
-#  
-# 
-#  
-#  Constraints: 
-# 
-#  
-#  1 <= ranges.length <= 50 
-#  1 <= starti <= endi <= 50 
-#  1 <= left <= right <= 50 
-#  
-# 
+#
+#
+#
+#  Constraints:
+#
+#
+#  1 <= ranges.length <= 50
+#  1 <= starti <= endi <= 50
+#  1 <= left <= right <= 50
+#
+#
 #  Related Topics Array Hash Table Prefix Sum 👍 325 👎 62
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
-        
+        res = set()
+        for start, end in ranges:
+            for i in range(start, end + 1):
+                if left <= i <= right:
+                    res.add(i)
+        return len(res) == (right - left + 1)
 # leetcode submit region end(Prohibit modification and deletion)
