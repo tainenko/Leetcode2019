6b5b38eed65019b2e17384a78141e6beedf2024c
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-09-14 23:49:11 +0800

    add solution of leetcode 2341 maximum number of pairs in array

--- leetcode/editor/en/[2341]Maximum Number of Pairs in Array.py
+++ leetcode/editor/en/[2341]Maximum Number of Pairs in Array.py
@@ -1,21 +1,21 @@
-# You are given a 0-indexed integer array nums. In one operation, you may do 
-# the following: 
-# 
-#  
-#  Choose two integers in nums that are equal. 
-#  Remove both integers from nums, forming a pair. 
-#  
-# 
-#  The operation is done on nums as many times as possible. 
-# 
-#  Return a 0-indexed integer array answer of size 2 where answer[0] is the 
-# number of pairs that are formed and answer[1] is the number of leftover integers in 
-# nums after doing the operation as many times as possible. 
-# 
-#  
-#  Example 1: 
-# 
-#  
+# You are given a 0-indexed integer array nums. In one operation, you may do
+# the following:
+#
+#
+#  Choose two integers in nums that are equal.
+#  Remove both integers from nums, forming a pair.
+#
+#
+#  The operation is done on nums as many times as possible.
+#
+#  Return a 0-indexed integer array answer of size 2 where answer[0] is the
+# number of pairs that are formed and answer[1] is the number of leftover integers in
+# nums after doing the operation as many times as possible.
+#
+#
+#  Example 1:
+#
+#
 # Input: nums = [1,3,2,1,3,2,2]
 # Output: [3,1]
 # Explanation:
@@ -25,42 +25,50 @@
 # ,2,2].
 # Form a pair with nums[0] and nums[1] and remove them from nums. Now, nums = [2
 # ].
-# No more pairs can be formed. A total of 3 pairs have been formed, and there 
+# No more pairs can be formed. A total of 3 pairs have been formed, and there
 # is 1 number leftover in nums.
-#  
-# 
-#  Example 2: 
-# 
-#  
+#
+#
+#  Example 2:
+#
+#
 # Input: nums = [1,1]
 # Output: [1,0]
-# Explanation: Form a pair with nums[0] and nums[1] and remove them from nums. 
+# Explanation: Form a pair with nums[0] and nums[1] and remove them from nums.
 # Now, nums = [].
-# No more pairs can be formed. A total of 1 pair has been formed, and there are 
+# No more pairs can be formed. A total of 1 pair has been formed, and there are
 # 0 numbers leftover in nums.
-#  
-# 
-#  Example 3: 
-# 
-#  
+#
+#
+#  Example 3:
+#
+#
 # Input: nums = [0]
 # Output: [0,1]
 # Explanation: No pairs can be formed, and there is 1 number leftover in nums.
-#  
-# 
-#  
-#  Constraints: 
-# 
-#  
-#  1 <= nums.length <= 100 
-#  0 <= nums[i] <= 100 
-#  
-# 
+#
+#
+#
+#  Constraints:
+#
+#
+#  1 <= nums.length <= 100
+#  0 <= nums[i] <= 100
+#
+#
 #  Related Topics Array Hash Table Counting 👍 265 👎 7
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+from collections import Counter
+
+
 class Solution:
     def numberOfPairs(self, nums: List[int]) -> List[int]:
-        
+        pair = 0
+        left = 0
+        for val in Counter(nums).values():
+            pair += val // 2
+            left += val % 2
+        return [pair, left]
 # leetcode submit region end(Prohibit modification and deletion)
