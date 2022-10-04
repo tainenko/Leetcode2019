afcdeea482b1b53bd85024ed3dd394fd257d4c8d
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-10-04 22:27:18 +0800

    add solution of leetcode 2395 find subarrays with equal sum

---
+++ leetcode/editor/en/[2395]Find Subarrays With Equal Sum.py
@@ -0,0 +1,59 @@
+# Given a 0-indexed integer array nums, determine whether there exist two 
+# subarrays of length 2 with equal sum. Note that the two subarrays must begin at 
+# different indices. 
+# 
+#  Return true if these subarrays exist, and false otherwise. 
+# 
+#  A subarray is a contiguous non-empty sequence of elements within an array. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [4,2,4]
+# Output: true
+# Explanation: The subarrays with elements [4,2] and [2,4] have the same sum of 
+# 6.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [1,2,3,4,5]
+# Output: false
+# Explanation: No two subarrays of size 2 have the same sum.
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: nums = [0,0,0]
+# Output: true
+# Explanation: The subarrays [nums[0],nums[1]] and [nums[1],nums[2]] have the 
+# same sum of 0. 
+# Note that even though the subarrays have the same content, the two subarrays 
+# are considered different because they are in different positions in the original 
+# array.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  2 <= nums.length <= 1000 
+#  -10⁹ <= nums[i] <= 10⁹ 
+#  
+# 
+#  Related Topics Array Hash Table 👍 202 👎 10
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def findSubarrays(self, nums: List[int]) -> bool:
+        total = set()
+        for i in range(len(nums) - 1):
+            if nums[i] + nums[i + 1] in total:
+                return True
+            total.add(nums[i] + nums[i + 1])
+        return False
+# leetcode submit region end(Prohibit modification and deletion)
