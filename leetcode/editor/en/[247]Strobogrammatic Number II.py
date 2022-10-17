d95a70723b5a95914846241627f8c3267420f159
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-10-18 00:04:34 +0800

    init problem of leetcode 247 strobogrammatic number II

---
+++ leetcode/editor/en/[247]Strobogrammatic Number II.py
@@ -0,0 +1,30 @@
+# Given an integer n, return all the strobogrammatic numbers that are of length 
+# n. You may return the answer in any order. 
+# 
+#  A strobogrammatic number is a number that looks the same when rotated 180 
+# degrees (looked at upside down). 
+# 
+#  
+#  Example 1: 
+#  Input: n = 2
+# Output: ["11","69","88","96"]
+#  
+#  Example 2: 
+#  Input: n = 1
+# Output: ["0","1","8"]
+#  
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= n <= 14 
+#  
+# 
+#  Related Topics Array String Recursion 👍 826 👎 211
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def findStrobogrammatic(self, n: int) -> List[str]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
