b1cb4c359e30d55d15a3f0689298b88a1961cd4d
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-10-18 00:21:15 +0800

    add solution of leetcode 247 strobogrammatic number II

--- leetcode/editor/en/[247]Strobogrammatic Number II.py
+++ leetcode/editor/en/[247]Strobogrammatic Number II.py
@@ -26,5 +26,25 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def findStrobogrammatic(self, n: int) -> List[str]:
-        
+
+        records = dict()
+        records[1] = ["0", "1", "8"]
+        records[2] = ["11", "69", "88", "96"]
+        pair = ["00", "11", "69", "88", "96"]
+        if n <= 2:
+            return records[n]
+        count = 3
+        while count <= n:
+            record = []
+            if (count - 1) % 2 == 0:
+                for num in records[count - 1]:
+                    for item in records[1]:
+                        record.append(num[:len(num) // 2] + item + num[len(num) // 2:])
+            else:
+                for num in records[count - 2]:
+                    for item in pair:
+                        record.append(num[:len(num) // 2] + item + num[len(num) // 2:])
+            records[count] = record
+            count += 1
+        return records[n]
 # leetcode submit region end(Prohibit modification and deletion)
