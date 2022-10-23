372a29fd952475c78ecefea36ba89b901a344567
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-10-24 00:57:17 +0800

    add solution of leetcode 973 k closet points to origin

--- leetcode/editor/en/[973]K Closest Points to Origin.py
+++ leetcode/editor/en/[973]K Closest Points to Origin.py
@@ -44,5 +44,7 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        
+        points.sort(key=lambda x: x[0] ** 2 + x[1] ** 2)
+        return points[:k]
+
 # leetcode submit region end(Prohibit modification and deletion)
