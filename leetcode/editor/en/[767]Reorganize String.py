876f088dd2470e7386749b001c9ee95b35258056
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-10-24 00:43:17 +0800

    init problem of leetcode 767 reorganize string

---
+++ leetcode/editor/en/[767]Reorganize String.py
@@ -0,0 +1,31 @@
+# Given a string s, rearrange the characters of s so that any two adjacent 
+# characters are not the same. 
+# 
+#  Return any possible rearrangement of s or return "" if not possible. 
+# 
+#  
+#  Example 1: 
+#  Input: s = "aab"
+# Output: "aba"
+#  
+#  Example 2: 
+#  Input: s = "aaab"
+# Output: ""
+#  
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 500 
+#  s consists of lowercase English letters. 
+#  
+# 
+#  Related Topics Hash Table String Greedy Sorting Heap (Priority Queue) 
+# Counting 👍 5524 👎 191
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        
+# leetcode submit region end(Prohibit modification and deletion)
