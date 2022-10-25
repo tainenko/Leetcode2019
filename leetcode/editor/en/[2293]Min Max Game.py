2e041e05630c2f383188cd31b892a56cd3a87141
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-10-25 09:20:36 +0800

    add solution of leetcode 2293 min max game

--- leetcode/editor/en/[2293]Min Max Game.py
+++ leetcode/editor/en/[2293]Min Max Game.py
@@ -52,5 +52,13 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def minMaxGame(self, nums: List[int]) -> int:
-        
+        while len(nums) > 1:
+            new_nums = [0] * (len(nums) // 2)
+            for i in range(len(new_nums)):
+                if i % 2 == 0:
+                    new_nums[i] = min(nums[2 * i], nums[2 * i + 1])
+                else:
+                    new_nums[i] = max(nums[2 * i], nums[2 * i + 1])
+            nums = new_nums
+        return nums[0]
 # leetcode submit region end(Prohibit modification and deletion)
