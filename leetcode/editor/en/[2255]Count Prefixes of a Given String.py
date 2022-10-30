0a5efe60e9b9b41c5a679f05cdb9a0a6ffbec2be
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-10-30 23:24:34 +0800

    add solution of leetcode 2255 count prefixes of a given string

--- leetcode/editor/en/[2255]Count Prefixes of a Given String.py
+++ leetcode/editor/en/[2255]Count Prefixes of a Given String.py
@@ -42,5 +42,10 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def countPrefixes(self, words: List[str], s: str) -> int:
-        
+        total = 0
+        for word in words:
+            if s.startswith(word):
+                total += 1
+        return total
+
 # leetcode submit region end(Prohibit modification and deletion)
