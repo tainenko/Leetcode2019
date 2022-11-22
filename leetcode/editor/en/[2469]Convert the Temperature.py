49e46824f865c42ccf9562db427a937208a96a58
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-11-22 23:10:45 +0800

    add solution of leetcode 2469 convert the temperature

--- leetcode/editor/en/[2469]Convert the Temperature.py
+++ leetcode/editor/en/[2469]Convert the Temperature.py
@@ -46,5 +46,6 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        
+        return celsius + 273.15, celsius * 1.8 + 32
+
 # leetcode submit region end(Prohibit modification and deletion)
