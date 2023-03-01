2cd63961363ffd07932da277f06d2be5a892131e
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-03-01 22:53:34 +0800

    add solution of leetcode 2562 find the array concatenation value

--- leetcode/editor/en/[2562]Find the Array Concatenation Value.py
+++ leetcode/editor/en/[2562]Find the Array Concatenation Value.py
@@ -82,5 +82,15 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def findTheArrayConcVal(self, nums: List[int]) -> int:
-        
+        res = 0
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            res += int(str(nums[left]) + str(nums[right]))
+            left += 1
+            right -= 1
+        if left == right:
+            res += nums[left]
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
