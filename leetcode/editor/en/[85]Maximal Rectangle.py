a14e69980fb6107cc47f2334748b8a9e9145d587
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-03-16 00:21:31 +0800

    add solution of leetcode 85 maximal rectangle

--- leetcode/editor/en/[85]Maximal Rectangle.py
+++ leetcode/editor/en/[85]Maximal Rectangle.py
@@ -41,5 +41,32 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        
+        if not matrix or not (matrix[0]):
+            return 0
+        res = 0
+        m = len(matrix)
+        n = len(matrix[0])
+        height = [0] * n
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '0':
+                    height[j] = 0
+                else:
+                    height[j] += 1
+            res = max(res, self.max_rectangle(height))
+        return res
+
+    def max_rectangle(self, height):
+        stack = list()
+        res = 0
+        height.append(0)
+        for i in range(len(height)):
+            curr = height[i]
+            while stack and curr < height[stack[-1]]:
+                h = height[stack.pop()]
+                w = i if not stack else i - stack[-1] - 1
+                res = max(res, h * w)
+            stack.append(i)
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
