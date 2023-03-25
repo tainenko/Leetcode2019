a62026961297a5590ba3fdfdff85f83b064a97cc
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-03-26 01:19:48 +0800

    add solution of leetcode 267 palindrome permutation II

--- leetcode/editor/en/[267]Palindrome Permutation II.py
+++ leetcode/editor/en/[267]Palindrome Permutation II.py
@@ -25,7 +25,27 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+from collections import Counter
+from itertools import permutations
+
+
 class Solution:
     def generatePalindromes(self, s: str) -> List[str]:
-        
-# leetcode submit region end(Prohibit modification and deletion)
+        count = Counter(s)
+        mid = ""
+        partial = ""
+        for k, v in count.items():
+            partial += k * (v // 2)
+            mid += k * (v % 2)
+
+        if len(mid) > 1:
+            return []
+
+        res = []
+        for half in set(permutations(partial)):
+            word = "".join(half)
+            res.append(f"{word}{mid}{word[::-1]}")
+
+        return res
+
+        # leetcode submit region end(Prohibit modification and deletion)
