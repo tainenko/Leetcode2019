48419994b511136609292963e440866c8b253746
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-03-28 23:57:56 +0800

    add solution of leetcode 2525 categorize box according to criteria

--- leetcode/editor/en/[2525]Categorize Box According to Criteria.py
+++ leetcode/editor/en/[2525]Categorize Box According to Criteria.py
@@ -59,5 +59,14 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def categorizeBox(self, length: int, width: int, height: int, mass: int) -> str:
-        
+        is_bulky = length >= 10 ** 4 or width >= 10 ** 4 or height >= 10 ** 4 or length * width * height >= 10 ** 9
+        is_heavy = mass >= 100
+        if is_bulky and is_heavy:
+            return "Both"
+        elif not is_bulky and not is_heavy:
+            return "Neither"
+        elif is_bulky:
+            return "Bulky"
+        else:
+            return "Heavy"
 # leetcode submit region end(Prohibit modification and deletion)
