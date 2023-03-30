cde0ceef3cf65450dcc5758ea16022fd156659de
tainenko <31752048+tainenko@users.noreply.github.com>
2023-03-31 01:11:47 +0800

    add solution of leetcode 1603 design parking system

--- leetcode/editor/en/[1603]Design Parking System.py
+++ leetcode/editor/en/[1603]Design Parking System.py
@@ -53,11 +53,27 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        
+        self.big = big
+        self.medium = medium
+        self.small = small
 
     def addCar(self, carType: int) -> bool:
-        
-
+        if carType == 1:
+            if self.big == 0:
+                return False
+            self.big -= 1
+            return True
+        elif carType == 2:
+            if self.medium == 0:
+                return False
+            self.medium -= 1
+            return True
+        elif carType == 3:
+            if self.small == 0:
+                return False
+            self.small -= 1
+            return True
+        return False
 
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
