519982a3bad0981ceab98d02346bd6b38eaa7612
tainenko <31752048+tainenko@users.noreply.github.com>
2023-04-03 09:57:05 +0800

    init problem of leetcode 881 boats to save people

---
+++ leetcode/editor/en/[881]Boats to Save People.py
@@ -0,0 +1,48 @@
+# You are given an array people where people[i] is the weight of the iᵗʰ person,
+#  and an infinite number of boats where each boat can carry a maximum weight of 
+# limit. Each boat carries at most two people at the same time, provided the sum 
+# of the weight of those people is at most limit. 
+# 
+#  Return the minimum number of boats to carry every given person. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: people = [1,2], limit = 3
+# Output: 1
+# Explanation: 1 boat (1, 2)
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: people = [3,2,2,1], limit = 3
+# Output: 3
+# Explanation: 3 boats (1, 2), (2) and (3)
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: people = [3,5,3,4], limit = 5
+# Output: 4
+# Explanation: 4 boats (3), (3), (4), (5)
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= people.length <= 5 * 10⁴ 
+#  1 <= people[i] <= limit <= 3 * 10⁴ 
+#  
+# 
+#  Related Topics Array Two Pointers Greedy Sorting 👍 3796 👎 101
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
