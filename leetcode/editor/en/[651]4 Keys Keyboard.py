61d762689b091c2679e05e001ec9ef8b5b4d9e63
tainenko <31752048+tainenko@users.noreply.github.com>
2023-04-05 00:18:10 +0800

    add solution of leetcode 651 4 keys keyboard

--- leetcode/editor/en/[651]4 Keys Keyboard.py
+++ leetcode/editor/en/[651]4 Keys Keyboard.py
@@ -45,5 +45,11 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def maxA(self, n: int) -> int:
-        
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = dp[i - 1] + 1
+            for j in range(2, i):
+                dp[i] = max(dp[i], dp[j - 2] * (i - j + 1))
+        return dp[-1]
+
 # leetcode submit region end(Prohibit modification and deletion)
