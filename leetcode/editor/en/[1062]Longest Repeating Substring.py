13b52a2d74c9d165924fee106cee5800cf0c5749
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-04-06 23:34:33 +0800

    add solution of leetcode 1062 longest repeating substring

--- leetcode/editor/en/[1062]Longest Repeating Substring.py
+++ leetcode/editor/en/[1062]Longest Repeating Substring.py
@@ -42,5 +42,14 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
-        
+        n = len(s)
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        res = 0
+        for i in range(1, n + 1):
+            for j in range(i + 1, n + 1):
+                if s[i - 1] == s[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    res = max(res, dp[i][j])
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
