9b403e9c1fd96d224e960ded59e62e11b3b8744c
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-04-06 23:20:00 +0800

    init problem of leetcode 1062 longest repeating substring

---
+++ leetcode/editor/en/[1062]Longest Repeating Substring.py
@@ -0,0 +1,46 @@
+# Given a string s, return the length of the longest repeating substrings. If 
+# no repeating substring exists, return 0. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: s = "abcd"
+# Output: 0
+# Explanation: There is no repeating substring.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: s = "abbaba"
+# Output: 2
+# Explanation: The longest repeating substrings are "ab" and "ba", each of 
+# which occurs twice.
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: s = "aabcaabdaab"
+# Output: 3
+# Explanation: The longest repeating substring is "aab", which occurs 3 times.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 2000 
+#  s consists of lowercase English letters. 
+#  
+# 
+#  Related Topics String Binary Search Dynamic Programming Rolling Hash Suffix 
+# Array Hash Function 👍 599 👎 41
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def longestRepeatingSubstring(self, s: str) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
