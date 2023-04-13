f862b0d25186ec7cf7c28834b310fbfe3b2911ad
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-04-13 23:05:27 +0800

    add solution of leetcode 1167 minimum cost to connect sticks

--- leetcode/editor/en/[1167]Minimum Cost to Connect Sticks.py
+++ leetcode/editor/en/[1167]Minimum Cost to Connect Sticks.py
@@ -58,7 +58,17 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+import heapq
+
+
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
-        
+        res = 0
+        heapq.heapify(sticks)
+        while len(sticks) > 1:
+            x = heapq.heappop(sticks)
+            y = heapq.heappop(sticks)
+            heapq.heappush(sticks, x + y)
+            res += x + y
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
