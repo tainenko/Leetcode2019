d8178fcc0d25b206020ddbbe01fbfc0218e31874
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-04-20 21:35:01 +0800

    add solution of leetcode 1105 filling bookcase shelves

--- leetcode/editor/en/[1105]Filling Bookcase Shelves.py
+++ leetcode/editor/en/[1105]Filling Bookcase Shelves.py
@@ -56,5 +56,18 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-        
+        n = len(books)
+        dp = [100000] * (n + 1)
+        dp[0]=0
+        for i in range(1, n + 1):
+            w, h = 0, 0
+            j = i
+            while j > 0:
+                w += books[j - 1][0]
+                if w > shelfWidth:
+                    break
+                h = max(h, books[j - 1][1])
+                dp[i] = min(dp[i], dp[j - 1] + h)
+                j -= 1
+        return dp[-1]
 # leetcode submit region end(Prohibit modification and deletion)
