77dea407bd4df05e29c2e7d65f912d6cfdabd258
tainenko <31752048+tainenko@users.noreply.github.com>
2023-05-01 16:28:06 +0800

    init problem of leetcode 614 second degree follower

--- leetcode/editor/en/[614]Second Degree Follower.sql
+++ leetcode/editor/en/[614]Second Degree Follower.sql
@@ -64,5 +64,9 @@
 
 #leetcode submit region begin(Prohibit modification and deletion)
 # Write your MySQL query statement below
-
+select f1.follower, count(distinct(f2.follower)) num
+from follow as f1
+join follow as f2
+on f1.follower=f2.followee
+group by f1.follower
 #leetcode submit region end(Prohibit modification and deletion)
