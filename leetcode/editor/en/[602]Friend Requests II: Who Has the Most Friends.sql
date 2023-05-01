9f84c6116269f4db70ab712eab785c50a9339e26
tainenko <31752048+tainenko@users.noreply.github.com>
2023-05-01 13:15:16 +0800

    add solution of leetcode 602 friend requests

--- leetcode/editor/en/[602]Friend Requests II: Who Has the Most Friends.sql
+++ leetcode/editor/en/[602]Friend Requests II: Who Has the Most Friends.sql
@@ -56,5 +56,15 @@
 
 #leetcode submit region begin(Prohibit modification and deletion)
 # Write your MySQL query statement below
-
+select id, count(*) num
+from (
+    select requester_id id
+    from RequestAccepted
+    union all
+    select accepter_id
+    from RequestAccepted
+     ) t
+group by id
+order by count(*) desc
+limit 1;
 #leetcode submit region end(Prohibit modification and deletion)
