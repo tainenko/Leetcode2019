4b2d93b72f0b81d7178c395cfd6923d009ec7503
tainenko <31752048+tainenko@users.noreply.github.com>
2023-05-01 18:16:53 +0800

    add solution of leetcode 626 exchange seats

---
+++ leetcode/editor/en/[626]Exchange Seats.sql
@@ -0,0 +1,66 @@
+#Table: Seat 
+#
+# 
+#+-------------+---------+
+#| Column Name | Type    |
+#+-------------+---------+
+#| id          | int     |
+#| student     | varchar |
+#+-------------+---------+
+#id is the primary key column for this table.
+#Each row of this table indicates the name and the ID of a student.
+#id is a continuous increment.
+# 
+#
+# 
+#
+# Write an SQL query to swap the seat id of every two consecutive students. If 
+#the number of students is odd, the id of the last student is not swapped. 
+#
+# Return the result table ordered by id in ascending order. 
+#
+# The query result format is in the following example. 
+#
+# 
+# Example 1: 
+#
+# 
+#Input: 
+#Seat table:
+#+----+---------+
+#| id | student |
+#+----+---------+
+#| 1  | Abbot   |
+#| 2  | Doris   |
+#| 3  | Emerson |
+#| 4  | Green   |
+#| 5  | Jeames  |
+#+----+---------+
+#Output: 
+#+----+---------+
+#| id | student |
+#+----+---------+
+#| 1  | Doris   |
+#| 2  | Abbot   |
+#| 3  | Green   |
+#| 4  | Emerson |
+#| 5  | Jeames  |
+#+----+---------+
+#Explanation: 
+#Note that if the number of students is odd, there is no need to change the 
+#last one's seat.
+# 
+#
+# Related Topics Database 👍 975 👎 418
+
+
+#leetcode submit region begin(Prohibit modification and deletion)
+# Write your MySQL query statement below
+select case
+    when id%2=1 and id=last then id
+    when id%2=1 then id+1
+    when id%2=0 then id-1
+    end id , student
+from seat, ( select count(*) as last from seat) as tmp
+order by id
+#leetcode submit region end(Prohibit modification and deletion)
