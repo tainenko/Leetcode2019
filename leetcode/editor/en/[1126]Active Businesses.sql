2ff4092b6df3227c8bdb5fca43e8f06a1173722e
tainenko <31752048+tainenko@users.noreply.github.com>
2023-05-08 16:30:23 +0800

    init problem of leetcode 1126 active businesses

---
+++ leetcode/editor/en/[1126]Active Businesses.sql
@@ -0,0 +1,68 @@
+#Table: Events 
+#
+# 
+#+---------------+---------+
+#| Column Name   | Type    |
+#+---------------+---------+
+#| business_id   | int     |
+#| event_type    | varchar |
+#| occurences    | int     | 
+#+---------------+---------+
+#(business_id, event_type) is the primary key of this table.
+#Each row in the table logs the info that an event of some type occurred at 
+#some business for a number of times.
+# 
+#
+# 
+#
+# The average activity for a particular event_type is the average occurences 
+#across all companies that have this event. 
+#
+# An active business is a business that has more than one event_type such that 
+#their occurences is strictly greater than the average activity for that event. 
+#
+# Write an SQL query to find all active businesses. 
+#
+# Return the result table in any order. 
+#
+# The query result format is in the following example. 
+#
+# 
+# Example 1: 
+#
+# 
+#Input: 
+#Events table:
+#+-------------+------------+------------+
+#| business_id | event_type | occurences |
+#+-------------+------------+------------+
+#| 1           | reviews    | 7          |
+#| 3           | reviews    | 3          |
+#| 1           | ads        | 11         |
+#| 2           | ads        | 7          |
+#| 3           | ads        | 6          |
+#| 1           | page views | 3          |
+#| 2           | page views | 12         |
+#+-------------+------------+------------+
+#Output: 
+#+-------------+
+#| business_id |
+#+-------------+
+#| 1           |
+#+-------------+
+#Explanation:  
+#The average activity for each event can be calculated as follows:
+#- 'reviews': (7+3)/2 = 5
+#- 'ads': (11+7+6)/3 = 8
+#- 'page views': (3+12)/2 = 7.5
+#The business with id=1 has 7 'reviews' events (more than 5) and 11 'ads' 
+#events (more than 8), so it is an active business.
+# 
+#
+# Related Topics Database 👍 247 👎 26
+
+
+#leetcode submit region begin(Prohibit modification and deletion)
+# Write your MySQL query statement below
+
+#leetcode submit region end(Prohibit modification and deletion)
