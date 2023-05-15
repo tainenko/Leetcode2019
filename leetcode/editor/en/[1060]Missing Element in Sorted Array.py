b0e316c37f7b5fc28037520c0705ce81ccb04b23
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-05-15 23:57:31 +0800

    add solution of leetcode 1060 missing element in sorted array

--- leetcode/editor/en/[1060]Missing Element in Sorted Array.py
+++ leetcode/editor/en/[1060]Missing Element in Sorted Array.py
@@ -49,5 +49,12 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def missingElement(self, nums: List[int], k: int) -> int:
-        
+        for i in range(1, len(nums)):
+            gap = nums[i] - nums[i - 1] - 1
+            if gap >= k:
+                return nums[i - 1] + k
+            else:
+                k -= gap
+        return nums[-1] + k
+
 # leetcode submit region end(Prohibit modification and deletion)
