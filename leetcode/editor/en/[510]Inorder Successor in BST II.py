2213347b817aac3be6a6ae25afd4fdf9e932a2fa
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-05-16 00:09:25 +0800

    add solution of leetcode 510 inorder successor in bst ii

--- leetcode/editor/en/[510]Inorder Successor in BST II.py
+++ leetcode/editor/en/[510]Inorder Successor in BST II.py
@@ -62,7 +62,19 @@ class Node:
         self.parent = None
 """
 
+
 class Solution:
     def inorderSuccessor(self, node: 'Node') -> 'Optional[Node]':
-        
+        if not node:
+            return node
+
+        if node.right:
+            node = node.right
+            while node.left:
+                node = node.left
+            return node
+
+        while node.parent and node.parent.right == node:
+            node = node.parent
+        return node.parent
 # leetcode submit region end(Prohibit modification and deletion)
