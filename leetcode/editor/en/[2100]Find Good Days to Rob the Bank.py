4766559e037b88785876ca7ff45cd54d7f76a22f
Tony Ko <31752048+tainenko@users.noreply.github.com>
2023-07-19 00:11:40 +0800

    add solution of leetcode 2100 find good days to rob the bank

--- leetcode/editor/en/[2100]Find Good Days to Rob the Bank.py
+++ leetcode/editor/en/[2100]Find Good Days to Rob the Bank.py
@@ -69,5 +69,16 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
-        
+        dec = [0] * len(security)
+        inc = [0] * len(security)
+
+        for i in range(1, len(security)):
+            if security[i] <= security[i - 1]:
+                dec[i] = dec[i - 1] + 1
+
+        for i in range(len(security) - 2, -1, -1):
+            if security[i] <= security[i + 1]:
+                inc[i] = inc[i + 1] + 1
+
+        return [i for i, (a, b) in enumerate(zip(inc, dec)) if a >= time and b >= time]
 # leetcode submit region end(Prohibit modification and deletion)
