c67caf79eefe497e74285e5f46a9cf9d292febe2
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-19 10:03:14 +0800

    add solution of leetcode 2041 accepted candidates from the interviews

--- leetcode/editor/en/[2041]Accepted Candidates From the Interviews.sql
+++ leetcode/editor/en/[2041]Accepted Candidates From the Interviews.sql
@@ -94,5 +94,11 @@
 
 #leetcode submit region begin(Prohibit modification and deletion)
 # Write your MySQL query statement below
-
+SELECT candidate_id
+FROM
+    Candidates AS c
+    LEFT JOIN Rounds AS r ON c.interview_id = r.interview_id
+WHERE years_of_exp >= 2
+GROUP BY c.interview_id
+HAVING sum(score) > 15;
 #leetcode submit region end(Prohibit modification and deletion)
