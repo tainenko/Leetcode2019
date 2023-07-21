d4b3b18f3c70c4e4e83bf7f2883a46948da24494
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-21 12:33:48 +0800

    add solution of leetcode 2112 the airport with the most traffic

--- leetcode/editor/en/[2112]The Airport With the Most Traffic.sql
+++ leetcode/editor/en/[2112]The Airport With the Most Traffic.sql
@@ -88,5 +88,20 @@
 
 #leetcode submit region begin(Prohibit modification and deletion)
 # Write your MySQL query statement below
+with cte as(
+    select departure_airport as airport_id, flights_count
+    from flights
+    union all
+    select arrival_airport as airport_id, flights_count
+    from flights
+)
 
+select airport_id
+from
+(
+select airport_id, rank() over (order by sum(flights_count) desc) rk
+from cte
+group by airport_id
+) tmp
+where rk=1
 #leetcode submit region end(Prohibit modification and deletion)
