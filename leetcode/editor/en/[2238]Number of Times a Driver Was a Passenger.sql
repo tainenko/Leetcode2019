c9e5732419e876e2d9ab2aee5f5eedcf73c050b1
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-21 10:47:20 +0800

    init problem of leetcode 2238 number of times a driver was a passenger

---
+++ leetcode/editor/en/[2238]Number of Times a Driver Was a Passenger.sql
@@ -0,0 +1,61 @@
+#Table: Rides 
+#
+# 
+#+--------------+------+
+#| Column Name  | Type |
+#+--------------+------+
+#| ride_id      | int  |
+#| driver_id    | int  |
+#| passenger_id | int  |
+#+--------------+------+
+#ride_id is the primary key for this table.
+#Each row of this table contains the ID of the driver and the ID of the 
+#passenger that rode in ride_id.
+#Note that driver_id != passenger_id.
+# 
+#
+# 
+#
+# Write an SQL query to report the ID of each driver and the number of times 
+#they were a passenger. 
+#
+# Return the result table in any order. 
+#
+# The query result format is in the following example. 
+#
+# 
+# Example 1: 
+#
+# 
+#Input: 
+#Rides table:
+#+---------+-----------+--------------+
+#| ride_id | driver_id | passenger_id |
+#+---------+-----------+--------------+
+#| 1       | 7         | 1            |
+#| 2       | 7         | 2            |
+#| 3       | 11        | 1            |
+#| 4       | 11        | 7            |
+#| 5       | 11        | 7            |
+#| 6       | 11        | 3            |
+#+---------+-----------+--------------+
+#Output: 
+#+-----------+-----+
+#| driver_id | cnt |
+#+-----------+-----+
+#| 7         | 2   |
+#| 11        | 0   |
+#+-----------+-----+
+#Explanation: 
+#There are two drivers in all the given rides: 7 and 11.
+#The driver with ID = 7 was a passenger two times.
+#The driver with ID = 11 was never a passenger.
+# 
+#
+# Related Topics Database 👍 59 👎 4
+
+
+#leetcode submit region begin(Prohibit modification and deletion)
+# Write your MySQL query statement below
+
+#leetcode submit region end(Prohibit modification and deletion)
