635f86485a807d46219342c7810d9daee3870dad
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-23 02:05:29 +0800

    add solution of leetcode 1501 countries you can safely invest in

--- leetcode/editor/en/[1501]Countries You Can Safely Invest In.sql
+++ leetcode/editor/en/[1501]Countries You Can Safely Invest In.sql
@@ -125,5 +125,18 @@
 
 #leetcode submit region begin(Prohibit modification and deletion)
 # Write your MySQL query statement below
-
+SELECT
+ co.name AS country
+FROM
+ person p
+ JOIN
+     country co
+     ON SUBSTRING(phone_number,1,3) = country_code
+ JOIN
+     calls c
+     ON p.id IN (c.caller_id, c.callee_id)
+GROUP BY
+ co.name
+HAVING
+ AVG(duration) > (SELECT AVG(duration) FROM calls)
 #leetcode submit region end(Prohibit modification and deletion)
