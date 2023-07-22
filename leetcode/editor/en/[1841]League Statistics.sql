d57b461528739d7fd9f78c04975968ef7a841666
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-22 23:12:38 +0800

    add solution of leetcode 1841 league statistics

--- leetcode/editor/en/[1841]League Statistics.sql
+++ leetcode/editor/en/[1841]League Statistics.sql
@@ -101,5 +101,39 @@
 
 #leetcode submit region begin(Prohibit modification and deletion)
 # Write your MySQL query statement below
+with cte as(
+    select
+    home_team_id as team_id,
+    home_team_goals as goal,
+    away_team_goals as goal_against,
+    case
+        when home_team_goals>away_team_goals then 3
+        when home_team_goals=away_team_goals then 1
+        else 0
+    end as points
+    from matches
+    union all
+    select away_team_id as team_id,
+    away_team_goals as goal,
+    home_team_goals as goal_against,
+    case
+        when home_team_goals<away_team_goals then 3
+        when home_team_goals=away_team_goals then 1
+        else 0
+    end as points
+    from matches
 
+)
+
+select
+    t.team_name,
+    count(*) as matches_played,
+    sum(cte.points) as points,
+    sum(cte.goal) as goal_for,
+    sum(cte.goal_against) as goal_against,
+    sum(cte.goal) - sum(cte.goal_against) as goal_diff
+from teams as t
+join cte using(team_id)
+group by t.team_id
+order by sum(cte.points) desc, sum(cte.goal) - sum(cte.goal_against) desc, t.team_name
 #leetcode submit region end(Prohibit modification and deletion)
