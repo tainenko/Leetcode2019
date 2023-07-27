3a8fe56d055224b98655a4eaddccebbab746e90e
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-27 09:04:52 +0800

    init problem of leetcode 1302 deepest leaves sum

---
+++ leetcode/editor/en/[1302]Deepest Leaves Sum.py
@@ -0,0 +1,41 @@
+# Given the root of a binary tree, return the sum of values of its deepest 
+# leaves.
+# 
+#  
+#  Example 1: 
+#  
+#  
+# Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+# Output: 15
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+# Output: 19
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  The number of nodes in the tree is in the range [1, 10⁴]. 
+#  1 <= Node.val <= 100 
+#  
+# 
+#  Related Topics Tree Depth-First Search Breadth-First Search Binary Tree 👍 43
+# 92 👎 113
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
