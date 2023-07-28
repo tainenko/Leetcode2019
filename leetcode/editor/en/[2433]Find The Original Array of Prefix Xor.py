f5e91653a883b1a8d247f849789e00547c5e6f63
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-29 01:05:11 +0800

    init problem of leetcode 2433 find the original array of prefix xor

---
+++ leetcode/editor/en/[2433]Find The Original Array of Prefix Xor.py
@@ -0,0 +1,49 @@
+# You are given an integer array pref of size n. Find and return the array arr 
+# of size n that satisfies: 
+# 
+#  
+#  pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i]. 
+#  
+# 
+#  Note that ^ denotes the bitwise-xor operation. 
+# 
+#  It can be proven that the answer is unique. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: pref = [5,2,0,3,1]
+# Output: [5,7,2,3,2]
+# Explanation: From the array [5,7,2,3,2] we have the following:
+# - pref[0] = 5.
+# - pref[1] = 5 ^ 7 = 2.
+# - pref[2] = 5 ^ 7 ^ 2 = 0.
+# - pref[3] = 5 ^ 7 ^ 2 ^ 3 = 3.
+# - pref[4] = 5 ^ 7 ^ 2 ^ 3 ^ 2 = 1.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: pref = [13]
+# Output: [13]
+# Explanation: We have pref[0] = arr[0] = 13.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= pref.length <= 10⁵ 
+#  0 <= pref[i] <= 10⁶ 
+#  
+# 
+#  Related Topics Array Bit Manipulation 👍 564 👎 26
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
