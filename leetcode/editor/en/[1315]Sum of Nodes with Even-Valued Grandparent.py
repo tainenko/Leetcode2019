e15df8ca056b379ac1c2b1a9de1fd7a514bb65fe
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-29 00:47:06 +0800

    init problem of leetcode 1315 sum of nodes with even-valued grandparent

---
+++ leetcode/editor/en/[1315]Sum of Nodes with Even-Valued Grandparent.py
@@ -0,0 +1,46 @@
+# Given the root of a binary tree, return the sum of values of nodes with an 
+# even-valued grandparent. If there are no nodes with an even-valued grandparent, 
+# return 0. 
+# 
+#  A grandparent of a node is the parent of its parent if it exists. 
+# 
+#  
+#  Example 1: 
+#  
+#  
+# Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+# Output: 18
+# Explanation: The red nodes are the nodes with even-value grandparent while 
+# the blue nodes are the even-value grandparents.
+#  
+# 
+#  Example 2: 
+#  
+#  
+# Input: root = [1]
+# Output: 0
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  The number of nodes in the tree is in the range [1, 10⁴]. 
+#  1 <= Node.val <= 100 
+#  
+# 
+#  Related Topics Tree Depth-First Search Breadth-First Search Binary Tree 👍 25
+# 29 👎 72
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
