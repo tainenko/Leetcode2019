1d51e9eca7a14a55fd8deda8774e82973c58386a
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-29 17:26:19 +0800

    add solution of leetcode 2161 partition array according to given pivot

--- leetcode/editor/en/[2161]Partition Array According to Given Pivot.py
+++ leetcode/editor/en/[2161]Partition Array According to Given Pivot.py
@@ -62,5 +62,6 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        
+        return [num for num in nums if num < pivot] + [num for num in nums if num == pivot] + [num for num in nums if
+                                                                                               num > pivot]
 # leetcode submit region end(Prohibit modification and deletion)
