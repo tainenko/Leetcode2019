883f29e491d453daa78c7d98e39ffd5eeaa3ecda
tainenko <31752048+tainenko@users.noreply.github.com>
2023-07-31 02:32:58 +0800

    add solution of leetcode 1551 minimum operations to make array equal

---
+++ leetcode/editor/en/[1551]Minimum Operations to Make Array Equal.py
@@ -0,0 +1,50 @@
+# You have an array arr of length n where arr[i] = (2 * i) + 1 for all valid 
+# values of i (i.e., 0 <= i < n). 
+# 
+#  In one operation, you can select two indices x and y where 0 <= x, y < n and 
+# subtract 1 from arr[x] and add 1 to arr[y] (i.e., perform arr[x] -=1 and arr[y] 
+# += 1). The goal is to make all the elements of the array equal. It is 
+# guaranteed that all the elements of the array can be made equal using some operations. 
+# 
+#  Given an integer n, the length of the array, return the minimum number of 
+# operations needed to make all the elements of arr equal. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: n = 3
+# Output: 2
+# Explanation: arr = [1, 3, 5]
+# First operation choose x = 2 and y = 0, this leads arr to be [2, 3, 4]
+# In the second operation choose x = 2 and y = 0 again, thus arr = [3, 3, 3].
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: n = 6
+# Output: 9
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= n <= 10⁴ 
+#  
+# 
+#  Related Topics Math 👍 1328 👎 170
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def minOperations(self, n: int) -> int:
+        if n % 2 == 1:
+            n //= 2
+            return n * (n + 1)
+        else:
+            n //= 2
+            return n * n
+
+# leetcode submit region end(Prohibit modification and deletion)
