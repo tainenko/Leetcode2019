f4f30e4cd353756559ab24cf8761ba924e9682a1
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-01 00:58:50 +0800

    add solution of leetcode 351 android unlock patterns

--- leetcode/editor/en/[351]Android Unlock Patterns.py
+++ leetcode/editor/en/[351]Android Unlock Patterns.py
@@ -70,5 +70,29 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def numberOfPatterns(self, m: int, n: int) -> int:
-        
+        def dfs(i, cnt=1):
+            if cnt > n:
+                return 0
+            visited[i] = True
+            res = int(cnt >= m)
+            for j in range(1, 10):
+                x = cross[i][j]
+                if not visited[j] and (visited[x] or x == 0):
+                    res += dfs(j, cnt + 1)
+            visited[i] = False
+            return res
+
+        cross = [[0] * 10 for _ in range(10)]
+
+        cross[1][3] = cross[3][1] = 2
+        cross[4][6] = cross[6][4] = 5
+        cross[7][9] = cross[9][7] = 8
+        cross[1][7] = cross[7][1] = 4
+        cross[2][8] = cross[8][2] = 5
+        cross[3][9] = cross[9][3] = 6
+        cross[1][9] = cross[9][1] = 5
+        cross[3][7] = cross[7][3] = 5
+        visited = [False for _ in range(10)]
+        return dfs(1) * 4 + dfs(2) * 4 + dfs(5)
+
 # leetcode submit region end(Prohibit modification and deletion)
