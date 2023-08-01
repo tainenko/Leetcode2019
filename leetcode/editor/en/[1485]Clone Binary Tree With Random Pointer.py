82409931ef0b026003dd1aaf7d5250192c9de2aa
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-01 09:34:04 +0800

    add solution of leetcode 1485 clone binary tree with random pointer

--- leetcode/editor/en/[1485]Clone Binary Tree With Random Pointer.py
+++ leetcode/editor/en/[1485]Clone Binary Tree With Random Pointer.py
@@ -68,5 +68,18 @@
 
 class Solution:
     def copyRandomBinaryTree(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
-        
+        def dfs(root: 'Optional[Node]') -> 'Optional[NodeCopy]':
+            if not root:
+                return None
+            if root in mp:
+                return mp[root]
+            copy = NodeCopy(root.val)
+            mp[root] = copy
+            copy.left = dfs(root.left)
+            copy.right = dfs(root.right)
+            copy.random = dfs(root.random)
+            return copy
+
+        mp = {}
+        return dfs(root)
 # leetcode submit region end(Prohibit modification and deletion)
