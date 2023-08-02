1d08898c9abe288b22feb04b1946738b27b6e777
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-02 23:00:14 +0800

    add solution of leetcode 1472 design browser history

--- leetcode/editor/en/[1472]Design Browser History.py
+++ leetcode/editor/en/[1472]Design Browser History.py
@@ -73,17 +73,25 @@
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        
+        self.history = [homepage]
+        self.idx = 0
 
     def visit(self, url: str) -> None:
-        
+        self.history = self.history[:self.idx + 1]
+        self.history.append(url)
+        self.idx += 1
 
     def back(self, steps: int) -> str:
-        
+        self.idx -= steps
+        if self.idx < 0:
+            self.idx = 0
+        return self.history[self.idx]
 
     def forward(self, steps: int) -> str:
-        
-
+        self.idx += steps
+        if self.idx >= len(self.history):
+            self.idx = len(self.history) - 1
+        return self.history[self.idx]
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
