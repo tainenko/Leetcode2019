e7d9c0ab5467cb76bfe95ac8d6d208d0365cc809
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-04 17:46:56 +0800

    init problme of leetcode 1079 letter tile possibilities

---
+++ leetcode/editor/en/[1079]Letter Tile Possibilities.py
@@ -0,0 +1,45 @@
+# You have n tiles, where each tile has one letter tiles[i] printed on it. 
+# 
+#  Return the number of possible non-empty sequences of letters you can make 
+# using the letters printed on those tiles. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: tiles = "AAB"
+# Output: 8
+# Explanation: The possible sequences are "A", "B", "AA", "AB", "BA", "AAB", 
+# "ABA", "BAA".
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: tiles = "AAABBC"
+# Output: 188
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: tiles = "V"
+# Output: 1
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= tiles.length <= 7 
+#  tiles consists of uppercase English letters. 
+#  
+# 
+#  Related Topics Hash Table String Backtracking Counting 👍 2317 👎 64
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
