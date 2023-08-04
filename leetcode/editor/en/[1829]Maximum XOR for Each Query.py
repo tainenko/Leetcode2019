b6d2ddeddc918f9b9dd6f6daee131d3218c57753
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-04 11:31:55 +0800

    init problem of leetcode 1829 maximum xor for each query

---
+++ leetcode/editor/en/[1829]Maximum XOR for Each Query.py
@@ -0,0 +1,63 @@
+# You are given a sorted array nums of n non-negative integers and an integer 
+# maximumBit. You want to perform the following query n times: 
+# 
+#  
+#  Find a non-negative integer k < 2maximumBit such that nums[0] XOR nums[1] 
+# XOR ... XOR nums[nums.length-1] XOR k is maximized. k is the answer to the iᵗʰ 
+# query. 
+#  Remove the last element from the current array nums. 
+#  
+# 
+#  Return an array answer, where answer[i] is the answer to the iᵗʰ query. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [0,1,1,3], maximumBit = 2
+# Output: [0,3,2,3]
+# Explanation: The queries are answered as follows:
+# 1ˢᵗ query: nums = [0,1,1,3], k = 0 since 0 XOR 1 XOR 1 XOR 3 XOR 0 = 3.
+# 2ⁿᵈ query: nums = [0,1,1], k = 3 since 0 XOR 1 XOR 1 XOR 3 = 3.
+# 3ʳᵈ query: nums = [0,1], k = 2 since 0 XOR 1 XOR 2 = 3.
+# 4ᵗʰ query: nums = [0], k = 3 since 0 XOR 3 = 3.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [2,3,4,7], maximumBit = 3
+# Output: [5,2,6,5]
+# Explanation: The queries are answered as follows:
+# 1ˢᵗ query: nums = [2,3,4,7], k = 5 since 2 XOR 3 XOR 4 XOR 7 XOR 5 = 7.
+# 2ⁿᵈ query: nums = [2,3,4], k = 2 since 2 XOR 3 XOR 4 XOR 2 = 7.
+# 3ʳᵈ query: nums = [2,3], k = 6 since 2 XOR 3 XOR 6 = 7.
+# 4ᵗʰ query: nums = [2], k = 5 since 2 XOR 5 = 7.
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: nums = [0,1,2,2,5,7], maximumBit = 3
+# Output: [4,3,6,4,6,7]
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  nums.length == n 
+#  1 <= n <= 10⁵ 
+#  1 <= maximumBit <= 20 
+#  0 <= nums[i] < 2maximumBit 
+#  nums is sorted in ascending order. 
+#  
+# 
+#  Related Topics Array Bit Manipulation Prefix Sum 👍 693 👎 51
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
