b20638e5c3b49dfa712e0f0f9a1d429945e5dce0
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-11 02:25:38 +0800

    add solution of leetcode 1833 maximum ice cream bars

--- leetcode/editor/en/[1833]Maximum Ice Cream Bars.py
+++ leetcode/editor/en/[1833]Maximum Ice Cream Bars.py
@@ -55,5 +55,13 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        
+        costs.sort()
+        res = 0
+        for cost in costs:
+            if coins < cost:
+                break
+            coins -= cost
+            res += 1
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
