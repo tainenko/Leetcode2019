24b2f200d7a18f4f2ebb21596f0b73d354813a2d
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-24 01:05:57 +0800

    add solution of leetcode 2348 number of zero-filled subarrays

--- leetcode/editor/en/[2348]Number of Zero-Filled Subarrays.py
+++ leetcode/editor/en/[2348]Number of Zero-Filled Subarrays.py
@@ -49,5 +49,20 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        
-# leetcode submit region end(Prohibit modification and deletion)
+        res = 0
+        i = 0
+        while i < len(nums):
+
+            if nums[i] != 0:
+                i += 1
+                continue
+            j = i
+            while j < len(nums) and nums[j] == 0:
+                j += 1
+            res += (j - i) * (j - i + 1) // 2
+
+            i = j + 1
+
+        return res
+
+    # leetcode submit region end(Prohibit modification and deletion)
