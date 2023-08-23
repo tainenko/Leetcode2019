97a425b5c8bf03c56da81d172b45c67513e19ba1
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-23 09:27:37 +0800

    add solution of leetcode 2828 check if a string is an acronym of words

--- leetcode/editor/en/[2828]Check if a String Is an Acronym of Words.py
+++ leetcode/editor/en/[2828]Check if a String Is an Acronym of Words.py
@@ -55,5 +55,5 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def isAcronym(self, words: List[str], s: str) -> bool:
-        
+        return "".join([word[0] for word in words]) == s
 # leetcode submit region end(Prohibit modification and deletion)
