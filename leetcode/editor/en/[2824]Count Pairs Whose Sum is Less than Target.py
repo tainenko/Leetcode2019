590049d22c68c4bf6475f62316d61276b09d1ff9
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-23 09:23:05 +0800

    add problem of leetcode 2824 count pairs whose sum is less than target

--- leetcode/editor/en/[2824]Count Pairs Whose Sum is Less than Target.py
+++ leetcode/editor/en/[2824]Count Pairs Whose Sum is Less than Target.py
@@ -50,5 +50,11 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
-        
+        res = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] < target:
+                    res += 1
+        return res
+
 # leetcode submit region end(Prohibit modification and deletion)
