cfc4f87d225eb89b51b95ab28a8c6b867f61e3ad
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-24 11:35:35 +0800

    init problem of leetcode encode number

---
+++ leetcode/editor/en/[1256]Encode Number.py
@@ -0,0 +1,37 @@
+# Given a non-negative integer num, Return its encoding string. 
+# 
+#  The encoding is done by converting the integer to a string using a secret 
+# function that you should deduce from the following table: 
+# 
+#  
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: num = 23
+# Output: "1000"
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: num = 107
+# Output: "101100"
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  0 <= num <= 10^9 
+#  
+# 
+#  Related Topics Math String Bit Manipulation 👍 71 👎 240
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def encode(self, num: int) -> str:
+        
+# leetcode submit region end(Prohibit modification and deletion)
