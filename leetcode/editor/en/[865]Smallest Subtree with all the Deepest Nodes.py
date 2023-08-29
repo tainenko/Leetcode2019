a2c2b475bd287e49d9e3e53d44f7a01c1c8be823
tainenko <31752048+tainenko@users.noreply.github.com>
2023-08-29 13:34:08 +0800

    add solution of leetcode 865 smallest subtree with all the deepest nodes

--- leetcode/editor/en/[865]Smallest Subtree with all the Deepest Nodes.py
+++ leetcode/editor/en/[865]Smallest Subtree with all the Deepest Nodes.py
@@ -66,5 +66,16 @@
 #         self.right = right
 class Solution:
     def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
-        
+        def dfs(root):
+            if not root:
+                return None, 0
+            l, d1 = dfs(root.left)
+            r, d2 = dfs(root.right)
+            if d1 > d2:
+                return l, d1 + 1
+            if d1 < d2:
+                return r, d2 + 1
+            return root, d1 + 1
+
+        return dfs(root)[0]
 # leetcode submit region end(Prohibit modification and deletion)
