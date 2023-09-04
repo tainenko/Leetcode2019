646fc1340550e0fa1965e948a8ee96d9ba8ee9d4
tainenko <31752048+tainenko@users.noreply.github.com>
2023-09-04 10:34:34 +0800

    add solution of leetcode 2839 check if strings can be made equal with operations i

--- leetcode/editor/en/[2839]Check if Strings Can be Made Equal With Operations I.py
+++ leetcode/editor/en/[2839]Check if Strings Can be Made Equal With Operations I.py
@@ -45,5 +45,5 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        
+        return {s1[0], s1[2]} == {s2[0], s2[2]} and {s1[1], s1[3]} == {s2[1], s2[3]}
 # leetcode submit region end(Prohibit modification and deletion)
