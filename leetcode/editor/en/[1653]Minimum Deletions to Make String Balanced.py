0c786f62f27323bbeb0075001f6b3a6133860eb2
tainenko <31752048+tainenko@users.noreply.github.com>
2023-09-16 00:52:08 +0800

    init problem of leetcode 1653 minimum deletions to make string balanced

---
+++ leetcode/editor/en/[1653]Minimum Deletions to Make String Balanced.py
@@ -0,0 +1,45 @@
+# You are given a string s consisting only of characters 'a' and 'b'. 
+# 
+#  You can delete any number of characters in s to make s balanced. s is 
+# balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 
+# 'a'. 
+# 
+#  Return the minimum number of deletions needed to make s balanced. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: s = "aababbab"
+# Output: 2
+# Explanation: You can either:
+# Delete the characters at 0-indexed positions 2 and 6 ("aababbab" -> "aaabbb"),
+#  or
+# Delete the characters at 0-indexed positions 3 and 6 ("aababbab" -> "aabbbb").
+# 
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: s = "bbaaaaabb"
+# Output: 2
+# Explanation: The only solution is to delete the first two characters.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 10⁵ 
+#  s[i] is 'a' or 'b'. 
+#  
+# 
+#  Related Topics String Dynamic Programming Stack 👍 1234 👎 35
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
