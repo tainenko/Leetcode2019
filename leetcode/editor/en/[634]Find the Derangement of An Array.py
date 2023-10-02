6ed9a448c233286e908475cecdd0b88f647291f4
tainenko <31752048+tainenko@users.noreply.github.com>
2023-10-03 00:14:25 +0800

    add solution of leetcode 634 find the derangement of an array

--- leetcode/editor/en/[634]Find the Derangement of An Array.py
+++ leetcode/editor/en/[634]Find the Derangement of An Array.py
@@ -35,5 +35,9 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def findDerangement(self, n: int) -> int:
-        
+        mod = 10 ** 9 + 7
+        a, b = 1, 0
+        for i in range(2, n + 1):
+            a, b = b, (i - 1) * (a + b) % mod
+        return b
 # leetcode submit region end(Prohibit modification and deletion)
