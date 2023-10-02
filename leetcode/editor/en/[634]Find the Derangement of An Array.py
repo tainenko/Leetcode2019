8d9de7a380b84a059853d635093eb0b95a73eca3
tainenko <31752048+tainenko@users.noreply.github.com>
2023-10-02 12:24:54 +0800

    init problem of leetcode 634 find the derangement of an array

---
+++ leetcode/editor/en/[634]Find the Derangement of An Array.py
@@ -0,0 +1,39 @@
+# In combinatorial mathematics, a derangement is a permutation of the elements 
+# of a set, such that no element appears in its original position. 
+# 
+#  You are given an integer n. There is originally an array consisting of n 
+# integers from 1 to n in ascending order, return the number of derangements it can 
+# generate. Since the answer may be huge, return it modulo 10⁹ + 7. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: n = 3
+# Output: 2
+# Explanation: The original array is [1,2,3]. The two derangements are [2,3,1] 
+# and [3,1,2].
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: n = 2
+# Output: 1
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= n <= 10⁶ 
+#  
+# 
+#  Related Topics Math Dynamic Programming 👍 213 👎 161
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def findDerangement(self, n: int) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
