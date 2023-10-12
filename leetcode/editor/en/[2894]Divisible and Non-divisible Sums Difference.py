7a4b9b2ae98c7aadadc6089ee8d8f407a07aca47
tainenko <31752048+tainenko@users.noreply.github.com>
2023-10-13 00:34:40 +0800

    add solution of leetcode 2894 divisible and non-divisible sums difference

--- leetcode/editor/en/[2894]Divisible and Non-divisible Sums Difference.py
+++ leetcode/editor/en/[2894]Divisible and Non-divisible Sums Difference.py
@@ -63,5 +63,12 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
-        
+        res = 0
+        for num in range(1, n + 1):
+            if num % m == 0:
+                sign = -1
+            else:
+                sign = 1
+            res += num * sign
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
