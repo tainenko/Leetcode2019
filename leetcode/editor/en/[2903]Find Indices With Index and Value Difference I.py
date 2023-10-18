8a10c73fb61e0278ef5504748ce9367c87a45816
tainenko <31752048+tainenko@users.noreply.github.com>
2023-10-18 10:01:06 +0800

    add solution of leetcode 2903 find indices with index and value difference i

--- leetcode/editor/en/[2903]Find Indices With Index and Value Difference I.py
+++ leetcode/editor/en/[2903]Find Indices With Index and Value Difference I.py
@@ -63,5 +63,9 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
-        
+        for i in range(len(nums)):
+            for j in range(i + indexDifference, len(nums)):
+                if abs(nums[i] - nums[j]) >= valueDifference:
+                    return [i, j]
+        return [-1, -1]
 # leetcode submit region end(Prohibit modification and deletion)
