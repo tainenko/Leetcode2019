246594814f4b57df00ae80dd9153378882d74617
tainenko <31752048+tainenko@users.noreply.github.com>
2023-10-18 21:52:58 +0800

    add solution of leetcode 718 maximum length of repeated subarray

--- leetcode/editor/en/[718]Maximum Length of Repeated Subarray.py
+++ leetcode/editor/en/[718]Maximum Length of Repeated Subarray.py
@@ -33,5 +33,13 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        
+        m = len(nums1)
+        n = len(nums2)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                if nums1[i] == nums2[j]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+        return max(max(row) for row in dp)
 # leetcode submit region end(Prohibit modification and deletion)
