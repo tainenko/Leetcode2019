02efa7abc99ca53737fc0a0317adedf0454e140f
tainenko <31752048+tainenko@users.noreply.github.com>
2023-11-09 00:10:25 +0800

    add solution of leetcode 2923 find champion i

--- leetcode/editor/en/[2923]Find Champion I.py
+++ leetcode/editor/en/[2923]Find Champion I.py
@@ -51,5 +51,9 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def findChampion(self, grid: List[List[int]]) -> int:
-        
+        n = len(grid)
+        for i in range(n):
+            if all(grid[i][j] == 1 for j in range(n) if i != j):
+                return i
+        return -1
 # leetcode submit region end(Prohibit modification and deletion)
