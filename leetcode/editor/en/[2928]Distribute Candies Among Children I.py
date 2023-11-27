8cec807a5bc1d714a9d5e7e0ebb9b220a8c0cd28
tainenko <31752048+tainenko@users.noreply.github.com>
2023-11-27 12:28:09 +0800

    add solution of leetcode 2928 distribute candies among children i

--- leetcode/editor/en/[2928]Distribute Candies Among Children I.py
+++ leetcode/editor/en/[2928]Distribute Candies Among Children I.py
@@ -37,5 +37,11 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        
+        res = 0
+        for i in range(limit + 1):
+            for j in range(limit + 1):
+                for k in range(limit + 1):
+                    if i + j + k == n:
+                        res += 1
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
