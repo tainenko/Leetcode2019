24024a4ac9ddf16d7aeb6aadd15f71a741b8fd5b
tainenko <31752048+tainenko@users.noreply.github.com>
2024-02-29 16:02:24 +0800

    add oslution ofl eetcode 2970 count the number of incremovable subarrays i

--- leetcode/editor/en/[2970]Count the Number of Incremovable Subarrays I.py
+++ leetcode/editor/en/[2970]Count the Number of Incremovable Subarrays I.py
@@ -58,5 +58,19 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def incremovableSubarrayCount(self, nums: List[int]) -> int:
-        
+        i, n = 0, len(nums)
+        while i + 1 < n and nums[i] < nums[i + 1]:
+            i += 1
+        if i == n - 1:
+            return n * (n + 1) // 2
+        res = i + 2
+        j = n - 1
+        while j:
+            while i >= 0 and nums[i] >= nums[j]:
+                i -= 1
+            res += i + 2
+            if nums[j - 1] >= nums[j]:
+                break
+            j -= 1
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
