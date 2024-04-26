e77bf3ba3cde0d7076842e6fd9074610d48b5c86
tainenko <31752048+tainenko@users.noreply.github.com>
2024-04-26 11:15:09 +0800

    init problem of leetcode 3120 count the number of special characters i

---
+++ leetcode/editor/en/[3120]Count the Number of Special Characters I.py
@@ -0,0 +1,59 @@
+# You are given a string word. A letter is called special if it appears both in 
+# lowercase and uppercase in word. 
+# 
+#  Return the number of special letters in word. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+#  Input: word = "aaAbcBC" 
+#  
+# 
+#  Output: 3 
+# 
+#  Explanation: 
+# 
+#  The special characters in word are 'a', 'b', and 'c'. 
+# 
+#  Example 2: 
+# 
+#  
+#  Input: word = "abc" 
+#  
+# 
+#  Output: 0 
+# 
+#  Explanation: 
+# 
+#  No character in word appears in uppercase. 
+# 
+#  Example 3: 
+# 
+#  
+#  Input: word = "abBCab" 
+#  
+# 
+#  Output: 1 
+# 
+#  Explanation: 
+# 
+#  The only special character in word is 'b'. 
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= word.length <= 50 
+#  word consists of only lowercase and uppercase English letters. 
+#  
+# 
+#  Related Topics Hash Table String 👍 79 👎 1
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        
+
+# leetcode submit region end(Prohibit modification and deletion)
