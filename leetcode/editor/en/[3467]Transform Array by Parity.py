f3359029935a503c1173737d7e2c3052994843dd
tainenko <31752048+tainenko@users.noreply.github.com>
2025-05-09 17:26:53 +0800

    init problem of leetcode 3467 transform array by parity

---
+++ leetcode/editor/en/[3467]Transform Array by Parity.py
@@ -0,0 +1,60 @@
+# You are given an integer array nums. Transform nums by performing the 
+# following operations in the exact order specified: 
+# 
+#  
+#  Replace each even number with 0. 
+#  Replace each odd numbers with 1. 
+#  Sort the modified array in non-decreasing order. 
+#  
+# 
+#  Return the resulting array after performing these operations. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+#  Input: nums = [4,3,2,1] 
+#  
+# 
+#  Output: [0,0,1,1] 
+# 
+#  Explanation: 
+# 
+#  
+#  Replace the even numbers (4 and 2) with 0 and the odd numbers (3 and 1) with 
+# 1. Now, nums = [0, 1, 0, 1]. 
+#  After sorting nums in non-descending order, nums = [0, 0, 1, 1]. 
+#  
+# 
+#  Example 2: 
+# 
+#  
+#  Input: nums = [1,5,1,4,2] 
+#  
+# 
+#  Output: [0,0,1,1,1] 
+# 
+#  Explanation: 
+# 
+#  
+#  Replace the even numbers (4 and 2) with 0 and the odd numbers (1, 5 and 1) 
+# with 1. Now, nums = [1, 1, 1, 0, 0]. 
+#  After sorting nums in non-descending order, nums = [0, 0, 1, 1, 1]. 
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= nums.length <= 100 
+#  1 <= nums[i] <= 1000 
+#  
+# 
+#  Related Topics Array Sorting Counting 👍 42 👎 2
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
