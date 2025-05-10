2c468c512265abe8c46fbd388498947bb7e4c2ee
tony.ko <31752048+tainenko@users.noreply.github.com>
2025-05-10 20:49:45 +0800

    add solution of leetcode 3432 count partitions with even sum difference

--- leetcode/editor/en/leetcode/editor/en/[3432]Count Partitions with Even Sum Difference.py
+++ leetcode/editor/en/leetcode/editor/en/[3432]Count Partitions with Even Sum Difference.py
@@ -69,5 +69,13 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def countPartitions(self, nums: List[int]) -> int:
-        
+        left = 0
+        right = sum(nums)
+        res = 0
+        for i in range(len(nums) - 1):
+            left += nums[i]
+            right -= nums[i]
+            if (left - right) % 2 == 0:
+                res += 1
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
