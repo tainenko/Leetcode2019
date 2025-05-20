1f653b12d565636ee74676f3684d8472a4a5f9b0
tainenko <31752048+tainenko@users.noreply.github.com>
2025-05-20 10:11:40 +0800

    init problem of leetcode 3191 minimum operations to make binary array elements equal to one i

---
+++ leetcode/editor/en/[3191]Minimum Operations to Make Binary Array Elements Equal to One I.py
@@ -0,0 +1,61 @@
+# You are given a binary array nums. 
+# 
+#  You can do the following operation on the array any number of times (
+# possibly zero): 
+# 
+#  
+#  Choose any 3 consecutive elements from the array and flip all of them. 
+#  
+# 
+#  Flipping an element means changing its value from 0 to 1, and from 1 to 0. 
+# 
+#  Return the minimum number of operations required to make all elements in 
+# nums equal to 1. If it is impossible, return -1. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+#  Input: nums = [0,1,1,1,0,0] 
+#  
+# 
+#  Output: 3 
+# 
+#  Explanation: We can do the following operations: 
+# 
+#  
+#  Choose the elements at indices 0, 1 and 2. The resulting array is nums = [1,0
+# ,0,1,0,0]. 
+#  Choose the elements at indices 1, 2 and 3. The resulting array is nums = [1,1
+# ,1,0,0,0]. 
+#  Choose the elements at indices 3, 4 and 5. The resulting array is nums = [1,1
+# ,1,1,1,1]. 
+#  
+# 
+#  Example 2: 
+# 
+#  
+#  Input: nums = [0,1,1,1] 
+#  
+# 
+#  Output: -1 
+# 
+#  Explanation: It is impossible to make all elements equal to 1. 
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  3 <= nums.length <= 10⁵ 
+#  0 <= nums[i] <= 1 
+#  
+# 
+#  Related Topics Array Bit Manipulation Queue Sliding Window Prefix Sum 👍 623 
+# 👎 30
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
