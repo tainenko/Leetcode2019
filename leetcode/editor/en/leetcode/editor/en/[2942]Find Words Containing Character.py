bbb569fb436d451a1427bcf1821ee9d2d08146c6
tony.ko <31752048+tainenko@users.noreply.github.com>
2025-05-24 10:55:35 +0800

    add solution of leetcode 2942 find words containing character

---
+++ leetcode/editor/en/leetcode/editor/en/[2942]Find Words Containing Character.py
@@ -0,0 +1,54 @@
+# You are given a 0-indexed array of strings words and a character x. 
+# 
+#  Return an array of indices representing the words that contain the character 
+# x. 
+# 
+#  Note that the returned array may be in any order. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: words = ["leet","code"], x = "e"
+# Output: [0,1]
+# Explanation: "e" occurs in both words: "leet", and "code". Hence, we return 
+# indices 0 and 1.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+# Output: [0,2]
+# Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2
+# .
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+# Output: []
+# Explanation: "z" does not occur in any of the words. Hence, we return an 
+# empty array.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= words.length <= 50 
+#  1 <= words[i].length <= 50 
+#  x is a lowercase English letter. 
+#  words[i] consists only of lowercase English letters. 
+#  
+# 
+#  Related Topics Array String 👍 421 👎 33
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        return [idx for idx, word in enumerate(words) if word.find(x) != -1]
+        
+# leetcode submit region end(Prohibit modification and deletion)
