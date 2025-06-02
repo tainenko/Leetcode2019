48b918ccda6356933baea060789ba86dc733a872
tainenko <31752048+tainenko@users.noreply.github.com>
2025-06-02 10:46:37 +0800

    add solution of leetcode 2486 append characters to string to make subsequence

--- leetcode/editor/en/[2486]Append Characters to String to Make Subsequence.py
+++ leetcode/editor/en/[2486]Append Characters to String to Make Subsequence.py
@@ -54,5 +54,14 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
-        
+        i = 0
+        j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                i += 1
+        return len(t) - j
+
 # leetcode submit region end(Prohibit modification and deletion)
