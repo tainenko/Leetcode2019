673d1c40d94899f94fd2ce18a5f797831b56ef55
tainenko <31752048+tainenko@users.noreply.github.com>
2025-06-04 14:04:54 +0800

    add solution of leetcode 1530 number of good leaf nodes pairs

--- leetcode/editor/en/[1530]Number of Good Leaf Nodes Pairs.py
+++ leetcode/editor/en/[1530]Number of Good Leaf Nodes Pairs.py
@@ -53,5 +53,25 @@
 #         self.right = right
 class Solution:
     def countPairs(self, root: Optional[TreeNode], distance: int) -> int:
-        
+        def dfs(root, cnt, i):
+            if root is None or i >= distance:
+                return
+            if root.left is None and root.right is None:
+                cnt[i] += 1
+                return
+            dfs(root.left, cnt, i + 1)
+            dfs(root.right, cnt, i + 1)
+
+        if not root:
+            return 0
+        ans = self.countPairs(root.left, distance) + self.countPairs(root.right, distance)
+        cnt1 = Counter()
+        cnt2 = Counter()
+        dfs(root.left, cnt1, 1)
+        dfs(root.right, cnt2, 1)
+        for k1, v1 in cnt1.items():
+            for k2, v2 in cnt2.items():
+                if k1 + k2 <= distance:
+                    ans += v1 * v2
+        return ans
 # leetcode submit region end(Prohibit modification and deletion)
