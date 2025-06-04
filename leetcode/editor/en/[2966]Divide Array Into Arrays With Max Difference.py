3bad56b21cba7f2af02416e53472614469e668b3
tainenko <31752048+tainenko@users.noreply.github.com>
2025-06-04 17:38:43 +0800

    add solution of leetcode 2966 divide array into arrays with max difference

--- leetcode/editor/en/[2966]Divide Array Into Arrays With Max Difference.py
+++ leetcode/editor/en/[2966]Divide Array Into Arrays With Max Difference.py
@@ -78,5 +78,11 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
-        
+        nums.sort()
+        ans = []
+        for i in range(0, len(nums), 3):
+            if nums[i + 2] - nums[i] > k:
+                return []
+            ans.append(nums[i:i + 3])
+        return ans
 # leetcode submit region end(Prohibit modification and deletion)
