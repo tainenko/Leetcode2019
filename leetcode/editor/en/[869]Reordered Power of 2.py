5add8947eb625315f67bfd1f9222ee9b37cba5fe
tainenko <31752048+tainenko@users.noreply.github.com>
2025-06-10 12:08:20 +0800

    init problem of leetcode 869 reordered power of 2

---
+++ leetcode/editor/en/[869]Reordered Power of 2.py
@@ -0,0 +1,36 @@
+# You are given an integer n. We reorder the digits in any order (including the 
+# original order) such that the leading digit is not zero. 
+# 
+#  Return true if and only if we can do this so that the resulting number is a 
+# power of two. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: n = 1
+# Output: true
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: n = 10
+# Output: false
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= n <= 10⁹ 
+#  
+# 
+#  Related Topics Hash Table Math Sorting Counting Enumeration 👍 2135 👎 440
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        
+# leetcode submit region end(Prohibit modification and deletion)
