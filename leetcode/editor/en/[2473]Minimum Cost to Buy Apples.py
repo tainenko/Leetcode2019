98887cbf096cb5e38c33f3a72e33d5e2c93aad67
tainenko <31752048+tainenko@users.noreply.github.com>
2025-06-18 16:56:15 +0800

    add solution of leetcode 2473 minimum cost to buy apples

--- leetcode/editor/en/[2473]Minimum Cost to Buy Apples.py
+++ leetcode/editor/en/[2473]Minimum Cost to Buy Apples.py
@@ -63,5 +63,27 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def minCost(self, n: int, roads: List[List[int]], appleCost: List[int], k: int) -> List[int]:
-        
+        def dijkstra(i: int) -> int:
+            q = [(0, i)]
+            dist = [inf] * n
+            dist[i] = 0
+            ans = inf
+            while q:
+                d, u = heappop(q)
+                ans = min(ans, appleCost[u] + d * (k + 1))
+                for v, w in g[u]:
+
+                    if dist[v] > dist[u] + w:
+                        dist[v] = dist[u] + w
+                        heappush(q, (dist[v], v))
+            return ans
+
+        g = defaultdict(list)
+        for a, b, c in roads:
+            a -= 1
+            b -= 1
+            g[a].append((b, c))
+            g[b].append((a, c))
+        return [dijkstra(i) for i in range(n)]
+
 # leetcode submit region end(Prohibit modification and deletion)
